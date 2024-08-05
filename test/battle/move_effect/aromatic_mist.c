107933fe35929b1d06468bb525681535cd112029
Greednut <115999607+Greednut@users.noreply.github.com>
2024-08-04 21:20:55 -0300

    hail mary

--- test/battle/move_effect/aromatic_mist.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "test/battle.h"
-
-TO_DO_BATTLE_TEST("Aromatic Mist raises Sp. Defense of a target ally by 1 stage");
-TO_DO_BATTLE_TEST("Aromatic Mist fails in Single Battles");
