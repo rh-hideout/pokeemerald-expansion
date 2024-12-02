026ef8bd66e55da0a7164c132a4730467b405d2c
Cmgold4 <cmgold428@gmail.com>
2024-12-02 12:56:02 -0700

    added pokerus flag and after ko annimations

--- include/config/pokemon.h
+++ include/config/pokemon.h
@@ -60,7 +60,7 @@
 // Eg: Replace with FLAG_UNUSED_0x264 so you can use that flag to toggle the feature.
 #define P_FLAG_FORCE_SHINY      0     // If this flag is set, all wild and gift Pokémon will forced into being Shiny.
 #define P_FLAG_FORCE_NO_SHINY   0     // If this flag is set, all wild and gift Pokémon will forced into NOT being Shiny.
-
+#define P_FLAG_FORCE_POKERUS    FLAG_FORCE_POKERUS      // If this flag is set, all wild and gift Pokémon will be forced into having Pokérus.
 // Go here if you want to disable specific families of Pokémon.
 #include "config/species_enabled.h"
 
--- include/constants/flags.h
+++ include/constants/flags.h
@@ -653,7 +653,7 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
 #define FLAG_HIDDEN_ITEMS_END  0x264 // was Unused Flag
-#define FLAG_UNUSED_0x265  0x265 // Unused Flag
+#define FLAG_FORCE_POKERUS  0x265 // forced Pokerus flag
 #define FLAG_UNUSED_0x266  0x266 // Unused Flag
 #define FLAG_UNUSED_0x267  0x267 // Unused Flag
 #define FLAG_UNUSED_0x268  0x268 // Unused Flag
