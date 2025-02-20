dc0da74e0ad349e7f907054062aae58367a8e583
Ruben <rubenwhite3005@gmail.com>
2025-02-20 16:11:40 +0000

    test merge part 2

--- include/bw_summary_screen.h
+++ include/bw_summary_screen.h
@@ -19,7 +19,7 @@
 #define BW_SUMMARY_NATURE_COLORS                    TRUE                // color stats increased or reduced by nature, red = boosted, blue = reduced
 #define BW_SUMMARY_NATURE_ARROWS                    FALSE               // arrows to show stats increased or reduced by nature
 #define BW_SUMMARY_CATEGORY_ICONS                   TRUE                // determines whether category (split) icons are shown or not
-#define BW_SUMMARY_IV_EV_DISPLAY                    BW_IV_EV_HIDDEN     // determines how to show IVs and EVs
+#define BW_SUMMARY_IV_EV_DISPLAY                    BW_IV_EV_PRECISE     // determines how to show IVs and EVs
 #define BW_SUMMARY_DECAP                            FALSE               // indicates if summary screen-specific strings should be decapitalized
 #define BW_SUMMARY_SHOW_FRIENDSHIP                  FALSE               // show a heart that fills up to indicate friendship value
 #define BW_SUMMARY_BW_STATUS_ICONS                  TRUE                // use Gen 5 style status icons instead of the default ones.
@@ -30,7 +30,7 @@
 #define BW_SUMMARY_BG_BLEND                         TRUE                // enables alpha blending for the main UI (semi-transparency)
 #define BW_SUMMARY_MON_IDLE_ANIMS                   TRUE                // loops the mon animations regularly as an "idle" anim
 #define BW_SUMMARY_MON_SHADOWS                      TRUE                // displays a shadow for the mon sprite
-#define BW_SUMMARY_SHOW_TERA_TYPE                   FALSE               // show tera type icons
+#define BW_SUMMARY_SHOW_TERA_TYPE                   TRUE               // show tera type icons
 #define BW_SUMMARY_SHOW_CONTEST_MOVES               TRUE                // show the contest moves page
 
 // Looking for configs for renaming mons and relearning moves? Those use the standard expansion configs
