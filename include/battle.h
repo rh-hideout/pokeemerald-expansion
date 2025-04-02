18ac2a75b15b0dbcb73734bfbd8ba3c04e48e41b
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2025-04-02 15:30:45 +0200

    Update include/battle.h
    Co-authored-by: Bassoonian <iasperbassoonian@gmail.com>

--- include/battle.h
+++ include/battle.h
@@ -725,7 +725,7 @@ struct BattleStruct
     u8 startingStatusDone:1;
     u8 terrainDone:1;
     u8 overworldWeatherDone:1;
-    u8 Unused:3;
+    u8 unused:3;
     u8 isAtkCancelerForCalledMove:1; // Certain cases in atk canceler should only be checked once, when the original move is called, however others need to be checked the twice.
     u8 friskedAbility:1; // If identifies two mons, show the ability pop-up only once.
     u8 fickleBeamBoosted:1;
