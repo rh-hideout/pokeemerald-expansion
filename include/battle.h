e16201d7afabab2dc6aa3d8c4c02878754dcb7e4
AlexOn1ine <alexthenotes@gmail.com>
2025-04-02 15:11:54 +0200

    review

--- include/battle.h
+++ include/battle.h
@@ -205,14 +205,14 @@ struct SpecialStatus
     u8 emergencyExited:1;
     u8 afterYou:1;
     u8 enduredDamage:1;
-    u8 unused_1:1;
-    // End of byte
     u8 stormDrainRedirected:1;
+    // End of byte
     u8 switchInAbilityDone:1;
     u8 switchInItemDone:1;
     u8 instructedChosenTarget:3;
     u8 berryReduced:1;
-    u8 unused_2:1;
+    u8 announceNeutralizingGas:1;   // See Cmd_switchineffects
+    u8 neutralizingGasRemoved:1;    // See VARIOUS_TRY_END_NEUTRALIZING_GAS
     // End of byte
     u8 gemParam;
     // End of byte
@@ -220,16 +220,14 @@ struct SpecialStatus
     u8 rototillerAffected:1;  // to be affected by rototiller
     u8 parentalBondState:2;
     u8 multiHitOn:1;
-    u8 announceNeutralizingGas:1;   // See Cmd_switchineffects
-    u8 neutralizingGasRemoved:1;    // See VARIOUS_TRY_END_NEUTRALIZING_GAS
-    u8 unused_3:1;
+    u8 distortedTypeMatchups:1;
+    u8 teraShellAbilityDone:1;
+    u8 criticalHit:1;
     // End of byte
     u8 dancerUsedMove:1;
     u8 dancerOriginalTarget:3;
     u8 preventLifeOrbDamage:1; // So that Life Orb doesn't activate various effects.
-    u8 distortedTypeMatchups:1;
-    u8 teraShellAbilityDone:1;
-    u8 criticalHit:1;
+    u8 unused:3;
     // End of byte
 };
 
