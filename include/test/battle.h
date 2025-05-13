b22b190a2e643984240f385ee1516ea4a89a826a
Eduardo Quezada <eduardo602002@gmail.com>
2025-05-13 09:43:48 -0400

    Abilities (100)

--- include/test/battle.h
+++ include/test/battle.h
@@ -752,7 +752,7 @@ extern struct BattleTestRunnerState *const gBattleTestRunnerState;
 
 /* Test */
 
-#define TO_DO_BATTLE_TEST(_name) \
+#define TO_DO_BATTLE_TEST(_name, ...) \
     TEST(_name) \
     { \
         TO_DO; \
