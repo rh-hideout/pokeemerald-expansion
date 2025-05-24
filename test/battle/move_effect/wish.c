539661158ba081c0ca94907c151bb85350e29286
DarkVexon <edanvirgo@gmail.com>
2025-05-24 14:57:03 -0700

    Add heal block fix and Wish tests

--- test/battle/move_effect/wish.c
+++ test/battle/move_effect/wish.c
@@ -1,4 +1,29 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("TODO: Write Wish (Move Effect) test titles")
+SINGLE_BATTLE_TEST("Wish heals")
+{
+	GIVEN {
+		PLAYER(SPECIES_TOGETIC);
+		OPPONENT(SPECIES_LUNATONE);
+	} WHEN {
+		TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_TACKLE); }
+		TURN { MOVE(player, MOVE_SPLASH); MOVE(opponent, MOVE_TACKLE); }
+	} SCENE {
+		HP_BAR(player);
+	}
+}
+
+SINGLE_BATTLE_TEST("Wish is blocked by Heal Block")
+{
+	GIVEN {
+		PLAYER(SPECIES_TOGETIC);
+		OPPONENT(SPECIES_LUNATONE);
+	} WHEN {
+		TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_TACKLE); }
+		TURN { MOVE(player, MOVE_SPLASH); MOVE(opponent, MOVE_HEAL_BLOCK); }
+	} SCENE {
+		MESSAGE("Togetic was prevented from healing!");
+		NOT HP_BAR(player);
+	}
+}	
