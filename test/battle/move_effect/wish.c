6aa87c278dc724440b33f171541df65aac10d9a0
DarkVexon <edanvirgo@gmail.com>
2025-05-25 10:31:34 -0700

    Update tests to match provided standard

--- test/battle/move_effect/wish.c
+++ test/battle/move_effect/wish.c
@@ -1,29 +1,40 @@
 #include "global.h"
 #include "test/battle.h"
 
-SINGLE_BATTLE_TEST("Wish heals")
+SINGLE_BATTLE_TEST("Wish heals the user at the end of the next turn")
 {
-	GIVEN {
-		PLAYER(SPECIES_TOGETIC);
-		OPPONENT(SPECIES_LUNATONE);
-	} WHEN {
-		TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_TACKLE); }
-		TURN { MOVE(player, MOVE_SPLASH); MOVE(opponent, MOVE_TACKLE); }
-	} SCENE {
-		HP_BAR(player);
-	}
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_POUND); }
+        TURN { }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_WISH, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_POUND, opponent);
+        HP_BAR(player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
+        MESSAGE("Wobbuffet's wish came true!");
+        HP_BAR(player);
+    }
 }
 
 SINGLE_BATTLE_TEST("Wish is blocked by Heal Block")
 {
-	GIVEN {
-		PLAYER(SPECIES_TOGETIC);
-		OPPONENT(SPECIES_LUNATONE);
-	} WHEN {
-		TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_TACKLE); }
-		TURN { MOVE(player, MOVE_SPLASH); MOVE(opponent, MOVE_HEAL_BLOCK); }
-	} SCENE {
-		MESSAGE("Togetic was prevented from healing!");
-		NOT HP_BAR(player);
-	}
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_WISH); MOVE(opponent, MOVE_POUND); }
+        TURN { MOVE(opponent, MOVE_HEAL_BLOCK); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_WISH, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_POUND, opponent);
+        HP_BAR(player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_HEAL_BLOCK, opponent);
+        MESSAGE("Wobbuffet was prevented from healing!");
+        NOT HP_BAR(player);
+    }
 }	
