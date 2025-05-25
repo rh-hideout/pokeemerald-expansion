db45f6057e11351051fed81dff71a23aa5259ebf
AlexOn1ine <alexthenotes@gmail.com>
2025-05-25 09:07:04 +0200

    sorting test

--- test/item.c
+++ test/item.c
@@ -6,29 +6,30 @@
 #include "test/test.h"
 #include "constants/vars.h"
 
-// TEST("TMs / HMs are sorted correctly in the bag")
-// {
-//     SetBagItemsPointers();
+TEST("TMs / HMs are sorted correctly in the bag")
+{
+    RUN_OVERWORLD_SCRIPT(
+        additem ITEM_HM07;
+        additem ITEM_TM25;
+        additem ITEM_TM14;
+        additem ITEM_TM42;
+        additem ITEM_HM05;
+        additem ITEM_TM05;
+        additem ITEM_TM01;
+        additem ITEM_HM02;
+    );
 
-//     RUN_OVERWORLD_SCRIPT(
-//         giveitem ITEM_HM07;
-//         giveitem ITEM_TM25;
-//         giveitem ITEM_TM14;
-//         giveitem ITEM_TM42;
-//         giveitem ITEM_HM05;
-//         giveitem ITEM_TM05;
-//         giveitem ITEM_TM01;
-//         giveitem ITEM_HM02;
-//     );
+    struct BagPocket *pocket = &gBagPockets[TMHM_POCKET];
 
-//     struct BagPocket *pocket = &gBagPockets[TMHM_POCKET];
+    SortTMHMs(pocket);
 
-//     EXPECT_EQ(pocket->itemSlots[0].itemId, ITEM_TM01);
-//     EXPECT_EQ(pocket->itemSlots[1].itemId, ITEM_TM05);
-//     EXPECT_EQ(pocket->itemSlots[2].itemId, ITEM_TM14);
-//     EXPECT_EQ(pocket->itemSlots[3].itemId, ITEM_TM25);
-//     EXPECT_EQ(pocket->itemSlots[4].itemId, ITEM_TM42);
-//     EXPECT_EQ(pocket->itemSlots[5].itemId, ITEM_HM02);
-//     EXPECT_EQ(pocket->itemSlots[6].itemId, ITEM_HM05);
-//     EXPECT_EQ(pocket->itemSlots[7].itemId, ITEM_HM07);
-// }
+    EXPECT_EQ(pocket->itemSlots[0].itemId, ITEM_TM01);
+    EXPECT_EQ(pocket->itemSlots[1].itemId, ITEM_TM05);
+    EXPECT_EQ(pocket->itemSlots[2].itemId, ITEM_TM14);
+    EXPECT_EQ(pocket->itemSlots[3].itemId, ITEM_TM25);
+    EXPECT_EQ(pocket->itemSlots[4].itemId, ITEM_TM42);
+    EXPECT_EQ(pocket->itemSlots[5].itemId, ITEM_HM02);
+    EXPECT_EQ(pocket->itemSlots[6].itemId, ITEM_HM05);
+    EXPECT_EQ(pocket->itemSlots[7].itemId, ITEM_HM07);
+    EXPECT_EQ(pocket->itemSlots[8].itemId, ITEM_NONE);
+}
