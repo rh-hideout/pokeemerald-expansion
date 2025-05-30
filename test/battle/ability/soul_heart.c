6858ac7ac961e35d338c70e4d2c03804653521e2
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2025-05-30 16:16:46 +0100

    Remove Protect from Soul Heart test (intended to verify if stat change animation plays after Memento)

--- test/battle/ability/soul_heart.c
+++ test/battle/ability/soul_heart.c
@@ -5,18 +5,18 @@ SINGLE_BATTLE_TEST("Soul Heart boosts Sp. Atk after opponent uses Memento")
 {
     GIVEN {
         ASSUME(GetMoveEffect(MOVE_MEMENTO) == EFFECT_MEMENTO);
-        ASSUME(GetMoveEffect(MOVE_PROTECT) == EFFECT_PROTECT);
         PLAYER(SPECIES_WOBBUFFET);
         PLAYER(SPECIES_WOBBUFFET);
         OPPONENT(SPECIES_MAGEARNA) { Ability(ABILITY_SOUL_HEART); }
     } WHEN {
-        TURN { MOVE(opponent, MOVE_PROTECT); MOVE(player, MOVE_MEMENTO); SEND_OUT(player, 1); }
+        TURN { MOVE(player, MOVE_MEMENTO); SEND_OUT(player, 1); }
     } SCENE {
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, opponent);
         MESSAGE("Wobbuffet fainted!");
         ABILITY_POPUP(opponent, ABILITY_SOUL_HEART);
         ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, opponent);
     } THEN {
-        EXPECT_EQ(opponent->statStages[STAT_SPATK], DEFAULT_STAT_STAGE + 1);
+        EXPECT_EQ(opponent->statStages[STAT_SPATK], DEFAULT_STAT_STAGE - 1);
     }
 }
 
