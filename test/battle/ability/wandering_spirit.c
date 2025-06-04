9368e81e97b0c7754453df0d478f8a745d010057
AlexOn1ine <alexthenotes@gmail.com>
2025-06-04 23:10:24 +0200

    change move

--- test/battle/ability/wandering_spirit.c
+++ test/battle/ability/wandering_spirit.c
@@ -6,13 +6,12 @@ TO_DO_BATTLE_TEST("TODO: Write Wandering Spirit (Ability) test titles")
 SINGLE_BATTLE_TEST("Wandering Spirit copied ability should not trigger on fainted mon")
 {
     GIVEN {
-        ASSUME(GetMoveEffect(MOVE_BRAVE_BIRD) == EFFECT_RECOIL);
         PLAYER(SPECIES_EKANS) { Ability(ABILITY_INTIMIDATE); }
         PLAYER(SPECIES_WOBBUFFET)
         OPPONENT(SPECIES_YAMASK_GALAR) { HP(1); Ability(ABILITY_WANDERING_SPIRIT); }
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
-        TURN { MOVE(player, MOVE_BRAVE_BIRD); SEND_OUT(opponent, 1); }
+        TURN { MOVE(player, MOVE_POISON_FANG); SEND_OUT(opponent, 1); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_BRAVE_BIRD, player);
         NOT ABILITY_POPUP(opponent, ABILITY_INTIMIDATE);
