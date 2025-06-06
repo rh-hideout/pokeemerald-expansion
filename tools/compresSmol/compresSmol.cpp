73a031723e360ff750514f3b1f31628c00af81e8
Hedara <hedara90@gmail.com>
2025-06-06 13:28:33 +0200

    Works for length = 2

--- tools/compresSmol/compresSmol.cpp
+++ tools/compresSmol/compresSmol.cpp
@@ -18,7 +18,6 @@ bool isNumber(std::string input)
 }
 
 enum Option {
-    ANALYZE,
     WRITE,
     FRAME_WRITE,
     DECODE,
@@ -31,15 +30,12 @@ int main(int argc, char *argv[])
     bool printUsage = false;
     std::string input;
     std::string output;
-    int numThreads = 1;
     InputSettings settings(true, true, true);
 
     if (argc > 1)
     {
         std::string argument = argv[1];
-        if (argument.compare("-a") == 0)
-            option = ANALYZE;
-        else if (argument.compare("-w") == 0)
+        if (argument.compare("-w") == 0)
             option = WRITE;
         else if (argument.compare("-fw") == 0)
             option = FRAME_WRITE;
@@ -48,46 +44,12 @@ int main(int argc, char *argv[])
     }
     switch (option)
     {
-        case ANALYZE:
-            if (argc > 2)
-                input = argv[2];
-            else
-                printUsage = true;
-            if (argc > 4)
-            {
-                std::string arg2 = argv[3];
-                std::string arg2arg = argv[4];
-                if (arg2.compare("-t") == 0 && isNumber(arg2arg))
-                    numThreads = std::stoi(arg2arg.c_str());
-            }
-            if (argc > 7)
-            {
-                std::string setting1 = argv[5];
-                std::string setting2 = argv[6];
-                std::string setting3 = argv[7];
-                if (setting1.compare("true") == 0)
-                    settings.canEncodeLO = true;
-                else if (setting1.compare("false") == 0)
-                    settings.canEncodeLO = false;
-                else
-                    fprintf(stderr, "Unrecognized setting1 \"%s\", defaulting to \"true\"\n", setting1.c_str());
-                if (setting2.compare("true") == 0)
-                    settings.canEncodeSyms = true;
-                else if (setting2.compare("false") == 0)
-                    settings.canEncodeSyms = false;
-                else
-                    fprintf(stderr, "Unrecognized setting2 \"%s\", defaulting to \"true\"\n", setting2.c_str());
-                if (setting3.compare("true") == 0)
-                    settings.canDeltaSyms = true;
-                else if (setting3.compare("false") == 0)
-                    settings.canDeltaSyms = false;
-                else
-                    fprintf(stderr, "Unrecognized setting3 \"%s\", defaulting to \"true\"\n", setting3.c_str());
-            }
-            break;
         case FRAME_WRITE:
+            //  Not implemented yet
+            fprintf(stderr, "Frame writing isn't implemented yet\n");
             settings.useFrames = true;
             option = WRITE;
+            return 1;
         case WRITE:
             if (argc > 3)
             {
@@ -158,59 +120,6 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    if (option == ANALYZE)
-    {
-        std::filesystem::path dirPath = input;
-        FileDispatcher dispatcher(dirPath);
-        if (!dispatcher.initFileList())
-        {
-            fprintf(stderr, "Failed to init file list\n");
-            return 1;
-        }
-        std::mutex dispatchMutex;
-        std::vector<CompressedImage> allImages;
-        std::mutex imageMutex;
-
-        settings.shouldCompare = true;
-
-        std::vector<std::thread> threads;
-        for (int i = 0; i < numThreads; i++)
-        {
-            threads.emplace_back(analyzeImages, &allImages, &imageMutex,
-                                                &dispatcher, &dispatchMutex,
-                                                settings);
-        }
-
-        for (int i = 0; i < numThreads; i++)
-            threads[i].join();
-
-        size_t lzSizes = 0;
-        size_t newSizes = 0;
-        size_t rawSizes = 0;
-        size_t totalImages = 0;
-        size_t invalidImages = 0;
-        for (CompressedImage currImage : allImages)
-        {
-            totalImages++;
-            if (currImage.isValid)
-            {
-                lzSizes += currImage.lzSize;
-                newSizes += currImage.compressedSize;
-                rawSizes += currImage.rawNumBytes;
-            }
-            else
-            {
-                fprintf(stderr, "Failed to solve %s\n", currImage.fileName.c_str());
-                invalidImages++;
-            }
-        }
-
-        fprintf(stderr, "RawSize: %zu\n", rawSizes);
-        fprintf(stderr, "LZsize: %zu\n", lzSizes);
-        fprintf(stderr, "SmolSize: %zu\n", newSizes);
-        fprintf(stderr, "Total Images: %zu\n", totalImages);
-        fprintf(stderr, "Invalid Images: %zu\n", invalidImages);
-    }
     if (option == WRITE)
     {
         if (std::filesystem::exists(input))
@@ -220,22 +129,23 @@ int main(int argc, char *argv[])
                 image = processImageFrames(input, settings);
             else
                 image = processImage(input, settings);
-            if (image.isValid)
-            {
-                std::ofstream fileOut(output.c_str(), std::ios::out | std::ios::binary);
-                fileOut.write(reinterpret_cast<const char *>(image.writeVec.data()), image.writeVec.size()*4);
-                fileOut.close();
-            }
-            else
-            {
-                fprintf(stderr, "Failed to compress image %s\n", image.fileName.c_str());
-            }
+            //if (image.isValid)
+            //{
+            //    std::ofstream fileOut(output.c_str(), std::ios::out | std::ios::binary);
+            //    fileOut.write(reinterpret_cast<const char *>(image.writeVec.data()), image.writeVec.size()*4);
+            //    fileOut.close();
+            //}
+            //else
+            //{
+            //    fprintf(stderr, "Failed to compress image %s\n", image.fileName.c_str());
+            //}
         }
         else
         {
             fprintf(stderr, "Input file %s doesn't exist\n", input.c_str());
         }
     }
+    /*
     if (option == DECODE)
     {
         if (std::filesystem::exists(input))
@@ -257,6 +167,7 @@ int main(int argc, char *argv[])
             fprintf(stderr, "Input file %s doesn't exist\n", input.c_str());
         }
     }
+    */
 
     return 0;
 }
