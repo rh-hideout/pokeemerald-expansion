cfmnephrite/pokeemerald

Replaced all instances of STATUS2 in code and status2animaion
To do: Baton Pass, GetItemStatus2Mask

data/battle_scripts_1.s
@@ -4188,7 +4188,7 @@ BattleScript_TryDestinyKnotTarget:
 	infatuatewithbattler BS_TARGET, BS_ATTACKER
 	playanimation BS_ATTACKER, B_ANIM_HELD_ITEM_EFFECT
 	waitanimation
-	status2animation BS_TARGET, STATUS2_INFATUATION
+	volatilestatusanimation BS_TARGET, VOLATILE_STATUS_INFATUATION
 	waitanimation
 	printstring STRINGID_DESTINYKNOTACTIVATES
 	waitmessage B_WAIT_TIME_LONG
@@ -4200,7 +4200,7 @@ BattleScript_TryDestinyKnotAttacker:
 	infatuatewithbattler BS_ATTACKER, BS_TARGET
 	playanimation BS_TARGET, B_ANIM_HELD_ITEM_EFFECT
 	waitanimation
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_INFATUATION
 	waitanimation
 	printstring STRINGID_DESTINYKNOTACTIVATES
 	waitmessage B_WAIT_TIME_LONG
@@ -7148,7 +7148,7 @@ BattleScript_PrintUproarOverTurns::
 	end2
 
 BattleScript_ThrashConfuses::
-	chosenstatus2animation BS_ATTACKER, STATUS2_CONFUSION
+	chosenvolatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_CONFUSION
 	printstring STRINGID_PKMNFATIGUECONFUSION
 	waitmessage B_WAIT_TIME_LONG
 	end2
@@ -7156,7 +7156,7 @@ BattleScript_ThrashConfuses::
 BattleScript_MoveUsedIsConfused::
 	printstring STRINGID_PKMNISCONFUSED
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_CONFUSION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_CONFUSION
 	jumpifbyte CMP_EQUAL, cMULTISTRING_CHOOSER, FALSE, BattleScript_MoveUsedIsConfusedRet
 BattleScript_DoSelfConfusionDmg::
 	cancelmultiturnmoves BS_ATTACKER
@@ -7182,7 +7182,7 @@ BattleScript_MoveUsedPowder::
 	ppreduce
 	pause B_WAIT_TIME_SHORT
 	cancelmultiturnmoves BS_ATTACKER
-	status2animation BS_ATTACKER, STATUS2_POWDER
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_POWDER
 	waitanimation
 	effectivenesssound
 	hitanimation BS_ATTACKER
@@ -7220,7 +7220,7 @@ BattleScript_WrapEnds::
 BattleScript_MoveUsedIsInLove::
 	printstring STRINGID_PKMNINLOVE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_INFATUATION
 	return
 
 BattleScript_MoveUsedIsInLoveCantAttack::
@@ -7231,13 +7231,13 @@ BattleScript_MoveUsedIsInLoveCantAttack::
 BattleScript_NightmareTurnDmg::
 	printstring STRINGID_PKMNLOCKEDINNIGHTMARE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_NIGHTMARE
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_NIGHTMARE
 	goto BattleScript_DoTurnDmg
 
 BattleScript_CurseTurnDmg::
 	printstring STRINGID_PKMNAFFLICTEDBYCURSE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_CURSED
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_CURSED
 	goto BattleScript_DoTurnDmg
 
 BattleScript_TargetPRLZHeal::
@@ -7367,7 +7367,7 @@ BattleScript_MoveEffectWrap::
 	return
 
 BattleScript_MoveEffectConfusion::
-	chosenstatus2animation BS_EFFECT_BATTLER, STATUS2_CONFUSION
+	chosenvolatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_STATUS_CONFUSION
 	printstring STRINGID_PKMNWASCONFUSED
 	waitmessage B_WAIT_TIME_LONG
 	return
@@ -8514,7 +8514,7 @@ BattleScript_BanefulBunkerEffect::
 
 BattleScript_CuteCharmActivates::
 	call BattleScript_AbilityPopUp
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_STATUS_INFATUATION
 	printstring STRINGID_PKMNSXINFATUATEDY
 	waitmessage B_WAIT_TIME_LONG
 	call BattleScript_TryDestinyKnotTarget
@@ -9795,7 +9795,7 @@ BattleScript_EffectConfuseSide::
 BattleScript_ConfuseSideLoop:
 	jumpifabsent BS_TARGET, BattleScript_ConfuseSideIncrement
 	trysetconfusion BattleScript_ConfuseSideIncrement
-	status2animation BS_EFFECT_BATTLER, STATUS2_CONFUSION
+	volatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_STATUS_CONFUSION
 BattleScript_ConfuseSidePrintMessage:
 	printfromtable gStatus2StringIds
 	waitmessage B_WAIT_TIME_LONG
@@ -9817,7 +9817,7 @@ BattleScript_EffectInfatuateSide::
 BattleScript_InfatuateSideLoop:
 	jumpifabsent BS_TARGET, BattleScript_InfatuateSideIncrement
 	trysetinfatuation BattleScript_InfatuateSideIncrement
-	status2animation BS_EFFECT_BATTLER, STATUS2_INFATUATION
+	volatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_STATUS_INFATUATION
 BattleScript_InfatuateSidePrintMessage:
 	printfromtable gStatus2StringIds
 	waitmessage B_WAIT_TIME_LONG

src/battle_dynamax.c
@@ -181,14 +181,14 @@ void ActivateDynamax(u32 battler)
     gBattleStruct->dynamax.dynamaxTurns[battler] = gBattleTurnCounter + DYNAMAX_TURNS_COUNT;
 
     // Substitute is removed upon Dynamaxing.
-    gBattleMons[battler].status2 &= ~STATUS2_SUBSTITUTE;
+    gBattleMons[battler].volatileStatuses.substitute = FALSE;
     ClearBehindSubstituteBit(battler);
 
     // Choiced Moves are reset upon Dynamaxing.
     gBattleStruct->choicedMove[battler] = MOVE_NONE;
 
     // Try Gigantamax form change.
-    if (!(gBattleMons[battler].status2 & STATUS2_TRANSFORMED)) // Ditto cannot Gigantamax.
+    if (!(gBattleMons[battler].volatileStatuses.transformed)) // Ditto cannot Gigantamax.
         TryBattleFormChange(battler, FORM_CHANGE_BATTLE_GIGANTAMAX);
 
     BattleScriptExecute(BattleScript_DynamaxBegins);

src/battle_gfx_sfx_util.c
@@ -478,13 +478,13 @@ void InitAndLaunchChosenStatusAnimation(u32 battler, bool32 isStatus2, u32 statu
     }
     else
     {
-        if (status & STATUS2_INFATUATION)
+        if (status == VOLATILE_STATUS_INFATUATION)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_INFATUATION);
-        else if (status & STATUS2_CONFUSION)
+        else if (status == VOLATILE_STATUS_CONFUSION)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_CONFUSION);
-        else if (status & STATUS2_CURSED)
+        else if (status == VOLATILE_STATUS_CURSED)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_CURSED);
-        else if (status & STATUS2_NIGHTMARE)
+        else if (status == VOLATILE_STATUS_NIGHTMARE)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_NIGHTMARE);
         else // no animation
             gBattleSpritesDataPtr->healthBoxesData[battler].statusAnimActive = 0;

src/battle_z_move.c
@@ -486,9 +486,9 @@ void SetZEffect(void)
         break;
     }
     case Z_EFFECT_BOOST_CRITS:
-        if (!(gBattleMons[gBattlerAttacker].status2 & STATUS2_FOCUS_ENERGY_ANY))
+        if (!(gBattleMons[gBattlerAttacker].volatileStatuses.dragonCheer || gBattleMons[gBattlerAttacker].volatileStatuses.focusEnergy))
         {
-            gBattleMons[gBattlerAttacker].status2 |= STATUS2_FOCUS_ENERGY;
+            gBattleMons[gBattlerAttacker].volatileStatuses.focusEnergy = TRUE;
             gBattleCommunication[MULTISTRING_CHOOSER] = B_MSG_Z_BOOST_CRITS;
             BattleScriptPush(gBattlescriptCurrInstr + Z_EFFECT_BS_LENGTH);
             gBattlescriptCurrInstr = BattleScript_ZEffectPrintString;

src/item_use.c
@@ -1225,7 +1225,7 @@ bool32 CannotUseItemsInBattle(u16 itemId, struct Pokemon *mon)
             cannotUse = TRUE;
         break;
     case EFFECT_ITEM_SET_FOCUS_ENERGY:
-        if (gBattleMons[gBattlerInMenuId].status2 & STATUS2_FOCUS_ENERGY_ANY)
+        if (gBattleMons[gBattlerInMenuId].volatileStatuses.dragonCheer || gBattleMons[gBattlerInMenuId].volatileStatuses.focusEnergy)
             cannotUse = TRUE;
         break;
     case EFFECT_ITEM_SET_MIST:

test/battle/ability/shield_dust.c
@@ -36,7 +36,7 @@ SINGLE_BATTLE_TEST("Shield Dust blocks secondary effects")
             MESSAGE("The opposing Vivillon was prevented from healing!");
         }
     } THEN { // Can't find good way to test trapping
-        EXPECT(!(opponent->status2 & STATUS2_ESCAPE_PREVENTION));
+        EXPECT(!opponent->volatileStatuses.escapePrevention);
     }
 }
 
@@ -78,8 +78,8 @@ SINGLE_BATTLE_TEST("Shield Dust does not block primary effects")
         }
     } THEN { // Can't find good way to test trapping
         if (move == MOVE_JAW_LOCK) {
-            EXPECT(opponent->status2 & STATUS2_ESCAPE_PREVENTION);
-            EXPECT(player->status2 & STATUS2_ESCAPE_PREVENTION);
+            EXPECT(opponent->volatileStatuses.escapePrevention);
+            EXPECT(player->volatileStatuses.escapePrevention);
         }
     }
 }

test/battle/move_effect/attract.c
@@ -19,7 +19,7 @@ SINGLE_BATTLE_TEST("Attract causes the target to become infatuated with the user
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Nidoking fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatileStatuses.infatuation);
     }
 }
 
@@ -35,7 +35,7 @@ SINGLE_BATTLE_TEST("Attract ignores type immunity")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Misdreavus fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatileStatuses.infatuation);
     }
 }
 
@@ -51,7 +51,7 @@ SINGLE_BATTLE_TEST("Attract bypasses Substitute")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Nidoking fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatileStatuses.infatuation);
     }
 }
 
@@ -69,7 +69,7 @@ SINGLE_BATTLE_TEST("Attract fails if the target is already infatuated")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatileStatuses.infatuation);
     }
 }
 
@@ -84,7 +84,7 @@ SINGLE_BATTLE_TEST("Attract fails when used on a Pokémon of the same gender")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(!(opponent->status2 & STATUS2_INFATUATION));
+        EXPECT(!(opponent->volatileStatuses.infatuation));
     }
 }
 
@@ -100,6 +100,6 @@ SINGLE_BATTLE_TEST("Attract fails when used on a genderless Pokémon")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(!(opponent->status2 & STATUS2_INFATUATION));
+        EXPECT(!(opponent->volatileStatuses.infatuation));
     }
 }

rh-hideout/pokeemerald-expansion

data/battle_scripts_1.s
@@ -4178,7 +4178,7 @@ BattleScript_TryDestinyKnotTarget:
 	infatuatewithbattler BS_TARGET, BS_ATTACKER
 	playanimation BS_ATTACKER, B_ANIM_HELD_ITEM_EFFECT
 	waitanimation
-	status2animation BS_TARGET, STATUS2_INFATUATION
+	volatilestatusanimation BS_TARGET, VOLATILE_INFATUATION
 	waitanimation
 	printstring STRINGID_DESTINYKNOTACTIVATES
 	waitmessage B_WAIT_TIME_LONG
@@ -4190,7 +4190,7 @@ BattleScript_TryDestinyKnotAttacker:
 	infatuatewithbattler BS_ATTACKER, BS_TARGET
 	playanimation BS_TARGET, B_ANIM_HELD_ITEM_EFFECT
 	waitanimation
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_INFATUATION
 	waitanimation
 	printstring STRINGID_DESTINYKNOTACTIVATES
 	waitmessage B_WAIT_TIME_LONG
@@ -7138,7 +7138,7 @@ BattleScript_PrintUproarOverTurns::
 	end2
 
 BattleScript_ThrashConfuses::
-	chosenstatus2animation BS_ATTACKER, STATUS2_CONFUSION
+	chosenvolatilestatusanimation BS_ATTACKER, VOLATILE_CONFUSION
 	printstring STRINGID_PKMNFATIGUECONFUSION
 	waitmessage B_WAIT_TIME_LONG
 	end2
@@ -7146,7 +7146,7 @@ BattleScript_ThrashConfuses::
 BattleScript_MoveUsedIsConfused::
 	printstring STRINGID_PKMNISCONFUSED
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_CONFUSION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_CONFUSION
 	jumpifbyte CMP_EQUAL, cMULTISTRING_CHOOSER, FALSE, BattleScript_MoveUsedIsConfusedRet
 BattleScript_DoSelfConfusionDmg::
 	cancelmultiturnmoves BS_ATTACKER
@@ -7172,7 +7172,7 @@ BattleScript_MoveUsedPowder::
 	ppreduce
 	pause B_WAIT_TIME_SHORT
 	cancelmultiturnmoves BS_ATTACKER
-	status2animation BS_ATTACKER, STATUS2_POWDER
+	volatilestatusanimation BS_ATTACKER, VOLATILE_POWDER
 	waitanimation
 	effectivenesssound
 	hitanimation BS_ATTACKER
@@ -7210,7 +7210,7 @@ BattleScript_WrapEnds::
 BattleScript_MoveUsedIsInLove::
 	printstring STRINGID_PKMNINLOVE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_INFATUATION
 	return
 
 BattleScript_MoveUsedIsInLoveCantAttack::
@@ -7221,13 +7221,13 @@ BattleScript_MoveUsedIsInLoveCantAttack::
 BattleScript_NightmareTurnDmg::
 	printstring STRINGID_PKMNLOCKEDINNIGHTMARE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_NIGHTMARE
+	volatilestatusanimation BS_ATTACKER, VOLATILE_NIGHTMARE
 	goto BattleScript_DoTurnDmg
 
 BattleScript_CurseTurnDmg::
 	printstring STRINGID_PKMNAFFLICTEDBYCURSE
 	waitmessage B_WAIT_TIME_LONG
-	status2animation BS_ATTACKER, STATUS2_CURSED
+	volatilestatusanimation BS_ATTACKER, VOLATILE_CURSED
 	goto BattleScript_DoTurnDmg
 
 BattleScript_TargetPRLZHeal::
@@ -7357,7 +7357,7 @@ BattleScript_MoveEffectWrap::
 	return
 
 BattleScript_MoveEffectConfusion::
-	chosenstatus2animation BS_EFFECT_BATTLER, STATUS2_CONFUSION
+	chosenvolatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_CONFUSION
 	printstring STRINGID_PKMNWASCONFUSED
 	waitmessage B_WAIT_TIME_LONG
 	return
@@ -8504,7 +8504,7 @@ BattleScript_BanefulBunkerEffect::
 
 BattleScript_CuteCharmActivates::
 	call BattleScript_AbilityPopUp
-	status2animation BS_ATTACKER, STATUS2_INFATUATION
+	volatilestatusanimation BS_ATTACKER, VOLATILE_INFATUATION
 	printstring STRINGID_PKMNSXINFATUATEDY
 	waitmessage B_WAIT_TIME_LONG
 	call BattleScript_TryDestinyKnotTarget
@@ -9785,7 +9785,7 @@ BattleScript_EffectConfuseSide::
 BattleScript_ConfuseSideLoop:
 	jumpifabsent BS_TARGET, BattleScript_ConfuseSideIncrement
 	trysetconfusion BattleScript_ConfuseSideIncrement
-	status2animation BS_EFFECT_BATTLER, STATUS2_CONFUSION
+	volatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_CONFUSION
 BattleScript_ConfuseSidePrintMessage:
 	printfromtable gStatus2StringIds
 	waitmessage B_WAIT_TIME_LONG
@@ -9807,7 +9807,7 @@ BattleScript_EffectInfatuateSide::
 BattleScript_InfatuateSideLoop:
 	jumpifabsent BS_TARGET, BattleScript_InfatuateSideIncrement
 	trysetinfatuation BattleScript_InfatuateSideIncrement
-	status2animation BS_EFFECT_BATTLER, STATUS2_INFATUATION
+	volatilestatusanimation BS_EFFECT_BATTLER, VOLATILE_INFATUATION
 BattleScript_InfatuateSidePrintMessage:
 	printfromtable gStatus2StringIds
 	waitmessage B_WAIT_TIME_LONG

src/battle_dynamax.c
@@ -181,14 +181,14 @@ void ActivateDynamax(u32 battler)
     gBattleStruct->dynamax.dynamaxTurns[battler] = gBattleTurnCounter + DYNAMAX_TURNS_COUNT;
 
     // Substitute is removed upon Dynamaxing.
-    gBattleMons[battler].status2 &= ~STATUS2_SUBSTITUTE;
+    gBattleMons[battler].volatiles.substitute = FALSE;
     ClearBehindSubstituteBit(battler);
 
     // Choiced Moves are reset upon Dynamaxing.
     gBattleStruct->choicedMove[battler] = MOVE_NONE;
 
     // Try Gigantamax form change.
-    if (!(gBattleMons[battler].status2 & STATUS2_TRANSFORMED)) // Ditto cannot Gigantamax.
+    if (!(gBattleMons[battler].volatiles.transformed)) // Ditto cannot Gigantamax.
         TryBattleFormChange(battler, FORM_CHANGE_BATTLE_GIGANTAMAX);
 
     BattleScriptExecute(BattleScript_DynamaxBegins);

src/battle_gfx_sfx_util.c
@@ -478,13 +478,13 @@ void InitAndLaunchChosenStatusAnimation(u32 battler, bool32 isStatus2, u32 statu
     }
     else
     {
-        if (status & STATUS2_INFATUATION)
+        if (status == VOLATILE_INFATUATION)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_INFATUATION);
-        else if (status & STATUS2_CONFUSION)
+        else if (status == VOLATILE_CONFUSION)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_CONFUSION);
-        else if (status & STATUS2_CURSED)
+        else if (status == VOLATILE_CURSED)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_CURSED);
-        else if (status & STATUS2_NIGHTMARE)
+        else if (status == VOLATILE_NIGHTMARE)
             LaunchStatusAnimation(battler, B_ANIM_STATUS_NIGHTMARE);
         else // no animation
             gBattleSpritesDataPtr->healthBoxesData[battler].statusAnimActive = 0;

src/battle_z_move.c
@@ -486,9 +486,9 @@ void SetZEffect(void)
         break;
     }
     case Z_EFFECT_BOOST_CRITS:
-        if (!(gBattleMons[gBattlerAttacker].status2 & STATUS2_FOCUS_ENERGY_ANY))
+        if (!(gBattleMons[gBattlerAttacker].volatiles.dragonCheer || gBattleMons[gBattlerAttacker].volatiles.focusEnergy))
         {
-            gBattleMons[gBattlerAttacker].status2 |= STATUS2_FOCUS_ENERGY;
+            gBattleMons[gBattlerAttacker].volatiles.focusEnergy = TRUE;
             gBattleCommunication[MULTISTRING_CHOOSER] = B_MSG_Z_BOOST_CRITS;
             BattleScriptPush(gBattlescriptCurrInstr + Z_EFFECT_BS_LENGTH);
             gBattlescriptCurrInstr = BattleScript_ZEffectPrintString;

src/item_use.c
@@ -1224,7 +1224,7 @@ bool32 CannotUseItemsInBattle(u16 itemId, struct Pokemon *mon)
             cannotUse = TRUE;
         break;
     case EFFECT_ITEM_SET_FOCUS_ENERGY:
-        if (gBattleMons[gBattlerInMenuId].status2 & STATUS2_FOCUS_ENERGY_ANY)
+        if (gBattleMons[gBattlerInMenuId].volatiles.dragonCheer || gBattleMons[gBattlerInMenuId].volatiles.focusEnergy)
             cannotUse = TRUE;
         break;
     case EFFECT_ITEM_SET_MIST:

test/battle/ability/shield_dust.c
@@ -36,7 +36,7 @@ SINGLE_BATTLE_TEST("Shield Dust blocks secondary effects")
             MESSAGE("The opposing Vivillon was prevented from healing!");
         }
     } THEN { // Can't find good way to test trapping
-        EXPECT(!(opponent->status2 & STATUS2_ESCAPE_PREVENTION));
+        EXPECT(!opponent->volatiles.escapePrevention);
     }
 }
 
@@ -78,8 +78,8 @@ SINGLE_BATTLE_TEST("Shield Dust does not block primary effects")
         }
     } THEN { // Can't find good way to test trapping
         if (move == MOVE_JAW_LOCK) {
-            EXPECT(opponent->status2 & STATUS2_ESCAPE_PREVENTION);
-            EXPECT(player->status2 & STATUS2_ESCAPE_PREVENTION);
+            EXPECT(opponent->volatiles.escapePrevention);
+            EXPECT(player->volatiles.escapePrevention);
         }
     }
 }

test/battle/move_effect/attract.c
@@ -19,7 +19,7 @@ SINGLE_BATTLE_TEST("Attract causes the target to become infatuated with the user
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Nidoking fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatiles.infatuation);
     }
 }
 
@@ -35,7 +35,7 @@ SINGLE_BATTLE_TEST("Attract ignores type immunity")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Misdreavus fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatiles.infatuation);
     }
 }
 
@@ -51,7 +51,7 @@ SINGLE_BATTLE_TEST("Attract bypasses Substitute")
         ANIMATION(ANIM_TYPE_MOVE, MOVE_ATTRACT, player);
         MESSAGE("The opposing Nidoking fell in love!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatiles.infatuation);
     }
 }
 
@@ -69,7 +69,7 @@ SINGLE_BATTLE_TEST("Attract fails if the target is already infatuated")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(opponent->status2 & STATUS2_INFATUATION);
+        EXPECT(opponent->volatiles.infatuation);
     }
 }
 
@@ -84,7 +84,7 @@ SINGLE_BATTLE_TEST("Attract fails when used on a Pokémon of the same gender")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(!(opponent->status2 & STATUS2_INFATUATION));
+        EXPECT(!(opponent->volatiles.infatuation));
     }
 }
 
@@ -100,6 +100,6 @@ SINGLE_BATTLE_TEST("Attract fails when used on a genderless Pokémon")
         MESSAGE("Nidoqueen used Attract!");
         MESSAGE("But it failed!");
     } THEN {
-        EXPECT(!(opponent->status2 & STATUS2_INFATUATION));
+        EXPECT(!(opponent->volatiles.infatuation));
     }
 }