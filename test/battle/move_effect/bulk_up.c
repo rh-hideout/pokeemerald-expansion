1e1fa450abbea6e151bc5c115cc3ba142245176b
Eduardo Quezada <eduardo602002@gmail.com>
2025-06-09 21:19:16 -0400

    Wrote some missing tests

--- test/battle/move_effect/bulk_up.c
+++ test/battle/move_effect/bulk_up.c
@@ -1,4 +1,21 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("Bulk Up increases the user's Attack and Defense");
+ASSUMPTIONS
+{
+    ASSUME(GetMoveEffect(MOVE_BULK_UP) == EFFECT_BULK_UP);
+}
+
+SINGLE_BATTLE_TEST("Bulk Up increases the user's Attack and Defense by 1 stage each")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_BULK_UP); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_BULK_UP, player);
+        MESSAGE("Wobbuffet's Attack rose!");
+        MESSAGE("Wobbuffet's Defense rose!");
+    }
+}
--- test/battle/move_effect/calm_mind.c
+++ test/battle/move_effect/calm_mind.c
@@ -1,4 +1,21 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("Calm Mind increases the user's Sp. Attack and Sp. Defense by 1 stage each");
+ASSUMPTIONS
+{
+    ASSUME(GetMoveEffect(MOVE_CALM_MIND) == EFFECT_CALM_MIND);
+}
+
+SINGLE_BATTLE_TEST("Calm Mind increases the user's Sp. Attack and Sp. Defense by 1 stage each")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_CALM_MIND); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CALM_MIND, player);
+        MESSAGE("Wobbuffet's Sp. Atk rose!");
+        MESSAGE("Wobbuffet's Sp. Def rose!");
+    }
+}
