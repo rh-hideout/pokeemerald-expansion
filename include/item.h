cfmnephrite/pokeemerald

Remove remaining STATUS2 references (other than Baton Pass
To do: Baton Pass

include/item.h
@@ -2,6 +2,7 @@
 #define GUARD_ITEM_H
 
 #include "constants/item.h"
+#include "constants/item_effects.h"
 #include "constants/items.h"
 #include "constants/tms_hms.h"
 
@@ -83,7 +84,7 @@ u8 GetItemBattleUsage(u16 itemId);
 u32 GetItemSecondaryId(u32 itemId);
 u32 GetItemFlingPower(u32 itemId);
 u32 GetItemStatus1Mask(u16 itemId);
-u32 GetItemStatus2Mask(u16 itemId);
+bool32 ItemHealsVolatileStatus(u16 itemId, enum VolatileStatus volatileStatus);
 
 /* Expands to:
  * enum

src/battle_script_commands.c
@@ -16986,26 +16986,15 @@ void BS_ItemCureStatus(void)
 {
     NATIVE_ARGS(const u8 *noStatusInstr);
     u32 battler = gBattlerAttacker;
-    u32 previousStatus2 = 0;
     bool32 statusChanged = FALSE;
     struct Pokemon *party = GetBattlerParty(gBattlerAttacker);
 
     // Heal Status2 conditions if battler is active.
     if (gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[gBattlerAttacker])
-    {
-        previousStatus2 = gBattleMons[battler].status2;
-        gBattleMons[gBattlerAttacker].status2 &= ~GetItemStatus2Mask(gLastUsedItem);
-    }
+        statusChanged = ItemHealMonVolatileStatus(battler, gLastUsedItem);
     else if (IsDoubleBattle()
-                && gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[BATTLE_PARTNER(gBattlerAttacker)])
-    {
-        battler = BATTLE_PARTNER(gBattlerAttacker);
-        previousStatus2 = gBattleMons[battler].status2;
-        gBattleMons[battler].status2 &= ~GetItemStatus2Mask(gLastUsedItem);
-    }
-
-    if (previousStatus2 != gBattleMons[battler].status2)
-        statusChanged = TRUE;
+     && gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[BATTLE_PARTNER(gBattlerAttacker)])
+        statusChanged = ItemHealMonVolatileStatus(BATTLE_PARTNER(gBattlerAttacker), gLastUsedItem);
 
     // Heal Status1 conditions.
     if (!HealStatusConditions(&party[gBattleStruct->itemPartyIndex[gBattlerAttacker]], GetItemStatus1Mask(gLastUsedItem), battler))
@@ -17013,7 +17002,7 @@ void BS_ItemCureStatus(void)
         statusChanged = TRUE;
         if (GetItemStatus1Mask(gLastUsedItem) & STATUS1_SLEEP)
             gBattleMons[battler].volatileStatuses.nightmare = FALSE;
-        if (GetItemStatus2Mask(gLastUsedItem) & STATUS2_CONFUSION)
+        if (ItemHealsVolatileStatus(gLastUsedItem, VOLATILE_STATUS_CONFUSION))
             gStatuses4[battler] &= ~STATUS4_INFINITE_CONFUSION;
     }
 

src/item_use.c
@@ -1192,12 +1192,25 @@ void ItemUseInBattle_PartyMenuChooseMove(u8 taskId)
     ItemUseInBattle_ShowPartyMenu(taskId);
 }
 
+static bool32 ItemHealsMonVolatileStatus(u32 battler, u16 itemId)
+{
+    const u8 *effect = GetItemEffect(itemId);
+    if (effect[3] & ITEM3_STATUS_ALL)
+        return (gBattleMons[battler].volatileStatuses.infatuation || gBattleMons[battler].volatileStatuses.confusionTurns > 0);
+    else if (effect[0] & ITEM0_INFATUATION)
+        return gBattleMons[battler].volatileStatuses.infatuation;
+    else if (effect[3] & ITEM3_CONFUSION)
+        return gBattleMons[battler].volatileStatuses.confusionTurns > 0;
+
+    return FALSE;
+}
+
 static bool32 SelectedMonHasStatus2(u16 itemId)
 {
     if (gPartyMenu.slotId == 0)
-        return gBattleMons[0].status2 & GetItemStatus2Mask(itemId);
+        return ItemHealsMonVolatileStatus(0, itemId);
     else if (gBattleTypeFlags & (BATTLE_TYPE_DOUBLE | BATTLE_TYPE_MULTI) && gPartyMenu.slotId == 1)
-        return gBattleMons[2].status2 & GetItemStatus2Mask(itemId);
+        return ItemHealsMonVolatileStatus(2, itemId);
     return FALSE;
 }
 

rh-hideout/pokeemerald-expansion

include/item.h
@@ -2,6 +2,7 @@
 #define GUARD_ITEM_H
 
 #include "constants/item.h"
+#include "constants/item_effects.h"
 #include "constants/items.h"
 #include "constants/tms_hms.h"
 
@@ -83,7 +84,7 @@ u8 GetItemBattleUsage(u16 itemId);
 u32 GetItemSecondaryId(u32 itemId);
 u32 GetItemFlingPower(u32 itemId);
 u32 GetItemStatus1Mask(u16 itemId);
-u32 GetItemStatus2Mask(u16 itemId);
+bool32 ItemHasVolatileFlag(u16 itemId, enum Volatile volatileStatus);
 
 /* Expands to:
  * enum

src/battle_script_commands.c
@@ -16978,26 +16978,15 @@ void BS_ItemCureStatus(void)
 {
     NATIVE_ARGS(const u8 *noStatusInstr);
     u32 battler = gBattlerAttacker;
-    u32 previousStatus2 = 0;
     bool32 statusChanged = FALSE;
     struct Pokemon *party = GetBattlerParty(gBattlerAttacker);
 
     // Heal Status2 conditions if battler is active.
     if (gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[gBattlerAttacker])
-    {
-        previousStatus2 = gBattleMons[battler].status2;
-        gBattleMons[gBattlerAttacker].status2 &= ~GetItemStatus2Mask(gLastUsedItem);
-    }
+        statusChanged = ItemHealMonVolatileStatus(battler, gLastUsedItem);
     else if (IsDoubleBattle()
-                && gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[BATTLE_PARTNER(gBattlerAttacker)])
-    {
-        battler = BATTLE_PARTNER(gBattlerAttacker);
-        previousStatus2 = gBattleMons[battler].status2;
-        gBattleMons[battler].status2 &= ~GetItemStatus2Mask(gLastUsedItem);
-    }
-
-    if (previousStatus2 != gBattleMons[battler].status2)
-        statusChanged = TRUE;
+     && gBattleStruct->itemPartyIndex[gBattlerAttacker] == gBattlerPartyIndexes[BATTLE_PARTNER(gBattlerAttacker)])
+        statusChanged = ItemHealMonVolatileStatus(BATTLE_PARTNER(gBattlerAttacker), gLastUsedItem);
 
     // Heal Status1 conditions.
     if (!HealStatusConditions(&party[gBattleStruct->itemPartyIndex[gBattlerAttacker]], GetItemStatus1Mask(gLastUsedItem), battler))
@@ -17005,7 +16994,7 @@ void BS_ItemCureStatus(void)
         statusChanged = TRUE;
         if (GetItemStatus1Mask(gLastUsedItem) & STATUS1_SLEEP)
             gBattleMons[battler].volatiles.nightmare = FALSE;
-        if (GetItemStatus2Mask(gLastUsedItem) & STATUS2_CONFUSION)
+        if (ItemHasVolatileFlag(gLastUsedItem, VOLATILE_CONFUSION))
             gStatuses4[battler] &= ~STATUS4_INFINITE_CONFUSION;
     }
 

src/item_use.c
@@ -1191,12 +1191,25 @@ void ItemUseInBattle_PartyMenuChooseMove(u8 taskId)
     ItemUseInBattle_ShowPartyMenu(taskId);
 }
 
+static bool32 IteamHealsMonVolatile(u32 battler, u16 itemId)
+{
+    const u8 *effect = GetItemEffect(itemId);
+    if (effect[3] & ITEM3_STATUS_ALL)
+        return (gBattleMons[battler].volatiles.infatuation || gBattleMons[battler].volatiles.confusionTurns > 0);
+    else if (effect[0] & ITEM0_INFATUATION)
+        return gBattleMons[battler].volatiles.infatuation;
+    else if (effect[3] & ITEM3_CONFUSION)
+        return gBattleMons[battler].volatiles.confusionTurns > 0;
+
+    return FALSE;
+}
+
 static bool32 SelectedMonHasStatus2(u16 itemId)
 {
     if (gPartyMenu.slotId == 0)
-        return gBattleMons[0].status2 & GetItemStatus2Mask(itemId);
+        return IteamHealsMonVolatile(0, itemId);
     else if (gBattleTypeFlags & (BATTLE_TYPE_DOUBLE | BATTLE_TYPE_MULTI) && gPartyMenu.slotId == 1)
-        return gBattleMons[2].status2 & GetItemStatus2Mask(itemId);
+        return IteamHealsMonVolatile(2, itemId);
     return FALSE;
 }
 