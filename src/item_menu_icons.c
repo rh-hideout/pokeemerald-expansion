dfa70e314a8dd2a47eec9ed1742a38c5e0342ad3
Nephrite <thechurchofcage@gmail.com>
2025-06-16 14:50:17 +0300

    Fixed pocket order

--- src/item_menu_icons.c
+++ src/item_menu_icons.c
@@ -97,10 +97,10 @@ static const union AnimCmd sSpriteAnim_Bag_Berries[] =
 static const union AnimCmd *const sBagSpriteAnimTable[] =
 {
     [POCKET_ITEMS]      = sSpriteAnim_Bag_Items,
-    [POCKET_KEY_ITEMS]  = sSpriteAnim_Bag_KeyItems,
     [POCKET_POKE_BALLS] = sSpriteAnim_Bag_Pokeballs,
     [POCKET_TM_HM]      = sSpriteAnim_Bag_TMsHMs,
     [POCKET_BERRIES]    = sSpriteAnim_Bag_Berries,
+    [POCKET_KEY_ITEMS]  = sSpriteAnim_Bag_KeyItems,
     [POCKET_NONE]       = sSpriteAnim_Bag_Closed,
 };
 
