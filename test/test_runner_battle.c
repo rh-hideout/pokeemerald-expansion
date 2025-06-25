3f8a48c74079e551a55be4f70e10cf0f8b2a3748
hedara90 <90hedara@gmail.com>
2025-06-25 12:35:32 +0200

    Update test/test_runner_battle.c

--- test/test_runner_battle.c
+++ test/test_runner_battle.c
@@ -1564,7 +1564,8 @@ void OpenPokemon(u32 sourceLine, u32 side, u32 species)
     for (i = 0; i < MAX_MON_MOVES; i++)
         SetMonData(DATA.currentMon, MON_DATA_MOVE1 + i, &data);
     data = 0;
-    if (B_FRIENDSHIP_BOOST) {
+    if (B_FRIENDSHIP_BOOST)
+    {
         // This way, we avoid the boost affecting tests unless explicitly stated.
         SetMonData(DATA.currentMon, MON_DATA_FRIENDSHIP, &data);
         CalculateMonStats(DATA.currentMon);
