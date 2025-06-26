1e48361009f47b72874c56a2a25523f60f2fee32
AlexOn1ine <alexthenotes@gmail.com>
2025-06-26 23:53:50 +0200

    Fixes Scald defrosting target while asleep

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -6951,6 +6951,7 @@ static void Cmd_moveend(void)
                 effect = TRUE;
             }
             if (gBattleMons[gBattlerTarget].status1 & STATUS1_FROSTBITE
+                && IsBattlerTurnDamaged(gBattlerTarget)
                 && IsBattlerAlive(gBattlerTarget)
                 && gBattlerAttacker != gBattlerTarget
                 && MoveThawsUser(originallyUsedMove)
