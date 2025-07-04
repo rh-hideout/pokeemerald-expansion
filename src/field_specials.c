13f07f0fc26f34c3b21c017f275fb3bba1986a2c
AlexOn1ine <alexthenotes@gmail.com>
2025-07-04 15:47:44 +0200

    Add Set Hidden Nature option to the debug menu

--- src/field_specials.c
+++ src/field_specials.c
@@ -4360,3 +4360,10 @@ void GetCodeFeedback(void)
     else
         gSpecialVar_Result = 0;
 }
+
+void SetHiddenNature(void)
+{
+    u32 hiddenNature = gSpecialVar_Result;
+    SetMonData(&gPlayerParty[gSpecialVar_0x8004], MON_DATA_HIDDEN_NATURE, &hiddenNature);
+    CalculateMonStats(&gPlayerParty[gSpecialVar_0x8004]);
+}
