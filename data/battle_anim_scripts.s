bce9b7ba958712b2aa867c30ca642757cc13e1b4
Jonathan Lin <linathanzel@gmail.com>
2025-07-11 19:33:26 -0400

    Start of commit. Fix MOVE_BRUTAL_SWING anim completing before it

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -11420,6 +11420,7 @@ gBattleAnimMove_BrutalSwing::
 	createsprite gSlideMonToOriginalPosSpriteTemplate, ANIM_ATTACKER, 2, ANIM_ATTACKER, 0x0, 0x6
 	waitforvisualfinish
 	restorebg
+	waitbgfadein
 	blendoff
 	clearmonbg ANIM_TARGET
 	end
