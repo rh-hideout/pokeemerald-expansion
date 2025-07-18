bad8c80c2645326122b4f6830113a5b668a05f01
Hedara <hedara90@gmail.com>
2025-07-18 21:54:21 +0200

    Cleanup and fix from upcoming

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -298,7 +298,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
         .makesContact = TRUE,
         .punchingMove = TRUE,
         .additionalEffects = ADDITIONAL_EFFECTS({
-            .moveEffect = EFFECT_CURSE,
+            .moveEffect = MOVE_EFFECT_BURN,
             .chance = 10,
         }),
         .contestEffect = CONTEST_EFFECT_HIGHLY_APPEALING,
