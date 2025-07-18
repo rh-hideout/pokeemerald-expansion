4d835ae8055bec292f06d66a28ae0250f7ed06e5
cawtds <38510667+cawtds@users.noreply.github.com>
2025-07-18 15:26:11 +0200

    start menu flag

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -222,6 +222,7 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
         return TRUE;
     if (input->pressedStartButton)
     {
+        FlagSet(FLAG_OPENED_START_MENU);
         PlaySE(SE_WIN_OPEN);
         ShowStartMenu();
         return TRUE;
