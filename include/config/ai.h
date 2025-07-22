335f4fc68d456cea2b3a0d85fba927f16585075a
Pawkkie <moog272@gmail.com>
2025-07-22 13:49:23 -0400

    Comment clarification

--- include/config/ai.h
+++ include/config/ai.h
@@ -59,7 +59,7 @@
 #define SHOULD_RECOVER_CHANCE                                   50 // Chance the AI will give recovery moves score increase if less than ENABLE_RECOVERY_THRESHOLD and in no immediate danger
 #define ENABLE_RECOVERY_THRESHOLD                               60 // HP percentage beneath which SHOULD_RECOVER_CHANCE is active
 #define SUCKER_PUNCH_CHANCE                                     50 // Chance for the AI to not use Sucker Punch if the player has a status move
-#define SUCKER_PUNCH_PREDICTION_CHANCE                          70 // Chance for the AI to not use Sucker Punch if actively predicting a status move
+#define SUCKER_PUNCH_PREDICTION_CHANCE                          50 // Additional chance for the AI to not use Sucker Punch if actively predicting a status move if SUCKER_PUNCH_CHANCE fails
 
 // AI damage calc considerations
 #define RISKY_AI_CRIT_STAGE_THRESHOLD                           2   // Stat stages at which Risky will assume it gets a crit
