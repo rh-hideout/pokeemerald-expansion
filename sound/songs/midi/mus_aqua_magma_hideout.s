e64ed0e1836c98d2e92bc2f665674e784897ac3a
Josh <32826900+shinydragonhunter@users.noreply.github.com>
2025-07-30 19:43:08 +0100

    Allow names for voicegroups and use a macro for use with drumsets (#2149)
    mid2agb has been modified to allow more than just numbers when naming voicegroups and voicegroups have been renamed to match. A "voice_group" macro has been set up with a optional parameter for the starting note will shift the offset by the specified amount * 0xC which is the length of 1 voice in a voicegroup. All drumset voicegroups have a "starting_note" of 36.
    
    (cherry picked from commit 0dfec24967e94ff047ee210e362ecca0b53656ad)

--- sound/songs/midi/mus_aqua_magma_hideout.s
+++ sound/songs/midi/mus_aqua_magma_hideout.s
@@ -1,6 +1,6 @@
 	.include "MPlayDef.s"
 
-	.equ	mus_aqua_magma_hideout_grp, voicegroup076
+	.equ	mus_aqua_magma_hideout_grp, voicegroup_aqua_magma_hideout
 	.equ	mus_aqua_magma_hideout_pri, 0
 	.equ	mus_aqua_magma_hideout_rev, reverb_set+50
 	.equ	mus_aqua_magma_hideout_mvl, 84
