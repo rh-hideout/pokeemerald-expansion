22380ad9e4d5d71e58ed8dc71a3074a197586f7b
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-08-22 07:52:04 -0700

    fix: rewrote GetTrainerFlagFromScript to fix bug introduced by trainerbattle refactor

--- src/vs_seeker.c
+++ src/vs_seeker.c
@@ -706,21 +706,12 @@ static u8 GetResponseMovementTypeFromTrainerGraphicsId(u8 graphicsId)
 
 static u16 GetTrainerFlagFromScript(const u8 *script)
     /*
- * The trainer flag is a little-endian short located +2 from
- * the script pointer, assuming the trainerbattle command is
- * first in the script.  Because scripts are unaligned, and
- * because the ARM processor requires shorts to be 16-bit
- * aligned, this function needs to perform explicit bitwise
- * operations to get the correct flag.
- *
- * 5c XX YY ZZ ...
- *       -- --
+     * The trainer flag is a located 3 bytes (command + flags + localIdA) from the script pointer, assuming the trainerbattle command is first in the script.
+     * Because scripts are unaligned, and because the ARM processor requires shorts to be 16-bit aligned, this function needs to perform explicit bitwise operations to get the correct flag.
      */
 {
-    u16 trainerFlag;
-
-    script += 2;
-    trainerFlag = script[0];
+    script += 3;
+    u16 trainerFlag = script[0];
     trainerFlag |= script[1] << 8;
     return trainerFlag;
 }
