5225757176d3e6c04e3a08178bfa0d5de728810a
grintoul1 <liam.rintoul@outlook.com>
2025-08-24 18:14:51 +0100

    Added separate FRB animation, config for whether to use FRZ or FRB animation for FRB, and adjusted tests to accommodate

--- src/battle_anim.c
+++ src/battle_anim.c
@@ -195,6 +195,7 @@ static const u8* const sBattleAnims_StatusConditions[NUM_B_ANIMS_STATUS] =
     [B_ANIM_STATUS_FRZ]         = gBattleAnimStatus_Freeze,
     [B_ANIM_STATUS_CURSED]      = gBattleAnimStatus_Curse,
     [B_ANIM_STATUS_NIGHTMARE]   = gBattleAnimStatus_Nightmare,
+    [B_ANIM_STATUS_FRB]         = (B_USE_FROSTBITE_ANIMATION ? gBattleAnimStatus_Frostbite: gBattleAnimStatus_Freeze),
 };
 
 static const u8* const sBattleAnims_General[NUM_B_ANIMS_GENERAL] =
