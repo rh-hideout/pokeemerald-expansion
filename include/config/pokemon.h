5e181fdec2aca07a45d3923b349d9c546af6f529
Jamie Foster <jamiefosterandco@gmail.com>
2025-09-13 18:38:27 +0200

    Refactor pokerus and add configs

--- include/config/pokemon.h
+++ include/config/pokemon.h
@@ -66,6 +66,15 @@
 #define P_LEARNSET_HELPER_TEACHABLE      TRUE   // If TRUE, teachable_learnsets.h will be populated by tools/learnset_helpers/make_teachables.py using the included JSON files based on available TMs and tutors.
 #define P_TUTOR_MOVES_ARRAY              FALSE  // If TRUE, generates a gTutorMoves array automatically using make_teachables.py. (generally not needed, but the HGSS Pokedex has an optional use for it)
 
+//Pokerus settings
+//For pokerus, we refer to infection as a pokemon catching Pokerus from an enemy pokemon (trainer or wild) and we refer to spreading as pokemon catching pokerus from another infected pokemon in the party
+#define P_POKERUS_ENABLED                TRUE       // If FALSE, Pokerus will have no effect, won't be shown and won't be aquired in any way but save data won't be affected
+#define P_POKERUS_STRAIN_DISTRIBUTION    GEN_LATEST // Pokerus has 16 different strains and their probability distribution change depending on generation
+#define P_POKERUS_SPREAD_ADJACENECY      GEN_LATEST // In Gen 2, Pokerus spread to one adjacent pokemon but it spreads to both adjacent pokemon in gen 3+
+#define P_POKERUS_SPREAD_DAYS_LEFT       GEN_LATEST // In Gen 2, a freshly spreaded pokemon will get its full infection duration based on strain. In gen 3+, the pokerus duration will copy the duration from the pokemon it was spreaded from
+#define P_POKERUS_INFECT_AGAIN           TRUE       // If TRUE, your party can get infected even when it is already infected with Pokerus (doesn't affect spreading, only TRUE in gen 2)
+#define P_POKERUS_INFECTION_FLAG         0          // If Pokerus can only get infected if this flag is set or undefined (0). This emulates a gen 2 mechanic where Pokemon can only get infected by Pokerus after visiting Goldernrod. This does not affect spreading
+
 // Flag settings
 // To use the following features in scripting, replace the 0s with the flag ID you're assigning it to.
 // Eg: Replace with FLAG_UNUSED_0x264 so you can use that flag to toggle the feature.
