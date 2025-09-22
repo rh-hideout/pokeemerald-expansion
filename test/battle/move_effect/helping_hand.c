09479014b9af5a724981c5ef6093faa08037a42d
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2025-09-22 17:47:59 +0100

    Verify boost persists through the turn

--- test/battle/move_effect/helping_hand.c
+++ test/battle/move_effect/helping_hand.c
@@ -59,6 +59,40 @@ DOUBLE_BATTLE_TEST("Helping Hand boosts the power of attacking moves by 50%", s1
     }
 }
 
+DOUBLE_BATTLE_TEST("Helping Hand still boosts moves used due to Instruct", s16 damage)
+{
+    bool32 useHelpingHand;
+
+    PARAMETRIZE { useHelpingHand = FALSE; }
+    PARAMETRIZE { useHelpingHand = TRUE;  }
+
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        if (useHelpingHand)
+        {
+            TURN { MOVE(playerRight, MOVE_HELPING_HAND, target: playerLeft);
+                   MOVE(playerLeft, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(opponentLeft, MOVE_INSTRUCT, target: playerLeft); }
+        }
+        else
+        {
+            TURN { MOVE(playerLeft, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(opponentLeft, MOVE_INSTRUCT, target: playerLeft); }
+        }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, playerLeft);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_INSTRUCT, opponentLeft);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, playerLeft);
+        HP_BAR(opponentLeft, captureDamage: &results[i].damage);
+    } FINALLY {
+        EXPECT_MUL_EQ(results[0].damage, Q_4_12(1.5), results[1].damage);
+    }
+}
+
 DOUBLE_BATTLE_TEST("Helping Hand boosts the power of attacking moves by 125% if Instructed into using it again", s16 damage)
 {
     bool32 useHelpingHandTwice;
