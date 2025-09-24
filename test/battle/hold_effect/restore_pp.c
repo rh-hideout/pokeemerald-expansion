2079d06eb45caed88e374e25728bfaeef3db6775
Bassoonian <iasperbassoonian@gmail.com>
2025-09-24 20:22:25 +0200

    Update test/battle/hold_effect/restore_pp.c

--- test/battle/hold_effect/restore_pp.c
+++ test/battle/hold_effect/restore_pp.c
@@ -6,7 +6,7 @@ ASSUMPTIONS
     ASSUME(gItemsInfo[ITEM_LEPPA_BERRY].holdEffect == HOLD_EFFECT_RESTORE_PP);
 }
 
-SINGLE_BATTLE_TEST("Restore PP berry activates immeaditley on switch in")
+SINGLE_BATTLE_TEST("Restore PP berry activates immediately on switch in")
 {
     GIVEN {
         PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_LEPPA_BERRY); MovesWithPP({MOVE_SCRATCH, 0}, {MOVE_CELEBRATE, 20}); }
