55786d24ce2234a2cc8dcf7142e87dfdc9c5f03f
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-10-07 11:49:29 -0700

    test: added a test to ensure that pokemon are getting the correct friendship bonus when an item is used in battle

--- test/battle/item_effect/increase_stat.c
+++ test/battle/item_effect/increase_stat.c
@@ -282,3 +282,25 @@ SINGLE_BATTLE_TEST("Using X items in battle raises Friendship", s16 damage)
             EXPECT_EQ(player->friendship, X_ITEM_FRIENDSHIP_INCREASE);
     }
 }
+
+SINGLE_BATTLE_TEST("Using X items in battle where Pokemon was met raises Friendship with a bonus", s16 damage)
+{
+    u32 startingFriendship;
+    u8 metLocation = GetCurrentRegionMapSectionId();
+
+    PARAMETRIZE { startingFriendship = 0; }
+    PARAMETRIZE { startingFriendship = X_ITEM_MAX_FRIENDSHIP; }
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Friendship(startingFriendship); };
+        // Set met location to currentMapSec to get the friendship boost
+        SetMonData(&PLAYER_PARTY[0], MON_DATA_MET_LOCATION, &metLocation);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { USE_ITEM(player, ITEM_X_ACCURACY); MOVE(opponent, MOVE_CELEBRATE); }
+    } THEN {
+        if (startingFriendship == X_ITEM_MAX_FRIENDSHIP)
+            EXPECT_EQ(player->friendship, X_ITEM_MAX_FRIENDSHIP);
+        else
+            EXPECT_EQ(player->friendship, (ITEM_FRIENDSHIP_MAPSEC_BONUS + X_ITEM_FRIENDSHIP_INCREASE));
+    }
+}
