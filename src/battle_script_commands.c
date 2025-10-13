6864c9e16940f95946583ee757216c65473c1d55
Jamie Foster <jamiefosterandco@gmail.com>
2025-10-13 07:32:13 +0200

    Fix judgement messages not appearing properly in Battle Arena

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -17122,7 +17122,7 @@ void BS_EraseArenaRefTextBox(void)
 
 void BS_ArenaJudgmentString(void)
 {
-    CMD_ARGS(u8 id);
+    NATIVE_ARGS(u8 id);
     BattleStringExpandPlaceholdersToDisplayedString(gRefereeStringsTable[cmd->id]);
     BattlePutTextOnWindow(gDisplayedStringBattle, ARENA_WIN_JUDGMENT_TEXT);
     gBattlescriptCurrInstr = cmd->nextInstr;
