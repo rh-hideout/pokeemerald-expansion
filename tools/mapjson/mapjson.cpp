0c8b8386e7ffb818105a0d0c0858730b6583c2d0
Jamie Foster <jamiefosterandco@gmail.com>
2025-10-18 00:08:15 +0200

    Add map header flag to make wirting special var scount as a script effect

--- tools/mapjson/mapjson.cpp
+++ tools/mapjson/mapjson.cpp
@@ -175,7 +175,8 @@ string generate_map_header_text(Json map_data, Json layouts_data) {
              << "allow_cycling=" << json_to_string(map_data, "allow_cycling") << ", "
              << "allow_escaping=" << json_to_string(map_data, "allow_escaping") << ", "
              << "allow_running=" << json_to_string(map_data, "allow_running") << ", "
-             << "show_map_name=" << json_to_string(map_data, "show_map_name") << "\n";
+             << "show_map_name=" << json_to_string(map_data, "show_map_name") << ", "
+             << "write_specialvar_iseffect=" << json_to_string(map_data, "write_specialvar_iseffect") << "\n";
 
     if (version == "firered")
         text << "\t.byte " << json_to_string(map_data, "floor_number") << "\n";
