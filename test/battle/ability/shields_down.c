93feece2682b1f77a524e48a2463418a67b8fca0
Hedara <hedara90@gmail.com>
2025-10-18 17:34:07 +0200

    Changed Minor test to reflect actual cartridge behaviour

--- test/battle/ability/shields_down.c
+++ test/battle/ability/shields_down.c
@@ -1,23 +1,25 @@
 #include "global.h"
 #include "test/battle.h"
 
-SINGLE_BATTLE_TEST("Minior Meteor transforms into Minior Core on switch-in if it has 1/2 or less health")
+SINGLE_BATTLE_TEST("Minior Core doesn't transform into Minior Meteor on switch-in if it has 1/2 or less health")
 {
     GIVEN {
         PLAYER(SPECIES_WYNAUT);
         OPPONENT(SPECIES_WOBBUFFET) { HP(1); }
-        OPPONENT(SPECIES_MINIOR_METEOR) { Ability(ABILITY_SHIELDS_DOWN); HP(1); }
+        OPPONENT(SPECIES_MINIOR_CORE) { Ability(ABILITY_SHIELDS_DOWN); HP(1); }
     } WHEN {
         TURN { MOVE(player, MOVE_SCRATCH); SEND_OUT(opponent, 1); }
     } SCENE {
-        ABILITY_POPUP(opponent, ABILITY_SHIELDS_DOWN);
-        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_FORM_CHANGE, opponent);
+        NONE_OF {
+            ABILITY_POPUP(opponent, ABILITY_SHIELDS_DOWN);
+            ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_FORM_CHANGE, opponent);
+        }
     } THEN {
         EXPECT_EQ(opponent->species, SPECIES_MINIOR_CORE);
     }
 }
 
-SINGLE_BATTLE_TEST("Minior Core transforms into Minior Meteor on switch-in if it more then 1/2 health")
+SINGLE_BATTLE_TEST("Minior Core transforms into Minior Meteor on switch-in if has it more then 1/2 health")
 {
     GIVEN {
         PLAYER(SPECIES_WYNAUT);
@@ -25,6 +27,21 @@ SINGLE_BATTLE_TEST("Minior Core transforms into Minior Meteor on switch-in if it
         OPPONENT(SPECIES_MINIOR_CORE) { Ability(ABILITY_SHIELDS_DOWN); }
     } WHEN {
         TURN { MOVE(player, MOVE_SCRATCH); SEND_OUT(opponent, 1); }
+    } SCENE {
+        ABILITY_POPUP(opponent, ABILITY_SHIELDS_DOWN);
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_FORM_CHANGE, opponent);
+    } THEN {
+        EXPECT_EQ(opponent->species, SPECIES_MINIOR_METEOR);
+    }
+}
+
+SINGLE_BATTLE_TEST("Minior Core transforms into Minior Meteor on battle start if it has more then 1/2 health")
+{
+    GIVEN {
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_MINIOR_CORE) { Ability(ABILITY_SHIELDS_DOWN); }
+    } WHEN {
+        TURN { }
     } SCENE {
         ABILITY_POPUP(opponent, ABILITY_SHIELDS_DOWN);
         ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_FORM_CHANGE, opponent);
