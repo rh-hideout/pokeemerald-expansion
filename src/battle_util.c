0a3c1ab63592c1691719a5d82b4718031480ae45
Jamie Foster <jamiefosterandco@gmail.com>
2025-10-24 11:04:55 +0200

    Restore the correct attacker and target after triggering toxic debris

--- src/battle_util.c
+++ src/battle_util.c
@@ -4959,6 +4959,8 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
              && IsBattlerTurnDamaged(gBattlerTarget)
              && (gSideTimers[GetBattlerSide(gBattlerAttacker)].toxicSpikesAmount != 2))
             {
+                SaveBattlerTarget(gBattlerTarget);
+                SaveBattlerAttacker(gBattlerAttacker);
                 gBattlerAttacker = gBattlerTarget;
                 gBattlerTarget = BATTLE_OPPOSITE(gBattlerAttacker);
                 BattleScriptCall(BattleScript_ToxicDebrisActivates);
