47a77e3ce620d2e3def941e8e60941d0e29bb865
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2025-10-25 15:07:11 +0100

    Fixes Ruin field status not being prevented by Gastro Acid, protected by Ability Shield

--- test/battle/ability/beads_of_ruin.c
+++ test/battle/ability/beads_of_ruin.c
@@ -4,6 +4,7 @@
 ASSUMPTIONS
 {
     ASSUME(GetMoveCategory(MOVE_WATER_GUN) == DAMAGE_CATEGORY_SPECIAL);
+    ASSUME(GetMoveCategory(MOVE_ROUND) == DAMAGE_CATEGORY_SPECIAL);
     ASSUME(GetMoveEffect(MOVE_ROLE_PLAY) == EFFECT_ROLE_PLAY);
 }
 
@@ -74,4 +75,128 @@ SINGLE_BATTLE_TEST("Beads of Ruin's message displays correctly after all battler
     }
 }
 
-TO_DO_BATTLE_TEST("Beads of Ruin reduce Defense if Wonder Room is active");
+DOUBLE_BATTLE_TEST("Beads of Ruin increases damage taken by physical moves in Wonder Room")
+{
+    bool32 useWonderRoom;
+    s16 damage[4];
+
+    PARAMETRIZE { useWonderRoom = FALSE; }
+    PARAMETRIZE { useWonderRoom = TRUE; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_WONDER_ROOM) == EFFECT_WONDER_ROOM);
+        ASSUME(GetMoveCategory(MOVE_SCRATCH) == DAMAGE_CATEGORY_PHYSICAL);
+        ASSUME(GetMoveEffect(MOVE_ROUND) != EFFECT_PSYSHOCK);
+        PLAYER(SPECIES_CHI_YU) { Ability(ABILITY_BEADS_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        if (useWonderRoom)
+            TURN { MOVE(opponentLeft, MOVE_WONDER_ROOM);
+                   MOVE(playerLeft, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(playerRight, MOVE_ROUND, target: opponentRight); }
+        else
+            TURN { MOVE(playerLeft, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(playerRight, MOVE_ROUND, target: opponentRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_BEADS_OF_RUIN);
+        MESSAGE("Chi-Yu's Beads of Ruin weakened the Sp. Def of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, playerLeft);
+        HP_BAR(opponentLeft, captureDamage: &damage[i * 2]); // 0 when useWonderRoom = FALSE, 2 when useWonderRoom = TRUE
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROUND, playerRight);
+        HP_BAR(opponentRight, captureDamage: &damage[i * 2 + 1]); // 1 when useWonderRoom = FALSE, 3 when useWonderRoom = TRUE
+    } FINALLY {
+        EXPECT_GT(damage[2], damage[0]); // In Wonder Room, physical move deals more damage
+        EXPECT_LT(damage[3], damage[1]); // In Wonder Room, special move deals less damage
+    }
+}
+
+SINGLE_BATTLE_TEST("Beads of Ruin doesn't activate when dragged out by Mold Breaker attacker")
+{
+    s16 damage[2];
+    u32 ability;
+
+    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
+    PARAMETRIZE { ability = ABILITY_SAND_RUSH; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_DRAGON_TAIL) == EFFECT_HIT_SWITCH_TARGET);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_CHI_YU) { Ability(ABILITY_BEADS_OF_RUIN); }
+        OPPONENT(SPECIES_EXCADRILL) { Ability(ability); }
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_DRAGON_TAIL); }
+        TURN { MOVE(player, MOVE_ROUND); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_DRAGON_TAIL, opponent);
+        if (ability == ABILITY_MOLD_BREAKER)
+        {
+            NONE_OF {
+                ABILITY_POPUP(player, ABILITY_BEADS_OF_RUIN);
+                MESSAGE("Chi-Yu's Beads of Ruin weakened the Sp. Def of all surrounding Pokémon!");
+            }
+        }
+        else
+        {
+            ABILITY_POPUP(player, ABILITY_BEADS_OF_RUIN);
+            MESSAGE("Chi-Yu's Beads of Ruin weakened the Sp. Def of all surrounding Pokémon!");
+        }
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROUND, player);
+        HP_BAR(opponent, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_LT(damage[0], damage[1]);
+    }
+}
+
+DOUBLE_BATTLE_TEST("Beads of Ruin's Sp. Def reduction is not ignored by Mold Breaker")
+{
+    s16 damage[2];
+    u32 ability;
+
+    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
+    PARAMETRIZE { ability = ABILITY_SAND_RUSH; }
+
+    GIVEN {
+        PLAYER(SPECIES_CHI_YU) { Ability(ABILITY_BEADS_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_EXCADRILL) { Ability(ability); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentLeft, MOVE_ROUND, target: playerRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_BEADS_OF_RUIN);
+        MESSAGE("Chi-Yu's Beads of Ruin weakened the Sp. Def of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROUND, opponentLeft);
+        HP_BAR(playerRight, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_EQ(damage[0], damage[1]);
+    }
+}
+
+DOUBLE_BATTLE_TEST("Beads of Ruin's Sp. Def reduction is ignored by Gastro Acid")
+{
+    s16 damage[2];
+    u32 move;
+
+    PARAMETRIZE { move = MOVE_GASTRO_ACID; }
+    PARAMETRIZE { move = MOVE_CELEBRATE; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_GASTRO_ACID) == EFFECT_GASTRO_ACID);
+        PLAYER(SPECIES_CHI_YU) { Ability(ABILITY_BEADS_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentRight, move, target: playerLeft); MOVE(opponentLeft, MOVE_ROUND, target: playerRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_BEADS_OF_RUIN);
+        MESSAGE("Chi-Yu's Beads of Ruin weakened the Sp. Def of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, move, opponentRight);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROUND, opponentLeft);
+        HP_BAR(playerRight, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_LT(damage[0], damage[1]);
+    }
+}
--- test/battle/ability/sword_of_ruin.c
+++ test/battle/ability/sword_of_ruin.c
@@ -73,3 +73,129 @@ SINGLE_BATTLE_TEST("Sword of Ruin's message displays correctly after all battler
         MESSAGE("The opposing Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
     }
 }
+
+DOUBLE_BATTLE_TEST("Sword of Ruin increases damage taken by special moves in Wonder Room")
+{
+    bool32 useWonderRoom;
+    s16 damage[4];
+
+    PARAMETRIZE { useWonderRoom = FALSE; }
+    PARAMETRIZE { useWonderRoom = TRUE; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_WONDER_ROOM) == EFFECT_WONDER_ROOM);
+        ASSUME(GetMoveCategory(MOVE_ROUND) == DAMAGE_CATEGORY_SPECIAL);
+        ASSUME(GetMoveEffect(MOVE_ROUND) != EFFECT_PSYSHOCK);
+        PLAYER(SPECIES_CHIEN_PAO) { Ability(ABILITY_SWORD_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        if (useWonderRoom)
+            TURN { MOVE(opponentLeft, MOVE_WONDER_ROOM);
+                   MOVE(playerRight, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(playerLeft, MOVE_ROUND, target: opponentRight); }
+        else
+            TURN { MOVE(playerRight, MOVE_SCRATCH, target: opponentLeft);
+                   MOVE(playerLeft, MOVE_ROUND, target: opponentRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_SWORD_OF_RUIN);
+        MESSAGE("Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, playerRight);
+        HP_BAR(opponentLeft, captureDamage: &damage[i * 2]); // 0 when useWonderRoom = FALSE, 2 when useWonderRoom = TRUE
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ROUND, playerLeft);
+        HP_BAR(opponentRight, captureDamage: &damage[i * 2 + 1]); // 1 when useWonderRoom = FALSE, 3 when useWonderRoom = TRUE
+    } FINALLY {
+        EXPECT_LT(damage[2], damage[0]); // In Wonder Room, physical move deals less damage
+        EXPECT_GT(damage[3], damage[1]); // In Wonder Room, special move deals more damage
+    }
+}
+
+SINGLE_BATTLE_TEST("Sword of Ruin doesn't activate when dragged out by Mold Breaker attacker")
+{
+    s16 damage[2];
+    u32 ability;
+
+    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
+    PARAMETRIZE { ability = ABILITY_SAND_RUSH; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_DRAGON_TAIL) == EFFECT_HIT_SWITCH_TARGET);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_CHIEN_PAO) { Ability(ABILITY_SWORD_OF_RUIN); }
+        OPPONENT(SPECIES_EXCADRILL) { Ability(ability); }
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_DRAGON_TAIL); }
+        TURN { MOVE(player, MOVE_SCRATCH); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_DRAGON_TAIL, opponent);
+        if (ability == ABILITY_MOLD_BREAKER)
+        {
+            NONE_OF {
+                ABILITY_POPUP(player, ABILITY_SWORD_OF_RUIN);
+                MESSAGE("Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
+            }
+        }
+        else
+        {
+            ABILITY_POPUP(player, ABILITY_SWORD_OF_RUIN);
+            MESSAGE("Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
+        }
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, player);
+        HP_BAR(opponent, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_LT(damage[0], damage[1]);
+    }
+}
+
+DOUBLE_BATTLE_TEST("Sword of Ruin's Defense reduction is not ignored by Mold Breaker")
+{
+    s16 damage[2];
+    u32 ability;
+
+    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
+    PARAMETRIZE { ability = ABILITY_SAND_RUSH; }
+
+    GIVEN {
+        PLAYER(SPECIES_CHIEN_PAO) { Ability(ABILITY_SWORD_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_EXCADRILL) { Ability(ability); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentLeft, MOVE_SCRATCH, target: playerRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_SWORD_OF_RUIN);
+        MESSAGE("Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, opponentLeft);
+        HP_BAR(playerRight, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_EQ(damage[0], damage[1]);
+    }
+}
+
+DOUBLE_BATTLE_TEST("Sword of Ruin's Defense reduction is ignored by Gastro Acid")
+{
+    s16 damage[2];
+    u32 move;
+
+    PARAMETRIZE { move = MOVE_GASTRO_ACID; }
+    PARAMETRIZE { move = MOVE_CELEBRATE; }
+
+    GIVEN {
+        ASSUME(GetMoveEffect(MOVE_GASTRO_ACID) == EFFECT_GASTRO_ACID);
+        PLAYER(SPECIES_CHIEN_PAO) { Ability(ABILITY_SWORD_OF_RUIN); }
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentRight, move, target: playerLeft); MOVE(opponentLeft, MOVE_SCRATCH, target: playerRight); }
+    } SCENE {
+        ABILITY_POPUP(playerLeft, ABILITY_SWORD_OF_RUIN);
+        MESSAGE("Chien-Pao's Sword of Ruin weakened the Defense of all surrounding Pokémon!");
+        ANIMATION(ANIM_TYPE_MOVE, move, opponentRight);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, opponentLeft);
+        HP_BAR(playerRight, captureDamage: &damage[i]);
+    } FINALLY {
+        EXPECT_LT(damage[0], damage[1]);
+    }
+}
