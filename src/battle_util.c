86101cfe8821ae14e717ceb4fcac9d464cf4e1f9
grintoul <166724814+grintoul1@users.noreply.github.com>
2025-10-27 18:01:19 +0000

    Update src/battle_util.c
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/battle_util.c
+++ src/battle_util.c
@@ -11910,8 +11910,8 @@ bool32 BreaksThroughSemiInvulnerablity(u32 battler, u32 move)
 
 bool32 HasPartnerTrainer(u32 battler)
 {
-    if ((GetBattlerSide(gBattlerAttacker) == B_SIDE_PLAYER && gBattleTypeFlags & BATTLE_TYPE_PLAYER_HAS_PARTNER)
-    || (GetBattlerSide(gBattlerAttacker) == B_SIDE_OPPONENT && gBattleTypeFlags & BATTLE_TYPE_TWO_OPPONENTS))
+    if ((GetBattlerSide(battler) == B_SIDE_PLAYER && gBattleTypeFlags & BATTLE_TYPE_PLAYER_HAS_PARTNER)
+     || (GetBattlerSide(battler) == B_SIDE_OPPONENT && gBattleTypeFlags & BATTLE_TYPE_TWO_OPPONENTS))
         return TRUE;
     else
         return FALSE;
