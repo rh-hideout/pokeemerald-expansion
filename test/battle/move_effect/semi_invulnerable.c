2651e7ad67cb5e3d47598efcdab27a8b377e992f
AlexOn1ine <alexthenotes@gmail.com>
2025-10-30 15:12:11 +0100

    Fixes Gooey/Tangling Hair ability pop up triggering on Clear Body

--- test/battle/move_effect/semi_invulnerable.c
+++ test/battle/move_effect/semi_invulnerable.c
@@ -248,3 +248,14 @@ SINGLE_BATTLE_TEST("Semi-invulnerable moves apply a status that won't block cert
         HP_BAR(player);
     }
 }
+
+SINGLE_BATTLE_TEST("11")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_FLY); MOVE(opponent, MOVE_TACKLE); }
+    } SCENE {
+    }
+}
