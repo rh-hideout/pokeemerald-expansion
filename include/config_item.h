b577427195142a1f7339a4286eb3136425f1fd7a
Jamie Foster <jamiefosterandco@gmail.com>
2025-11-01 10:06:01 +0100

    Config splitter prototype

---
+++ include/config_item.h
@@ -0,0 +1,39 @@
+// DO NOT MODIFY THIS FILE
+// This file was auto-generate by tools/config_splitter/split_config.py
+// Modify the files in the include/config/ directory instead
+
+#ifndef GUARD_CONFIG_ITEM_H
+#define GUARD_CONFIG_ITEM_H
+#define I_SHINY_CHARM_ADDITIONAL_ROLLS 2 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_KEY_FOSSILS GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_KEY_ESCAPE_ROPE GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_HEALTH_RECOVERY GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_SITRUS_BERRY_HEAL GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_VITAMIN_EV_CAP GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_BERRY_EV_JUMP GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_GRISEOUS_ORB_FORM_CHANGE GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_GEM_BOOST_POWER GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_USE_EVO_HELD_ITEMS_FROM_BAG FALSE // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_TYPE_BOOST_POWER GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_SELL_VALUE_FRACTION GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_PRICE GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_BERRY_PRICE GEN_7 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_POWER_ITEM_BOOST GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_PREMIER_BALL_BONUS GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ROTOM_CATALOG_THUNDER_SHOCK GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_REPEL_INCLUDE_FAINTED GEN_LATEST // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_REUSABLE_TMS FALSE // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_EXP_SHARE_FLAG 0 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_EXP_SHARE_ITEM GEN_5 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define VAR_LAST_REPEL_LURE_USED 0 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_REPEL_LURE_MENU TRUE // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_VS_SEEKER_CHARGING 0 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_FLAG 0 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_SOUNDS TRUE // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_PAL 15 // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_NONE RGB_GRAY // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_SLOW RGB2GBA // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_NORMAL RGB2GBA // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_FAST RGB2GBA // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#define I_ORAS_DOWSING_COLOR_FASTER RGB_RED // DO NOT MODIFY THIS LINE, change include/config/item.h instead
+#endif
