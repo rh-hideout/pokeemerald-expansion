5a2d82fd01877cd589ece06a972d4ac105f218d5
grintoul1 <liam.rintoul@outlook.com>
2025-11-01 12:21:34 +0000

    Updated to not use illegal abilities

--- test/battle/ability/aura_break.c
+++ test/battle/ability/aura_break.c
@@ -1,29 +1,30 @@
 #include "global.h"
 #include "test/battle.h"
 
-SINGLE_BATTLE_TEST("Aura Break inverts Fairy Aura's effect")
+DOUBLE_BATTLE_TEST("Aura Break inverts Fairy Aura's effect")
 {
     s16 damage[3];
 
 	GIVEN {
 		PLAYER(SPECIES_WOBBUFFET);
-		PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_FAIRY_AURA); }
+		PLAYER(SPECIES_WOBBUFFET);
+		PLAYER(SPECIES_XERNEAS) { Ability(ABILITY_FAIRY_AURA); }
 		OPPONENT(SPECIES_WOBBUFFET);
-		OPPONENT(SPECIES_WOBBUFFET) { Ability(ABILITY_AURA_BREAK); }
+		OPPONENT(SPECIES_WOBBUFFET);
+		OPPONENT(SPECIES_ZYGARDE_50) { Ability(ABILITY_AURA_BREAK); }
 	} WHEN {
-		TURN { MOVE(player, MOVE_PLAY_ROUGH); }
-		TURN { SWITCH(player, 1); }
-		TURN { MOVE(player, MOVE_PLAY_ROUGH); }
-		TURN { MOVE(player, MOVE_PLAY_ROUGH); SWITCH(opponent, 1); }
+		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target:opponentLeft); }
+		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target:opponentLeft); SWITCH(playerRight, 2); }
+		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target:opponentLeft); SWITCH(opponentRight, 2); }
 	} SCENE {
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, player);
-		HP_BAR(opponent, captureDamage: &damage[0]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[0]);
 
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, player);
-		HP_BAR(opponent, captureDamage: &damage[1]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[1]);
 
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, player);
-		HP_BAR(opponent, captureDamage: &damage[2]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[2]);
 
 	} THEN {
 		EXPECT_MUL_EQ(damage[0], UQ_4_12(1.33), damage[1]);
@@ -31,29 +32,30 @@ SINGLE_BATTLE_TEST("Aura Break inverts Fairy Aura's effect")
 	}
 }
 
-SINGLE_BATTLE_TEST("Aura Break inverts Dark Aura's effect")
+DOUBLE_BATTLE_TEST("Aura Break inverts Dark Aura's effect")
 {
     s16 damage[3];
 
 	GIVEN {
 		PLAYER(SPECIES_WOBBUFFET);
-		PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_DARK_AURA); }
+		PLAYER(SPECIES_WOBBUFFET);
+		PLAYER(SPECIES_YVELTAL) { Ability(ABILITY_DARK_AURA); }
 		OPPONENT(SPECIES_WOBBUFFET);
-		OPPONENT(SPECIES_WOBBUFFET) { Ability(ABILITY_AURA_BREAK); }
+		OPPONENT(SPECIES_WOBBUFFET);
+		OPPONENT(SPECIES_ZYGARDE_50) { Ability(ABILITY_AURA_BREAK); }
 	} WHEN {
-		TURN { MOVE(player, MOVE_BITE); }
-		TURN { SWITCH(player, 1); }
-		TURN { MOVE(player, MOVE_BITE); }
-		TURN { MOVE(player, MOVE_BITE); SWITCH(opponent, 1); }
+		TURN { MOVE(playerLeft, MOVE_BITE, target:opponentLeft); }
+		TURN { MOVE(playerLeft, MOVE_BITE, target:opponentLeft); SWITCH(playerRight, 2); }
+		TURN { MOVE(playerLeft, MOVE_BITE, target:opponentLeft); SWITCH(opponentRight, 2); }
 	} SCENE {
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, player);
-		HP_BAR(opponent, captureDamage: &damage[0]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[0]);
 
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, player);
-		HP_BAR(opponent, captureDamage: &damage[1]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[1]);
 
-		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, player);
-		HP_BAR(opponent, captureDamage: &damage[2]);
+		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
+		HP_BAR(opponentLeft, captureDamage: &damage[2]);
 
 	} THEN {
 		EXPECT_MUL_EQ(damage[0], UQ_4_12(1.33), damage[1]);
@@ -64,28 +66,28 @@ SINGLE_BATTLE_TEST("Aura Break inverts Dark Aura's effect")
 DOUBLE_BATTLE_TEST("Aura Break ignores Mold Breaker abilities")
 {
     s16 damage[6];
-    u32 ability = ABILITY_NONE;
+    u32 species = SPECIES_WOBBUFFET, ability = ABILITY_NONE;
 
-    PARAMETRIZE { ability = ABILITY_SHADOW_TAG; }
-    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
-    PARAMETRIZE { ability = ABILITY_TERAVOLT; }
-    PARAMETRIZE { ability = ABILITY_TURBOBLAZE; }
+    PARAMETRIZE { species = SPECIES_WOBBUFFET, ability = ABILITY_SHADOW_TAG; }
+    PARAMETRIZE { species = SPECIES_CRANIDOS, ability = ABILITY_MOLD_BREAKER; }
+    PARAMETRIZE { species = SPECIES_ZEKROM, ability = ABILITY_TERAVOLT; }
+    PARAMETRIZE { species = SPECIES_RESHIRAM, ability = ABILITY_TURBOBLAZE; }
 
 	GIVEN {
-		PLAYER(SPECIES_WOBBUFFET) { Ability(ability); }
+		PLAYER(species) { Ability(ability); Level(50); }
 		PLAYER(SPECIES_WOBBUFFET);
-		PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_DARK_AURA); }
-		PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_FAIRY_AURA); }
+		PLAYER(SPECIES_YVELTAL) { Ability(ABILITY_DARK_AURA); }
+		PLAYER(SPECIES_XERNEAS) { Ability(ABILITY_FAIRY_AURA); }
 		OPPONENT(SPECIES_WOBBUFFET);
 		OPPONENT(SPECIES_WOBBUFFET);
-		OPPONENT(SPECIES_WOBBUFFET) { Ability(ABILITY_AURA_BREAK); }
+		OPPONENT(SPECIES_ZYGARDE_50) { Ability(ABILITY_AURA_BREAK); }
 	} WHEN {
 		TURN { MOVE(playerLeft, MOVE_BITE, target: opponentLeft); }
 		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target: opponentLeft); }
-		TURN { MOVE(playerLeft, MOVE_BITE, target: opponentRight); SWITCH(playerRight, 2); }
-		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target: opponentRight); SWITCH(playerRight, 3); }
-		TURN { MOVE(playerLeft, MOVE_BITE, target: opponentRight); SWITCH(playerRight, 2); SWITCH(opponentRight, 2); }
-		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target: opponentRight); SWITCH(playerRight, 3); }
+		TURN { MOVE(playerLeft, MOVE_BITE, target: opponentLeft); SWITCH(playerRight, 2); }
+		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target: opponentLeft); SWITCH(playerRight, 3); }
+		TURN { MOVE(playerLeft, MOVE_BITE, target: opponentLeft); SWITCH(playerRight, 2); SWITCH(opponentRight, 2); }
+		TURN { MOVE(playerLeft, MOVE_PLAY_ROUGH, target: opponentLeft); SWITCH(playerRight, 3); }
 	} SCENE {
 		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
 		HP_BAR(opponentLeft, captureDamage: &damage[0]);
@@ -94,16 +96,16 @@ DOUBLE_BATTLE_TEST("Aura Break ignores Mold Breaker abilities")
 		HP_BAR(opponentLeft, captureDamage: &damage[1]);
 
 		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
-		HP_BAR(opponentRight, captureDamage: &damage[2]);
+		HP_BAR(opponentLeft, captureDamage: &damage[2]);
 
 		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, playerLeft);
-		HP_BAR(opponentRight, captureDamage: &damage[3]);
+		HP_BAR(opponentLeft, captureDamage: &damage[3]);
 
 		ANIMATION(ANIM_TYPE_MOVE, MOVE_BITE, playerLeft);
-		HP_BAR(opponentRight, captureDamage: &damage[4]);
+		HP_BAR(opponentLeft, captureDamage: &damage[4]);
 
 		ANIMATION(ANIM_TYPE_MOVE, MOVE_PLAY_ROUGH, playerLeft);
-		HP_BAR(opponentRight, captureDamage: &damage[5]);
+		HP_BAR(opponentLeft, captureDamage: &damage[5]);
     } THEN {
 		EXPECT_MUL_EQ(damage[0], UQ_4_12(1.33), damage[2]);
 		EXPECT_MUL_EQ(damage[0], UQ_4_12(0.75), damage[4]);
