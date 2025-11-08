5de60d3e0140ba97db84dcb420346554ff3253f6
Frank <fdeblasio1004@gmail.com>
2025-11-08 18:52:19 -0500

    Added support for latest contest effects

--- include/config/contest.h
+++ include/config/contest.h
@@ -3,5 +3,6 @@
 
 // Move data settings
 #define C_UPDATED_MOVE_CATEGORIES   GEN_LATEST // Updates contest category.
+#define C_UPDATED_MOVE_EFFECTS      GEN_LATEST // Updates contest effects.
 
 #endif // GUARD_CONFIG_CONTEST_H
