602ef90fa5b8be68cbec59357bd85f4080310079
Frank <fdeblasio1004@gmail.com>
2025-11-08 18:50:21 -0500

    Added contest config and updated categories

---
+++ include/config/contest.h
@@ -0,0 +1,7 @@
+#ifndef GUARD_CONFIG_CONTEST_H
+#define GUARD_CONFIG_CONTEST_H
+
+// Move data settings
+#define C_UPDATED_MOVE_CATEGORIES   GEN_LATEST // Updates contest category.
+
+#endif // GUARD_CONFIG_CONTEST_H
