8a5756c6e7ba9d67eb3d39ecefd6739a2ba63b23
Jamie Foster <jamiefosterandco@gmail.com>
2025-11-11 11:51:22 +0100

    forgot files

---
+++ src/ow_synchronize.c
@@ -0,0 +1,111 @@
+#include "global.h"
+#include "ow_synchronize.h"
+#include "pokemon.h"
+#include "random.h"
+#include "constants/pokemon.h"
+
+static bool32 HasHalfChance(u32 species);
+static bool32 HasTwoThirdsChance(u32 species);
+static bool32 IsFalse(u32 species);
+static bool32 IsTrue(u32 species);
+static bool32 IsTrueIfUndiscoveredEggGroup(u32 species);
+
+const static bool32 (*sSynchronizeModes[]) (u32) = {
+#if OW_SYNCHRONIZE_NATURE == GEN_3
+    [WILDMON_ORIGIN] = HasHalfChance,
+    [STATIC_WILDMON_ORIGIN] = IsFalse,
+    [ROAMER_ORIGIN] = IsFalse,
+    [GIFTMON_ORIGIN] = IsFalse,
+#elif OW_SYNCHRONIZE_NATURE <= GEN_5
+    [WILDMON_ORIGIN] = IsTrue,
+    [STATIC_WILDMON_ORIGIN] = IsFalse,
+    [ROAMER_ORIGIN] = HasHalfChance,
+    [GIFTMON_ORIGIN] = HasHalfChance,
+#elif OW_SYNCHRONIZE_NATURE == GEN_6
+    [WILDMON_ORIGIN] = HasHalfChance,
+    [STATIC_WILDMON_ORIGIN] = HasHalfChance,
+    [ROAMER_ORIGIN] = IsFalse,
+    [GIFTMON_ORIGIN] = IsTrueIfUndiscoveredEggGroup,
+#elif OW_SYNCHRONIZE_NATURE == GEN_7
+    [WILDMON_ORIGIN] = HasHalfChance,
+    [STATIC_WILDMON_ORIGIN] = HasHalfChance,
+    [ROAMER_ORIGIN] = IsFalse,
+    [GIFTMON_ORIGIN] = IsTrue,
+#else
+    [WILDMON_ORIGIN] = IsTrue,
+    [STATIC_WILDMON_ORIGIN] = IsFalse,
+    [ROAMER_ORIGIN] = IsTrue,
+    [GIFTMON_ORIGIN] = IsFalse,
+#endif
+};
+
+const static bool32 (*sCuteCharmModes[]) (u32) = {
+    [WILDMON_ORIGIN] = HasTwoThirdsChance,
+    [STATIC_WILDMON_ORIGIN] = HasTwoThirdsChance,
+    [ROAMER_ORIGIN] = IsFalse,
+    [GIFTMON_ORIGIN] = IsFalse,
+};
+
+static bool32 HasHalfChance(u32 species)
+{
+    return Random() % 2;
+}
+
+static bool32 HasTwoThirdsChance(u32 species)
+{
+    return Random() % 3;
+}
+
+static bool32 IsFalse(u32 species)
+{
+    return FALSE;
+}
+
+static bool32 IsTrue(u32 species)
+{
+    return TRUE;
+}
+
+static bool32 IsTrueIfUndiscoveredEggGroup(u32 species)
+{
+    return (gSpeciesInfo[species].eggGroups[0] == EGG_GROUP_NO_EGGS_DISCOVERED);
+}
+
+static bool32 IsSynchronizeActive(void)
+{
+    if (!GetMonData(&gPlayerParty[0], MON_DATA_SANITY_IS_EGG)
+        && GetMonAbility(&gPlayerParty[0]) == ABILITY_SYNCHRONIZE)
+        return TRUE;
+    return FALSE;
+}
+
+static bool32 IsCuteCharmActive(void)
+{
+     if (!GetMonData(&gPlayerParty[0], MON_DATA_SANITY_IS_EGG)
+        && GetMonAbility(&gPlayerParty[0]) == ABILITY_CUTE_CHARM)
+        return TRUE;
+    return FALSE;
+}
+
+u32 GetSynchronizedNature(enum GeneratedMonOrigin origin, u32 species)
+{
+    if (!IsSynchronizeActive())
+        return NATURE_RANDOM;
+    if (!(sSynchronizeModes[origin](species)))
+        return NATURE_RANDOM;
+    return GetMonData(&gPlayerParty[0], MON_DATA_PERSONALITY) % NUM_NATURES;
+}
+
+u32 GetSynchronizedGender(enum GeneratedMonOrigin origin, u32 species)
+{
+    if (!IsCuteCharmActive())
+        return MON_GENDER_RANDOM;
+    if (!(sCuteCharmModes[origin](species)))
+        return MON_GENDER_RANDOM;
+    u8 leadingMonGender = GetMonGender(&gPlayerParty[0]);
+    // misses mon is genderless check, although no genderless mon can have cute charm as ability
+    if (leadingMonGender == MON_FEMALE)
+        return MON_MALE;
+    else
+        return MON_FEMALE;
+}
