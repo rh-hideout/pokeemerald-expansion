ca90945cb7b850da3337adb71f2f863cfc9cc19f
Jamie Foster <jamiefosterandco@gmail.com>
2025-11-11 13:50:57 +0100

    Fix tests4

--- test/test_runner_battle.c
+++ test/test_runner_battle.c
@@ -1788,9 +1788,9 @@ void OpenPokemon(u32 sourceLine, enum BattlerPosition position, u32 species)
     (*partySize)++;
 
     CreateMon(DATA.currentMon, species, 100, 0, OTID_STRUCT_PRESET(0));
-    data = MOVE_NONE;
     for (i = 0; i < MAX_MON_MOVES; i++)
     {
+        data = MOVE_NONE;
         SetMonData(DATA.currentMon, MON_DATA_MOVE1 + i, &data);
         data = 0x7F; // Max PP possible
         SetMonData(DATA.currentMon, MON_DATA_PP1 + i, &data);
