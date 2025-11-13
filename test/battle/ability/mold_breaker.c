ea9301b635959ec32883ef4f306ad4468922511c
FosterProgramming <jamiefosterandco@gmail.com>
2025-11-13 14:09:59 +0100

    Apply suggestions from code review
    Co-authored-by: Bassoonian <iasperbassoonian@gmail.com>

--- test/battle/ability/mold_breaker.c
+++ test/battle/ability/mold_breaker.c
@@ -4,11 +4,11 @@
 SINGLE_BATTLE_TEST("Mold Breaker cancels damage reduction from Ice Scales", s16 damage)
 {
     u16 ability;
-    PARAMETRIZE { ability = ABILITY_SHADOW_TAG;}
-    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER;}
+    PARAMETRIZE { ability = ABILITY_SHADOW_TAG; }
+    PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
     GIVEN {
         ASSUME(GetMoveCategory(MOVE_PSYCHIC) == DAMAGE_CATEGORY_SPECIAL);
-        PLAYER(SPECIES_WOBBUFFET) { Ability(ability); };
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ability); }
         OPPONENT(SPECIES_FROSMOTH) { Ability(ABILITY_ICE_SCALES); }
     } WHEN {
         TURN { MOVE(player, MOVE_PSYCHIC); }
