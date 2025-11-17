07d3938bd5724ffc1f9840ecac0e6d2fd36ac35c
Eduardo Quezada <eduardo602002@gmail.com>
2025-11-17 17:54:07 -0300

    Added tests for stat-changing moves

--- test/battle/move_effect/minimize.c
+++ test/battle/move_effect/minimize.c
@@ -1,4 +1,24 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("TODO: Write Minimize (Move Effect) test titles")
+ASSUMPTIONS
+{
+    ASSUME(GetMoveEffect(MOVE_MINIMIZE) == EFFECT_MINIMIZE);
+}
+
+SINGLE_BATTLE_TEST("Minimize increases the user's Evasion by 1 stage")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_MINIMIZE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_MINIMIZE, player);
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, player);
+    } THEN {
+        EXPECT_EQ(player->statStages[STAT_EVASION], DEFAULT_STAT_STAGE + 1);
+    }
+}
+
+// Minimize's flag tests are handled in tests/battle/move_flags/minimize_double_damage.c
