de0e703633434c57c10e4212b0ebd8c7a38ed06e
Moos Toet <moostoet@hotmail.com>
2025-11-18 14:47:51 +0100

    fix: gBattleMons[battler].ability --> GetBattlerAbility(battler)

--- src/battle_util.c
+++ src/battle_util.c
@@ -5653,14 +5653,14 @@ u32 GetParadoxHighestStatId(u32 battler)
 
 static void ResetParadoxWeatherStat(u32 battler)
 {
-    if (gBattleMons[battler].ability == ABILITY_PROTOSYNTHESIS
+    if (GetBattlerAbility(battler) == ABILITY_PROTOSYNTHESIS
      && !gDisableStructs[battler].boosterEnergyActivated)
         gDisableStructs[battler].paradoxBoostedStat = 0;
 }
 
 static void ResetParadoxTerrainStat(u32 battler)
 {
-    if (gBattleMons[battler].ability == ABILITY_QUARK_DRIVE
+    if (GetBattlerAbility(battler) == ABILITY_QUARK_DRIVE
      && !gDisableStructs[battler].boosterEnergyActivated)
         gDisableStructs[battler].paradoxBoostedStat = 0;
 }
