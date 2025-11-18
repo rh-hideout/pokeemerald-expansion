4e18b56569b25d7f65e06b528d951d76e50aa425
Jamie Foster <jamiefosterandco@gmail.com>
2025-11-18 08:54:15 +0100

    fix being able to select dead pokemon when using X items

--- src/item_use.c
+++ src/item_use.c
@@ -1276,13 +1276,13 @@ bool32 CannotUseItemsInBattle(u16 itemId, struct Pokemon *mon)
     switch (battleUsage)
     {
     case EFFECT_ITEM_INCREASE_STAT:
-        if (gPartyMenu.slotId > 1)
+        if (hp == 0 || gPartyMenu.slotId > 1)
             cannotUse = TRUE;
         else if (CompareStat(battlerTarget, GetItemEffect(itemId)[1], MAX_STAT_STAGE, CMP_EQUAL, GetBattlerAbility(battlerTarget)))
             cannotUse = TRUE;
         break;
     case EFFECT_ITEM_SET_FOCUS_ENERGY:
-        if (gPartyMenu.slotId > 1)
+        if (hp == 0 ||gPartyMenu.slotId > 1)
             cannotUse = TRUE;
         else if (gBattleMons[battlerTarget].volatiles.dragonCheer || gBattleMons[battlerTarget].volatiles.focusEnergy)
             cannotUse = TRUE;
@@ -1318,7 +1318,7 @@ bool32 CannotUseItemsInBattle(u16 itemId, struct Pokemon *mon)
         break;
     case EFFECT_ITEM_INCREASE_ALL_STATS:
     {
-        if (gPartyMenu.slotId > 1)
+        if (hp == 0 || gPartyMenu.slotId > 1)
         {
             cannotUse = TRUE;
             break;
