b9d79ffc601f8dfa72b2cddd2ed4cb5303f4fe71
Eduardo Quezada <eduardo602002@gmail.com>
2025-11-20 16:45:26 -0300

    Rrrolling Rs

--- src/generational_changes.c
+++ src/generational_changes.c
@@ -17,32 +17,32 @@ void TestFreeConfigData(void)
     TRY_FREE_AND_SET_NULL(gGenerationalChangesTestOverride)
 }
 
-EWRAM_DATA struct MoveDataOverride *gMoveDataTestOverrride = NULL;
+EWRAM_DATA struct MoveDataOverride *gMoveDataTestOverride = NULL;
 
 void TestInitMoveDataOverride(void)
 {
-    gMoveDataTestOverrride = AllocZeroed(sizeof(struct MoveDataOverride));
+    gMoveDataTestOverride = AllocZeroed(sizeof(struct MoveDataOverride));
 }
 
 void TestAddMoveDataOverride(u32 move, enum MoveDataType type, u8 value)
 {
     struct MoveDataOverride *temp = NULL;
     u32 i = 0;
-    for (; gMoveDataTestOverrride[i].moveId != 0; i++); // Count how big the array is.
+    for (; gMoveDataTestOverride[i].moveId != 0; i++); // Count how big the array is.
 
     temp = AllocZeroed(sizeof(struct MoveDataOverride) * (i + 2));
-    memcpy(temp, gMoveDataTestOverrride, sizeof(struct MoveDataOverride) * (i + 1));
+    memcpy(temp, gMoveDataTestOverride, sizeof(struct MoveDataOverride) * (i + 1));
 
     temp[i].moveId = move;
     temp[i].type = type;
     temp[i].data = value;
 
-    TRY_FREE_AND_SET_NULL(gMoveDataTestOverrride);
-    gMoveDataTestOverrride = temp;
+    TRY_FREE_AND_SET_NULL(gMoveDataTestOverride);
+    gMoveDataTestOverride = temp;
 }
 
 void TestFreeMoveDataOverride(void)
 {
-    TRY_FREE_AND_SET_NULL(gMoveDataTestOverrride);
+    TRY_FREE_AND_SET_NULL(gMoveDataTestOverride);
 }
 #endif
