4716c07b050c80941301ac9f467fcea32e048bb9
pkmnsnfrn <pkmnsnfrn@gmail.com>
2025-11-25 21:39:41 -0800

    chore: fixed berry colors to be accurate

--- src/berry.c
+++ src/berry.c
@@ -567,7 +567,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Qualot"),
         .firmness = BERRY_FIRMNESS_HARD,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_BLUE),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_YELLOW : BERRY_COLOR_PINK),
         .size = 110,
         .maxYield = YIELD_RATE(6, 5, 20, 26),
         .minYield = YIELD_RATE(2, 1, 1, 2),
@@ -751,7 +751,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Spelon"),
         .firmness = BERRY_FIRMNESS_SOFT,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_BLUE),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_PINK),
         .size = 133,
         .maxYield = YIELD_RATE(2, 15, 15, 15),
         .minYield = YIELD_RATE(1, 2, 3, 3),
@@ -843,7 +843,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Belue"),
         .firmness = BERRY_FIRMNESS_VERY_SOFT,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_YELLOW : BERRY_COLOR_PINK),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_BLUE),
         .size = 300,
         .maxYield = YIELD_RATE(2, 15, 15, 15),
         .minYield = YIELD_RATE(1, 2, 3, 3),
@@ -1096,7 +1096,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Payapa"),
         .firmness = BERRY_FIRMNESS_SOFT,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_PINK),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_RED),
         .size = 252,
         .maxYield = YIELD_RATE(5, 5, 20, 10),
         .minYield = YIELD_RATE(2, 1, 3, 2),
@@ -1257,7 +1257,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Roseli"),
         .firmness = BERRY_FIRMNESS_HARD,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_PINK),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PINK : BERRY_COLOR_RED),
         .size = 35,
         .maxYield = YIELD_RATE(5, 5, 20, 10),
         .minYield = YIELD_RATE(2, 1, 3, 2),
@@ -1280,7 +1280,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Liechi"),
         .firmness = BERRY_FIRMNESS_VERY_HARD,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_YELLOW : BERRY_COLOR_PINK),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_YELLOW),
         .size = 111,
         .maxYield = YIELD_RATE(2, 5, 10, 13),
         .minYield = YIELD_RATE(1, 1, 1, 2),
@@ -1303,7 +1303,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Ganlon"),
         .firmness = BERRY_FIRMNESS_VERY_HARD,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_RED),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_BLUE),
         .size = 33,
         .maxYield = YIELD_RATE(2, 5, 10, 13),
         .minYield = YIELD_RATE(1, 1, 1, 2),
@@ -1395,7 +1395,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Lansat"),
         .firmness = BERRY_FIRMNESS_SOFT,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PINK : BERRY_COLOR_RED),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_PINK),
         .size = 97,
         .maxYield = YIELD_RATE(2, 5, 5, 7),
         .minYield = YIELD_RATE(1, 1, 1, 1),
@@ -1441,7 +1441,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Enigma"),
         .firmness = BERRY_FIRMNESS_HARD,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_RED : BERRY_COLOR_YELLOW),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_YELLOW),
         .size = 155,
         .maxYield = YIELD_RATE(2, 5, 5, 13),
         .minYield = YIELD_RATE(1, 1, 1, 1),
@@ -1556,7 +1556,7 @@ const struct Berry gBerries[] =
     {
         .name = _("Kee"),
         .firmness = BERRY_FIRMNESS_UNKNOWN,
-        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_PURPLE : BERRY_COLOR_YELLOW),
+        .color = ((OW_BERRY_COLORS == GEN_6_XY) ? BERRY_COLOR_YELLOW : BERRY_COLOR_PINK),
         .size = 0,
         .maxYield = YIELD_RATE(2, 5, 10, 13),
         .minYield = YIELD_RATE(1, 1, 1, 2),
