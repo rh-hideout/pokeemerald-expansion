b8703a8595baa18dd01657c18d27264b35ab7ea5
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-11-28 21:23:54 +0000

    Remove CountFreePaletteSlots

--- src/sprite.c
+++ src/sprite.c
@@ -1653,17 +1653,6 @@ u32 IndexOfSpritePaletteTag(u16 tag)
     return 0xFF;
 }
 
-u8 CountFreePaletteSlots(void)
-{
-    u32 i;
-    u8 count = 0;
-    for (i = gReservedSpritePaletteCount; i < 16; i++)
-        if (sSpritePaletteTags[i] == TAG_NONE)
-            ++count;
-
-    return count;
-}
-
 u16 GetSpritePaletteTagByPaletteNum(u8 paletteNum)
 {
     return sSpritePaletteTags[paletteNum];
