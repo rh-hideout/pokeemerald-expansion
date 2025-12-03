9f0cd5c6a664198c5eb400963447d1f236c1ca9b
Eduardo Quezada <eduardo602002@gmail.com>
2025-12-03 08:52:00 -0300

    Remove useless hold effects

--- test/battle/hold_effect/dragon_scale.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "test/battle.h"
-
-// No effect?
-TO_DO_BATTLE_TEST("TODO: Write Dragon Scale (Hold Effect) test titles")
--- test/battle/hold_effect/upgrade.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "test/battle.h"
-
-// No effect?
-TO_DO_BATTLE_TEST("TODO: Write Upgrade (Hold Effect) test titles")
