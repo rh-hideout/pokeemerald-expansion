dba77d5f378806318f5cf715cd5c749c0323acdd
MathieuGaudet <mathieug@LAPTOP-LV834SOI.localdomain>
2025-12-16 16:49:38 -0500

    First Changes, added new section to Petalburg town

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1163,3 +1163,7 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/MathieuHouse/scripts.inc"
+
+	.include "data/maps/MathieuHouse_2/scripts.inc"
