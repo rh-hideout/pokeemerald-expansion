ffe67f499840ce8d189f9917c615397174c00375
Jamie Foster <jamiefosterandco@gmail.com>
2025-12-18 21:10:50 +0100

    remove unused code

--- include/match_call.h
+++ include/match_call.h
@@ -20,6 +20,5 @@ bool32 SelectMatchCallMessage(int trainerId, u8 *str);
 void LoadMatchCallWindowGfx(u32 windowId, u32 destOffset, u32 paletteId);
 void DrawMatchCallTextBoxBorder(u32 windowId, u32 tileOffset, u32 paletteId);
 void RedrawMatchCallTextBoxBorder(void);
-void StartDebugMatchCall(u32 trainerId);
 
 #endif //GUARD_MATCH_CALL_H
