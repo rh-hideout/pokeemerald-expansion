e62ac5aef637b42e57d88b0f636fe61ef1f05b6d
grintoul1 <liam.rintoul@outlook.com>
2025-12-27 16:38:40 +0000

    Move remaining SUB_HIT tests to test_test_runner.c

--- test/test_test_runner.c
+++ test/test_test_runner.c
@@ -9,11 +9,133 @@ TEST("Tests resume after CRASH")
     f();
 }
 
+SINGLE_BATTLE_TEST("Substitute's HP cost doesn't trigger effects that trigger on damage taken")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_AIR_BALLOON); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SUBSTITUTE, player);
+        MESSAGE("Wobbuffet put in a substitute!");
+        NOT MESSAGE("Wobbuffet's Air Balloon popped!");
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player);
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT, break TRUE")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Level(1); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player, subBreak: TRUE);
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT, break FALSE")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Level(100); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player, subBreak: FALSE);
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT, records damage")
+{
+    u16 damage;
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player, captureDamage: &damage);
+    } THEN {
+        EXPECT_GT(damage, 0);
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT, records damage, break FALSE")
+{
+    u16 damage;
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player, captureDamage: &damage, subBreak: FALSE);
+    } THEN {
+        EXPECT_GT(damage, 0);
+    }
+}
+
+SINGLE_BATTLE_TEST("Substitute hits are detected by SUB_HIT, records damage, break TRUE")
+{
+    u16 damage;
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Level(1); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_SUBSTITUTE); MOVE(opponent, MOVE_SCRATCH); }
+    } SCENE {
+        SUB_HIT(player, captureDamage: &damage, subBreak: TRUE);
+    } THEN {
+        EXPECT_GT(damage, 0);
+    }
+}
+
 TEST("EXPECT_FAIL allows expected failing tests to pass")
 {
     EXPECT_FAIL {
         // Test should fail but be considered a pass
         EXPECT_EQ(1, 2);
+    }
+}
+
+AI_SINGLE_BATTLE_TEST("EXPECT_FAIL detects failures caused by EXPECT_MOVE")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_CHOICE_SCARF); Moves(MOVE_FAKE_OUT, MOVE_BODY_SLAM); }
+    } WHEN {
+        TURN { EXPECT_MOVE(opponent, MOVE_FAKE_OUT); }
+        EXPECT_FAIL {
+            TURN { EXPECT_MOVE(opponent, MOVE_BODY_SLAM); }
+        }
+    }
+}
+
+AI_SINGLE_BATTLE_TEST("EXPECT_FAIL detects failures caused by EXPECT_SWITCH")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_CHOICE_SCARF); Moves(MOVE_FAKE_OUT, MOVE_BODY_SLAM); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { EXPECT_MOVE(opponent, MOVE_FAKE_OUT); }
+        EXPECT_FAIL {
+            TURN { EXPECT_SWITCH(opponent, 1); }
+        }
     }
 }
 
@@ -41,33 +163,6 @@ SINGLE_BATTLE_TEST("EXPECT_FAIL: Incorrect use of SUB_HIT results in test failur
     } EXPECT_FAIL {
         SCENE {
             SUB_HIT(player, subBreak: FALSE);
-        }
-    }
-}
-
-AI_SINGLE_BATTLE_TEST("EXPECT_FAIL detects failures caused by EXPECT_MOVE")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_CHOICE_SCARF); Moves(MOVE_FAKE_OUT, MOVE_BODY_SLAM); }
-    } WHEN {
-        TURN { EXPECT_MOVE(opponent, MOVE_FAKE_OUT); }
-        EXPECT_FAIL {
-            TURN { EXPECT_MOVE(opponent, MOVE_BODY_SLAM); }
-        }
-    }
-}
-
-AI_SINGLE_BATTLE_TEST("EXPECT_FAIL detects failures caused by EXPECT_SWITCH")
-{
-    GIVEN {
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_CHOICE_SCARF); Moves(MOVE_FAKE_OUT, MOVE_BODY_SLAM); }
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { EXPECT_MOVE(opponent, MOVE_FAKE_OUT); }
-        EXPECT_FAIL {
-            TURN { EXPECT_SWITCH(opponent, 1); }
         }
     }
 }
