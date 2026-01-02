b193cb11028410ed11e72d6cdfa6f05056572390
Kildemal <kildemal@protonmail.com>
2026-01-02 15:27:53 +0530

    fix(battle): check hidden power effect instead of move id for counter

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2146,7 +2146,7 @@ static void MoveDamageDataHpUpdate(u32 battler, u32 scriptBattler, const u8 *nex
     //       they are used in combination as general damage trackers for other purposes.
     if (GetConfig(CONFIG_COUNTER_MIRROR_COAT_ALLY) <= GEN_4 || !IsBattlerAlly(battler, gBattlerAttacker))
     {
-        if (IsBattleMovePhysical(gCurrentMove) || (SanitizeMoveId(gCurrentMove) == MOVE_HIDDEN_POWER && GetConfig(CONFIG_HIDDEN_POWER_COUNTER) < GEN_4))
+        if (IsBattleMovePhysical(gCurrentMove) || ((GetConfig(CONFIG_HIDDEN_POWER_COUNTER) < GEN_4) && MOVE_HIDDEN_POWER == GetMoveEffect(gCurrentMove)))
         {
             gProtectStructs[battler].physicalDmg = gBattleStruct->moveDamage[battler] + 1;
             gProtectStructs[battler].physicalBattlerId = gBattlerAttacker;
