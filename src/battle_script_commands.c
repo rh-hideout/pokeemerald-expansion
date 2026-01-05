809c38fb40c05af29dff371c092bc01f356ed218
amiosi <44352097+amiosi@users.noreply.github.com>
2026-01-05 17:03:00 -0500

    requested changes + memento

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -5908,18 +5908,32 @@ static bool32 HandleMoveEndMoveBlock(u32 moveEffect)
         {
             s32 recoil = 0;
             if (B_CRASH_IF_TARGET_IMMUNE == GEN_4 && gBattleStruct->moveResultFlags[gBattlerTarget] & MOVE_RESULT_DOESNT_AFFECT_FOE)
+            {
                 recoil = GetNonDynamaxMaxHP(gBattlerTarget) / 2;
+            }
             if (B_RECOIL_IF_MISS_DMG >= GEN_5)
+            {
                 recoil = GetNonDynamaxMaxHP(gBattlerAttacker) / 2;
+            }
             else if (B_RECOIL_IF_MISS_DMG >= GEN_3)
+            {
                 if ((GetNonDynamaxMaxHP(gBattlerTarget) / 2) < gBattleStruct->moveDamage[gBattlerTarget])
+                {
                     recoil = gBattleStruct->moveDamage[gBattlerTarget];
+                }
                 else
+                {
                     recoil = GetNonDynamaxMaxHP(gBattlerTarget) / 2;
+                }
+            }
             else if (B_RECOIL_IF_MISS_DMG == GEN_2)
+            {
                 recoil = gBattleStruct->moveDamage[gBattlerTarget] / 8;
+            }
             else
+            {
                 recoil = 1;
+            }
             SetPassiveDamageAmount(gBattlerAttacker, recoil);
             BattleScriptCall(BattleScript_RecoilIfMiss);
             effect = TRUE;
@@ -12502,7 +12516,7 @@ static void Cmd_trymemento(void)
 {
     CMD_ARGS(const u8 *failInstr);
 
-    if (B_MEMENTO_FAIL >= GEN_4
+    if (B_MEMENTO_FAIL >= GEN_5
       && (gBattleCommunication[MISS_TYPE] == B_MSG_PROTECTED
         || IsSemiInvulnerable(gBattlerTarget, CHECK_ALL)
         || IsBattlerProtected(gBattlerAttacker, gBattlerTarget, gCurrentMove)
