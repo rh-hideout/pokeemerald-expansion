74f5ff9c848d17d318773b77b0aa89e38e0ee598
amiosi <44352097+amiosi@users.noreply.github.com>
2026-01-05 15:33:43 -0500

    fix if statement nesting

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -5909,10 +5909,10 @@ static bool32 HandleMoveEndMoveBlock(u32 moveEffect)
             s32 recoil = 0;
             if (B_RECOIL_IF_MISS_DMG >= GEN_5)
                 recoil = GetNonDynamaxMaxHP(gBattlerAttacker) / 2;
-            if (B_CRASH_IF_TARGET_IMMUNE == GEN_4 && gBattleStruct->moveResultFlags[gBattlerTarget] & MOVE_RESULT_DOESNT_AFFECT_FOE)
-                recoil = GetNonDynamaxMaxHP(gBattlerTarget) / 2;
             else if (B_RECOIL_IF_MISS_DMG >= GEN_3)
-                if ((GetNonDynamaxMaxHP(gBattlerTarget) / 2) < gBattleStruct->moveDamage[gBattlerTarget])
+                if (B_CRASH_IF_TARGET_IMMUNE == GEN_4 && gBattleStruct->moveResultFlags[gBattlerTarget] & MOVE_RESULT_DOESNT_AFFECT_FOE)
+                    recoil = GetNonDynamaxMaxHP(gBattlerTarget) / 2;
+                else if ((GetNonDynamaxMaxHP(gBattlerTarget) / 2) < gBattleStruct->moveDamage[gBattlerTarget])
                     recoil = gBattleStruct->moveDamage[gBattlerTarget];
                 else
                     recoil = GetNonDynamaxMaxHP(gBattlerTarget) / 2;
