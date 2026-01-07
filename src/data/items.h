b572cc2b2550faedcbc1d8bd7b9abad4f2f92000
amiosi <44352097+amiosi@users.noreply.github.com>
2026-01-07 18:37:42 -0500

    add the type boost value for gen 3 sea incense

--- src/data/items.h
+++ src/data/items.h
@@ -8134,7 +8134,7 @@ const struct Item gItemsInfo[] =
         .name = ITEM_NAME("Sea Incense"),
         .price = (I_PRICE >= GEN_7) ? 2000 : 9600,
         .holdEffect = HOLD_EFFECT_TYPE_POWER,
-        .holdEffectParam = 20,
+        .holdEffectParam = I_POWER_ITEM_BOOST >= GEN_4 ? 20 : 5,
         .description = sSeaIncenseDesc,
         .pocket = POCKET_ITEMS,
         .sortType = ITEM_TYPE_INCENSE,
