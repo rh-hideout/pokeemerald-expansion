4a9a6a5c1d59f33678b0d379b647cd1931eb29e7
Jamie Foster <jamiefosterandco@gmail.com>
2026-01-16 10:28:04 +0100

    change randomweightedarray to use u16

--- src/random.c
+++ src/random.c
@@ -166,7 +166,7 @@ __attribute__((weak, alias("RandomUniformExceptDefault")))
 u32 RandomUniformExcept(enum RandomTag, u32 lo, u32 hi, bool32 (*reject)(u32));
 
 __attribute__((weak, alias("RandomWeightedArrayDefault")))
-u32 RandomWeightedArray(enum RandomTag tag, u32 sum, u32 n, const u8 *weights);
+u32 RandomWeightedArray(enum RandomTag tag, u32 sum, u32 n, const u16 *weights);
 
 __attribute__((weak, alias("RandomElementArrayDefault")))
 const void *RandomElementArray(enum RandomTag tag, const void *array, size_t size, size_t count);
@@ -191,9 +191,10 @@ u32 RandomUniformExceptDefault(enum RandomTag tag, u32 lo, u32 hi, bool32 (*reje
     LOOP_RANDOM_END;
 }
 
-u32 RandomWeightedArrayDefault(enum RandomTag tag, u32 sum, u32 n, const u8 *weights)
+u32 RandomWeightedArrayDefault(enum RandomTag tag, u32 sum, u32 n, const u16 *weights)
 {
     assertf(n > 0);
+    assertf(sum <= MAX_u16);
     s32 i, targetSum;
     targetSum = (sum * Random()) >> 16;
     for (i = 0; i < n - 1; i++)
