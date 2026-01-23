c86f58c669102239b08b0f54da6d5af22b9fb766
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-01-23 19:07:56 +0000

    Cleanup Spawn Anim Function

--- src/field_effect_helpers.c
+++ src/field_effect_helpers.c
@@ -1520,59 +1520,53 @@ u32 FldEff_OWE_SpawnAnim(void)
 {
     u8 spriteId;
     u8 visual;
-    s16 xOffset, yOffset;
+    u8 subpriority = 0;
+    s16 xOffset = 0, yOffset = 0;
     struct SpritePalette palette = gSpritePalette_GeneralFieldEffect0;
 
     switch (gFieldEffectArguments[3])
     {
     case OWE_SPAWN_ANIM_GRASS:
         visual = FLDEFFOBJ_JUMP_TALL_GRASS;
-        xOffset = 0;
         yOffset = 8;
         palette = gSpritePalette_GeneralFieldEffect1;
         break;
 
     case OWE_SPAWN_ANIM_LONG_GRASS:
         visual = FLDEFFOBJ_JUMP_LONG_GRASS;
-        xOffset = 0;
-        yOffset = 0;
         palette = gSpritePalette_GeneralFieldEffect1;
         break;
 
     case OWE_SPAWN_ANIM_WATER:
         visual = FLDEFFOBJ_JUMP_BIG_SPLASH;
-        xOffset = 0;
         yOffset = 8;
         break;
 
     case OWE_SPAWN_ANIM_UNDERWATER:
         visual = FLDEFFOBJ_BUBBLES;
-        xOffset = 0;
-        yOffset = 0;
+        subpriority = 82;
         break;
 
     case OWE_SPAWN_ANIM_CAVE:
         visual = FLDEFFOBJ_GROUND_IMPACT_DUST;
-        xOffset = 0;
         yOffset = 8;
         break;
     
     case OWE_SPAWN_ANIM_SHINY:
     default:
         visual = FLDEFFOBJ_SHINY_SPARKLE;
-        xOffset = 0;
-        yOffset = 0;
+        subpriority = 82;
         break;
     }
 
     FieldEffect_LoadFadedPalette(&palette, COLOR_MAP_DARK_CONTRAST);
     SetSpritePosToOffsetMapCoords((s16 *)&gFieldEffectArguments[0], (s16 *)&gFieldEffectArguments[1], 8, 0);
-    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[visual], gFieldEffectArguments[0] + xOffset, gFieldEffectArguments[1] + yOffset, 82);
+    spriteId = CreateSpriteAtEnd(gFieldEffectObjectTemplatePointers[visual], gFieldEffectArguments[0] + xOffset, gFieldEffectArguments[1] + yOffset, subpriority);
     if (spriteId != MAX_SPRITES)
     {
         struct Sprite *sprite = &gSprites[spriteId];
         sprite->coordOffsetEnabled = TRUE;
-        sprite->oam.priority = 1;
+        sprite->oam.priority = gFieldEffectArguments[2];
     }
     return spriteId;
 }
