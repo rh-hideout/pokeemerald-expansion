be74eaa5288c928b36d192fb2332d429f92bbea3
khbsd <khb.dev@gmail.com>
2026-01-24 19:54:30 -0600

    fix: IsDoubleBattle() usage

--- include/battle.h
+++ include/battle.h
@@ -1165,7 +1165,7 @@ static inline struct PartyState *GetBattlerPartyState(u32 battler)
 
 static inline bool32 IsDoubleBattle(void)
 {
-    return (gBattleTypeFlags & BATTLE_TYPE_MORE_THAN_TWO_BATTLERS);
+    return !!(gBattleTypeFlags & BATTLE_TYPE_MORE_THAN_TWO_BATTLERS);
 }
 
 static inline bool32 IsSpreadMove(enum MoveTarget moveTarget)
