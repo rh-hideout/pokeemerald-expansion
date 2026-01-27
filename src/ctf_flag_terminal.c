762948b75895a206fb62bd000076c3ad245af906
K4m4r0 <kamaro11@hotmail.com>
2026-01-27 18:16:10 +0100

    Bug behoben, der Freeze verursacht bei erneutem Aufrufen des CTF-Terminals

--- src/ctf_flag_terminal.c
+++ src/ctf_flag_terminal.c
@@ -2,13 +2,15 @@
 #include "ctf_flags.h"
 
 #include "event_data.h"          // VarGet / VarSet
-#include "script.h"              // ScriptContext2_Enable/Disable
+#include "script.h"              // ScriptContext_Enable
 #include "naming_screen.h"       // DoNamingScreen, NAMING_SCREEN_*
 #include "constants/vars.h"      // VAR_0x8000, VAR_RESULT
-#include "constants/characters.h"// EOS / CHAR_EOS (je nach Branch)
-#include "overworld.h"           // CB2_ReturnToFieldContinueScript (oft hier deklariert)
+#include "constants/characters.h"// EOS / CHAR_EOS
 
-// Größer als PLAYER_NAME_LENGTH, damit NAMING_SCREEN_CODE nicht overflowen kann
+// Falls dein Branch CB2_ReturnToFieldContinueScript nicht über Header findet,
+// ist diese Forward-Declaration der simpelste Weg.
+void CB2_ReturnToFieldContinueScript(void);
+
 #define CTF_INPUT_BUF_SIZE 16
 
 static u8 sCtfGymId;
@@ -19,27 +21,22 @@ static void CtfTerminal_ReturnFromNamingScreen(void)
     // Ergebnis setzen (1 = korrekt, 0 = falsch)
     VarSet(VAR_RESULT, Ctf_IsGymFlagCorrect(sCtfGymId, sCtfInput) ? 1 : 0);
 
-    // waitstate freigeben und Script fortsetzen
-    ScriptContext2_Disable();
+    // Script/Field wieder aufnehmen (setzt intern den Script-Flow fort)
     CB2_ReturnToFieldContinueScript();
 }
 
-// callnative Ctf_FlagTerminal
-// Erwartet: VAR_0x8000 = gymId (1..8)
-// Setzt:    VAR_RESULT = 1/0
 bool8 Ctf_FlagTerminal(void)
 {
     sCtfGymId = (u8)VarGet(VAR_0x8000);
 
     // Buffer leeren
-    sCtfInput[0] = EOS;
+    sCtfInput[0] = EOS; // falls EOS nicht existiert -> unten Hinweis
 
-    // Script soll per waitstate warten
-    ScriptContext2_Enable();
+    // Script “blocken”, bis Naming Screen zurückkommt
+    ScriptContext_Enable();
 
-    // Wichtig: NAMING_SCREEN_CODE zeigt nicht "Enter your name",
-    // sondern den Code-Prompt (passt besser für Flags).
+    // Nimmt den Code-Prompt statt "Enter your name" (nur für diese Eingabe!)
     DoNamingScreen(NAMING_SCREEN_CODE, sCtfInput, 0, 0, 0, CtfTerminal_ReturnFromNamingScreen);
 
-    return TRUE; // One-shot native: der Script wartet über waitstate auf ScriptContext2_Disable()
+    return TRUE; // one-shot native
 }
