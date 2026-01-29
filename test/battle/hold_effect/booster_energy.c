3305bba11be9f203c38156f338fa5c6ca71d5106
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2026-01-29 12:38:28 +0000

    Fix Booster Energy test

--- test/battle/hold_effect/booster_energy.c
+++ test/battle/hold_effect/booster_energy.c
@@ -230,16 +230,16 @@ SINGLE_BATTLE_TEST("Booster Energy increases special defense by 30% if it is the
     }
 }
 
-SINGLE_BATTLE_TEST("Booster Energy can't be flung if a Paradox species is involved")
+SINGLE_BATTLE_TEST("Booster Energy can't be flung by a Paradox species")
 {
     GIVEN {
         ASSUME(gSpeciesInfo[SPECIES_IRON_MOTH].isParadox == TRUE);
-        PLAYER(SPECIES_IRON_MOTH);
-        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_BOOSTER_ENERGY); }
+        PLAYER(SPECIES_IRON_MOTH) { Item(ITEM_BOOSTER_ENERGY); }
+        OPPONENT(SPECIES_TAPU_KOKO);
     } WHEN {
-        TURN { MOVE(opponent, MOVE_FLING); }
+        TURN { MOVE(player, MOVE_FLING); }
     } SCENE {
-        NOT ANIMATION(ANIM_TYPE_MOVE, MOVE_FLING, opponent);
+        NOT ANIMATION(ANIM_TYPE_MOVE, MOVE_FLING, player);
         MESSAGE("But it failed!");
     }
 }
