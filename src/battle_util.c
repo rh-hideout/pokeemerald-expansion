2a958d7cd02198315db1251d6ff7b74e73427345
GGbond <z2621915516@163.com>
2026-01-30 20:04:54 +0800

    Update

--- src/battle_util.c
+++ src/battle_util.c
@@ -1710,7 +1710,7 @@ u32 CheckMoveLimitations(u32 battler, u8 unusableMoves, u16 check)
             unusableMoves |= 1u << i;
         // Taunt
         else if (check & MOVE_LIMITATION_TAUNT && gDisableStructs[battler].tauntTimer && IsBattleMoveStatus(move)
-              && (B_UPDATED_MOVE_FLAGS < GEN_5 || moveEffect != EFFECT_ME_FIRST))
+              && (B_TAUNT_ME_FIRST < GEN_5 || moveEffect != EFFECT_ME_FIRST))
             unusableMoves |= 1u << i;
         // Imprison
         else if (check & MOVE_LIMITATION_IMPRISON && GetImprisonedMovesCount(battler, move))
