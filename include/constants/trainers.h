32dbfdf538880001ddd43fc53a4d23688238f587
K4m4r0 <kamaro11@hotmail.com>
2026-01-30 01:32:54 +0100

    Trainer Klasse Team Rocket hinzugefügt

--- include/constants/trainers.h
+++ include/constants/trainers.h
@@ -108,13 +108,12 @@
 #define TRAINER_PIC_LEAF                  90
 #define TRAINER_PIC_RS_BRENDAN            91
 #define TRAINER_PIC_RS_MAY                92
-#define TRAINER_PIC_COUNT                 93
 
 //CTF
-#define TRAINER_PIC_ROCKET                94
+#define TRAINER_PIC_ROCKET                93
 
 //Count immer Erhöhen!
-#define TRAINER_PIC_COUNT                 95
+#define TRAINER_PIC_COUNT                 94
 
 // The player back pics are assumed to alternate according to the gender values (MALE/FEMALE)
 #define TRAINER_BACK_PIC_BRENDAN                0
@@ -359,7 +358,7 @@ enum TrainerClassID
     TRAINER_CLASS_PIKE_QUEEN,
     TRAINER_CLASS_PYRAMID_KING,
     TRAINER_CLASS_RS_PROTAG,
-    TRAINER_CLASS_ROCKET, // CTF
+    TRAINER_CLASS_TEAM_ROCKET, // CTF
     TRAINER_CLASS_COUNT
 
 };
