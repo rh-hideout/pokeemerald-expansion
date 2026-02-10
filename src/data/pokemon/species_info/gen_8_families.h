bca76d774a6b2f1ceba0eeff1420cef353f1647c
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-10 15:05:34 -0500

    fix cram form change

--- src/data/pokemon/species_info/gen_8_families.h
+++ src/data/pokemon/species_info/gen_8_families.h
@@ -3159,7 +3159,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .teachableLearnset = sCramorantTeachableLearnset,
         .eggMoveLearnset = sCramorantEggMoveLearnset,
         .formSpeciesIdTable = sCramorantFormSpeciesIdTable,
-        .formChangeTable = sCramorantFormChangeTable,
+        .formChangeTable = sCramorantGulpingFormChangeTable,
     },
 
     [SPECIES_CRAMORANT_GORGING] =
@@ -3216,7 +3216,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .teachableLearnset = sCramorantTeachableLearnset,
         .eggMoveLearnset = sCramorantEggMoveLearnset,
         .formSpeciesIdTable = sCramorantFormSpeciesIdTable,
-        .formChangeTable = sCramorantFormChangeTable,
+        .formChangeTable = sCramorantGorgingFormChangeTable,
     },
 #endif //P_FAMILY_CRAMORANT
 
