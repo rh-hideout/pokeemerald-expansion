dc68484d557a1c554441f4e96904dd9d96e8ac9d
Jamie Foster <jamiefosterandco@gmail.com>
2026-02-24 05:37:43 +0100

    Revert "testing"
    This reverts commit 86b2a4502cdbb81e8b1d1bbe34553ac41485188a.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -124,6 +124,7 @@ gStdScripts::
 gStdScripts_End::
 
 
+	.include "data/maps/PetalburgCity/scripts.inc"
 	.include "data/maps/SlateportCity/scripts.inc"
 	.include "data/maps/MauvilleCity/scripts.inc"
 	.include "data/maps/RustboroCity/scripts.inc"
