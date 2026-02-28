1dc11f4d44988619ecffc054b7b61639efed3191
Meister-anon <user@DESKTOP-TQ65PS9>
2026-02-28 11:36:07 -0500

    update poparraywithBattlers Arg to prevent warning

--- src/battle_main.c
+++ src/battle_main.c
@@ -5150,7 +5150,7 @@ static void TurnValuesCleanUp(bool8 var0)
     ClearDamageCalcResults();
 }
 
-static void PopulateArrayWithBattlers(u8 *battlers)
+static void PopulateArrayWithBattlers(enum BattlerId *battlers)
 {
     for (enum BattlerId i = 0; i < gBattlersCount; i++)
         battlers[i] = i;
