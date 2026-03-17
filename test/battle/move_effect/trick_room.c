0911184d0e21fc195dee68095589b1383a04ec2c
GGbond <z2621915516@163.com>
2026-03-17 11:06:45 +0800

    Fix

--- test/battle/move_effect/trick_room.c
+++ test/battle/move_effect/trick_room.c
@@ -1,4 +1,28 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("TODO: Write Trick Room (Move Effect) test titles")
+ASSUMPTIONS
+{
+    ASSUME(GetMoveEffect(MOVE_TRICK_ROOM) == EFFECT_TRICK_ROOM);
+}
+
+SINGLE_BATTLE_TEST("Trick Room doesn't print its ending message twice when canceled")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Speed(10); Moves(MOVE_TRICK_ROOM, MOVE_CELEBRATE); }
+        OPPONENT(SPECIES_WOBBUFFET) { Speed(1); }
+    } WHEN {
+        TURN { MOVE(player, MOVE_TRICK_ROOM); }
+        TURN { MOVE(player, MOVE_CELEBRATE); }
+        TURN { MOVE(player, MOVE_CELEBRATE); }
+        TURN { MOVE(player, MOVE_CELEBRATE); }
+        TURN { MOVE(player, MOVE_TRICK_ROOM); }
+        TURN { MOVE(player, MOVE_CELEBRATE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TRICK_ROOM, player);
+        MESSAGE("Wobbuffet twisted the dimensions!");
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TRICK_ROOM, player);
+        MESSAGE("The twisted dimensions returned to normal!");
+        NOT MESSAGE("The twisted dimensions returned to normal!");
+    }
+}
