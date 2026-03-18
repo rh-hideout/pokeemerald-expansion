a050c1c76f1d8ae816f3788caa7778ff6dbdb4a8
grintoul1 <liam.rintoul@outlook.com>
2026-03-18 11:38:34 +0100

    Remove PARTNER_DUMMY by adding additional difficulty when TESTING

--- include/constants/battle_partner.h
+++ include/constants/battle_partner.h
@@ -2,10 +2,12 @@
 #ifndef GUARD_CONSTANTS_BATTLE_PARTNERS_H
 #define GUARD_CONSTANTS_BATTLE_PARTNERS_H
 
+#ifdef PARTNER_COUNT
+#undef PARTNER_COUNT
+#endif
+
 #define PARTNER_NONE 0
 #define PARTNER_STEVEN 1
-#define PARTNER_DUMMY 2
-#define PARTNER_COUNT 3
-//Tests need PARTNER_COUNT to be at least 3 so we add a dummy partner
+#define PARTNER_COUNT 2
 
 #endif  // GUARD_CONSTANTS_BATTLE_PARTNERS_H
