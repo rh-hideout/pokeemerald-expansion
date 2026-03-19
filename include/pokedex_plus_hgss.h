5149d2dc7dffc1b98195bcad3569b5a3a3c5e84b
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-03-19 22:41:08 +0000

    Remove `CB2_OpenPokedexPlusHGSS`

--- include/pokedex_plus_hgss.h
+++ include/pokedex_plus_hgss.h
@@ -1,7 +1,6 @@
 #ifndef GUARD_POKEDEX_PLUS_HGSS_H
 #define GUARD_POKEDEX_PLUS_HGSS_H
 
-void CB2_OpenPokedexPlusHGSS(void);
 void Task_DisplayCaughtMonDexPageHGSS(u8);
 
 bool32 TryOpenPokedexPage_HGSS(u8, u8);
