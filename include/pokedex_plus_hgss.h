131986cfc452f0f8510997d413a5eb7ca7586b2a
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2026-03-19 22:46:28 +0000

    Tidy pokedex_plus_hgss.h

--- include/pokedex_plus_hgss.h
+++ include/pokedex_plus_hgss.h
@@ -1,28 +1,24 @@
 #ifndef GUARD_POKEDEX_PLUS_HGSS_H
 #define GUARD_POKEDEX_PLUS_HGSS_H
 
-void Task_DisplayCaughtMonDexPageHGSS(u8);
-
-bool32 TryOpenPokedexPage_HGSS(u8, u8);
-bool32 Task_TryLoadInfoScreen_HGSS(u8);
 bool32 TryInitWindows_HGSS(void);
-
+bool32 TryOpenPokedexPage_HGSS(u8, u8);
+bool32 TryCreateMonListEntry_HGSS(u8 position, u16 b, u16 ignored);
+bool32 TryMoveMonForInfoScreen_HGSS(struct Sprite *sprite);
+bool32 Task_TryLoadInfoScreen_HGSS(u8);
+bool32 TryHandleInfoScreenInput_HGSS(u8 taskId);
+bool32 TryLoadAreaScreen_HGSS(u8 taskId);
+bool32 TrySwitchScreensFromAreaScreen_HGSS(u8 taskId);
+void Task_DisplayCaughtMonDexPageHGSS(u8);
+bool32 TryLoadCryScreen_HGSS(u8 taskId);
+bool32 TrySwitchScreensFromCryScreen_HGSS(u8 taskId);
+bool32 TryLoadSizeScreen_HGSS(u8 taskId);
+bool32 TryLoadPlayArrowPalette_HGSS(bool8 cryPlaying);
+bool32 TryLoadSearchMenu_HGSS(u8 taskId);
 void HandleDestroyStatBars_HGSS(void);
 void HandleDestroyStatBarsBg_HGSS(void);
 void HandleCreateStatBars_HGSS(void);
 void HandleCreateStatBarsDPAD_HGSS(void);
-bool32 TryMoveMonForInfoScreen_HGSS(struct Sprite *sprite);
-bool32 TryLoadSearchMenu_HGSS(u8 taskId);
-bool32 TryLoadCryScreen_HGSS(u8 taskId);
-bool32 TryLoadSizeScreen_HGSS(u8 taskId);
-bool32 TryLoadAreaScreen_HGSS(u8 taskId);
-bool32 TryCreateMonListEntry_HGSS(u8 position, u16 b, u16 ignored);
-
-bool32 TryHandleInfoScreenInput_HGSS(u8 taskId);
-bool32 TrySwitchScreensFromAreaScreen_HGSS(u8 taskId);
-bool32 TrySwitchScreensFromCryScreen_HGSS(u8 taskId);
-bool32 TryLoadPlayArrowPalette_HGSS(bool8 cryPlaying);
-
 void HandleCaughtMonPageTypeIcons_HGSS(void);
 bool32 TryHandleCaughtMonPageFlicker_HGSS(u8 taskId);
 
