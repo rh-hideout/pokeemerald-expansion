0c9b378cb241fc204cdb0ae6a231b81dee6d32f0
FosterProgramming <jamiefosterandco@gmail.com>
2026-03-21 07:32:48 +0100

    Update include/trainer_util.h
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- include/trainer_util.h
+++ include/trainer_util.h
@@ -5,8 +5,9 @@ struct TrainerGenerator
 {
     u8 gender:7;
     u8 isFrontier:1;
-    u8 name[TRAINER_NAME_LENGTH + 1];
     u8 trainerClass;
+    u8 padding;
+    u8 name[TRAINER_NAME_LENGTH + 1];
     struct OriginalTrainerId otID;
     rng_value_t localRngState;
 };
