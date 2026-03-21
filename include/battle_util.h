991f11bec1c642a4b6d84d07dce3c50b2cea3295
AlexOn1ine <alexthenotes@gmail.com>
2026-03-21 10:28:21 +0100

    Fix frame counter in multi battles

--- include/battle_util.h
+++ include/battle_util.h
@@ -121,8 +121,6 @@ struct BattleContext
     enum HoldEffect holdEffectAtk;
     enum HoldEffect holdEffectDef;
 
-    u8 aiTurnOrder[MAX_BATTLERS_COUNT];
-
     // Flags
     u32 isCrit:1;
     u32 randomFactor:1;
