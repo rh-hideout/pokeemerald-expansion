624369d1fbab4de86656961b513a7285077646ea
Jamie Foster <jamiefosterandco@gmail.com>
2026-03-26 21:50:21 +0100

    Add test suport for expected assert fail and inventory management in battle

--- test/test_runner.c
+++ test/test_runner.c
@@ -454,6 +454,9 @@ top:
             case TEST_RESULT_CRASH:
                 result = "CRASH";
                 break;
+            case TEST_RESULT_ASSERT_FAIL:
+                result = "EXPECTED_ASSERT_FAIL";
+                break;
             default:
                 result = "UNKNOWN";
                 break;
@@ -480,6 +483,9 @@ top:
                 Test_MgbaPrintf(":A%s%s\e[0m", color, result);
             else if (gTestRunnerState.result == TEST_RESULT_TODO)
                 Test_MgbaPrintf(":T%s%s\e[0m", color, result);
+            else if (gTestRunnerState.expectedResult == gTestRunnerState.result
+                 && gTestRunnerState.result == TEST_RESULT_ASSERT_FAIL)
+                Test_MgbaPrintf(":E%s%s\e[0m", color, result);
             else if (gTestRunnerState.expectedResult == gTestRunnerState.result
                  && gTestRunnerState.result == TEST_RESULT_CRASH)
                 Test_MgbaPrintf(":E%s%s\e[0m", color, result);
@@ -760,7 +766,7 @@ void Test_ExitWithResult_(enum TestResult result, u32 stopLine, const void *retu
          gTestRunnerState.test->filename, stopLine,
          gTestRunnerState.expectedFailLine, stopLine);
     }
-    
+
     ReinitCallbacks();
     if (gTestRunnerState.state == STATE_REPORT_RESULT
      && gTestRunnerState.result != gTestRunnerState.expectedResult)
