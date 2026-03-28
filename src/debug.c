e07f8f35e2b034a74b0ae89bd911f0e9c118ad93
LinathanZel <linathan@DESKTOP-RVGNQ5E.localdomain>
2026-03-28 13:44:46 -0400

    Debug warp fix

--- src/debug.c
+++ src/debug.c
@@ -1578,6 +1578,7 @@ static void DebugAction_Util_Warp_SelectWarp(u8 taskId)
         DoWarp();
         ResetInitialPlayerAvatarState();
         DebugAction_DestroyExtraWindow(taskId);
+        ScriptContext_Stop();
     }
     else if (JOY_NEW(B_BUTTON))
     {
