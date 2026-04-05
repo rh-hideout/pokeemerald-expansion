dd946eb726379957069831db5ed6ec51cd2ae3aa
kittenchilly <23617175+kittenchilly@users.noreply.github.com>
2026-04-05 12:00:41 -0500

    Gen 6

--- src/data/pokemon/species_info/gen_1_families.h
+++ src/data/pokemon/species_info/gen_1_families.h
@@ -18744,7 +18744,7 @@ const struct SpeciesInfo gSpeciesInfoGen1[] =
             SIZE_32x32,
             SHADOW_SIZE_M,
             TRACKS_FOOT,
-            sAnimTable_Following,
+            sAnimTable_Following_Asym,
             gOverworldPalette_Sylveon,
             gShinyOverworldPalette_Sylveon
         )
--- src/data/pokemon/species_info/gen_3_families.h
+++ src/data/pokemon/species_info/gen_3_families.h
@@ -4714,7 +4714,7 @@ const struct SpeciesInfo gSpeciesInfoGen3[] =
             SIZE_32x32,
             SHADOW_SIZE_M,
             TRACKS_FOOT,
-            sAnimTable_Following,
+            sAnimTable_Following_Asym,
             gOverworldPalette_SableyeMega,
             gShinyOverworldPalette_SableyeMega
         )
@@ -10528,7 +10528,7 @@ const struct SpeciesInfo gSpeciesInfoGen3[] =
             SIZE_32x32,
             SHADOW_SIZE_M,
             TRACKS_FOOT,
-            sAnimTable_Following,
+            sAnimTable_Following_Asym,
             gOverworldPalette_AbsolMega,
             gShinyOverworldPalette_AbsolMega
         )
