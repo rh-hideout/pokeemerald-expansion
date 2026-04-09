6571b9ad7b789102bc02426553f936a2c9c93a63
luuma <31407427+luuma@users.noreply.github.com>
2026-04-09 11:11:22 +0100

    kittenchilly commit

--- src/data/abilities.h
+++ src/data/abilities.h
@@ -2414,17 +2414,52 @@ const struct AbilityInfo gAbilitiesInfo[ABILITIES_COUNT] =
         .cantBeTraced = TRUE,
     },
 
+
+    [ABILITY_PIERCING_DRILL] =
+    {
+        .name = _("Piercing Drill"),
+        .description = COMPOUND_STRING("Unimplemented."),
+    },
+
     [ABILITY_DRAGONIZE] =
     {
         .name = _("Dragonize"),
-        .description = COMPOUND_STRING("Normal moves become Dragon."),
-        .aiRating = 8,
+        .description = COMPOUND_STRING("Normal moves turn Dragon."),
+    },
+
+    [ABILITY_313] =
+    {
+        .name = _("-------"),
+        .description = COMPOUND_STRING("No special ability."),
+    },
+
+    [ABILITY_314] =
+    {
+        .name = _("-------"),
+        .description = COMPOUND_STRING("No special ability."),
     },
 
     [ABILITY_MEGA_SOL] =
     {
         .name = _("Mega Sol"),
-        .description = COMPOUND_STRING("Behaves like under sun."),
-        .aiRating = 7,
+        .description = COMPOUND_STRING("Acts like under sun."),
+    },
+
+    [ABILITY_316] =
+    {
+        .name = _("-------"),
+        .description = COMPOUND_STRING("No special ability."),
+    },
+
+    [ABILITY_317] =
+    {
+        .name = _("-------"),
+        .description = COMPOUND_STRING("No special ability."),
+    },
+
+    [ABILITY_SPICY_SPRAY] =
+    {
+        .name = _("Spicy Spray"),
+        .description = COMPOUND_STRING("Unimplemented."),
     },
 };
