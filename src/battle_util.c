16abbc61750ba35d5a9d56028ada98ed87aa9802
Martin Griffin <martinrgriffin@gmail.com>
2026-04-11 21:40:57 +0100

    Detect (some) uninitialized battle buffer expansion
    At points where we know the buffers should be considered uninitialized
    we overwrite them with a poison value which trips an 'assertf' when
    expanded.
    
    imo poison is a hacky workaround to a bad API, but overhauling the way
    battle strings are handled is a ton of work, and this might get a
    surprising amount of bang for its buck. It caught two strings on master
    (which don't really matter because they're replaced in #9655), and it
    would have caught #9747.

--- src/battle_util.c
+++ src/battle_util.c
@@ -488,6 +488,8 @@ void HandleAction_UseMove(void)
     }
 
     gCurrentActionFuncId = B_ACTION_EXEC_SCRIPT;
+
+    BattleStringPoisonBuffers();
 }
 
 void HandleAction_Switch(void)
@@ -535,6 +537,8 @@ void HandleAction_UseItem(void)
 
     gBattlescriptCurrInstr = gBattlescriptsForUsingItem[GetItemBattleUsage(gLastUsedItem) - 1];
     gCurrentActionFuncId = B_ACTION_EXEC_SCRIPT;
+
+    BattleStringPoisonBuffers();
 }
 
 bool32 TryRunFromBattle(enum BattlerId battler)
@@ -9036,9 +9040,15 @@ enum ImmunityHealStatusOutcome TryImmunityAbilityHealStatus(enum BattlerId battl
         break;
     case ABILITY_OBLIVIOUS:
         if (gBattleMons[battler].volatiles.infatuation)
+        {
+            StringCopy(gBattleTextBuff1, COMPOUND_STRING("infatuation"));
             outcome = IMMUNITY_INFATUATION_CLEARED;
+        }
         else if (GetConfig(B_OBLIVIOUS_TAUNT) >= GEN_6 && gBattleMons[battler].volatiles.tauntTimer != 0)
+        {
+            StringCopy(gBattleTextBuff1, COMPOUND_STRING("taunt"));
             outcome = IMMUNITY_TAUNT_CLEARED;
+        }
         break;
     default:
         break;
