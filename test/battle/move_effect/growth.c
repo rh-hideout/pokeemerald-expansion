37be9822aa0d202f192fed05ec218140d5a207db
luuma <31407427+luuma@users.noreply.github.com>
2026-04-11 16:25:38 +0100

    Update growth.c

--- test/battle/move_effect/growth.c
+++ test/battle/move_effect/growth.c
@@ -1,46 +1,6 @@
 #include "global.h"
 #include "test/battle.h"
 
-SINGLE_BATTLE_TEST("Growth increases Sp. Atk and Sp. Def by 1 stage (Gen 1)") // Equivalent to raising Special
-{
-    KNOWN_FAILING; // fails because the moveeffect is built as gen 5
-    GIVEN {
-        WITH_CONFIG(B_GROWTH_STAT_RAISE, GEN_1);
-        //ASSUME(GetMoveEffect(MOVE_GROWTH) == EFFECT_SPECIAL_ATTACK_UP); //This is commented because the test runner notices it's failed before noticing KNOWN_FAILING
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { MOVE(player, MOVE_SUNNY_DAY); }
-        TURN { MOVE(player, MOVE_GROWTH); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_GROWTH, player);
-        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, player);
-    } THEN {
-        EXPECT_EQ(player->statStages[STAT_SPATK], DEFAULT_STAT_STAGE + 1);
-        EXPECT_EQ(player->statStages[STAT_SPDEF], DEFAULT_STAT_STAGE + 1);
-    }
-}
-
-SINGLE_BATTLE_TEST("Growth increases Sp. Atk by 1 stage (Gen 2-4)")
-{
-    KNOWN_FAILING; // fails because the moveeffect is built according to config in the test runner
-    GIVEN {
-        WITH_CONFIG(B_GROWTH_STAT_RAISE, GEN_2);
-        //ASSUME(GetMoveEffect(MOVE_GROWTH) == EFFECT_SPECIAL_ATTACK_UP); //This is commented because the test runner notices it's failed before noticing KNOWN_FAILING
-        PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET);
-    } WHEN {
-        TURN { MOVE(player, MOVE_SUNNY_DAY); }
-        TURN { MOVE(player, MOVE_GROWTH); }
-    } SCENE {
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_GROWTH, player);
-        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, player);
-    } THEN {
-        EXPECT_EQ(player->statStages[STAT_SPATK], DEFAULT_STAT_STAGE + 1);
-    }
-}
-
-
 SINGLE_BATTLE_TEST("Growth increases Attack and Sp. Atk by 1 stage or 2 stages under Sun (Gen 5+)")
 {
     enum Move move;
