2492f3966836698eb7825c1ff7859d33e741d4df
Pawkkie <moog272@gmail.com>
2026-04-13 17:34:45 -0400

    spacing

--- test/battle/ai/ai_thinking_time.c
+++ test/battle/ai/ai_thinking_time.c
@@ -7,7 +7,7 @@
 #define AI_FRAME_CEILING_DOUBLES_SMART_TRAINER                  36
 #define AI_FRAME_CEILING_STEVEN_MULTI                           26
 #define AI_FRAME_CEILING_STEVEN_MULTI_SMART_TRAINER             30
-#define AI_FRAME_CEILING_CHECK                                          FALSE // If TRUE, forces all thinking time tests to fail. Useful for printing all actual frame times to console by running the tests
+#define AI_FRAME_CEILING_CHECK                                  FALSE // If TRUE, forces all thinking time tests to fail. Useful for printing all actual frame times to console by running the tests
 
 AI_SINGLE_BATTLE_TEST("AI thinking time doesn't explode (singles, no flags)")
 {
