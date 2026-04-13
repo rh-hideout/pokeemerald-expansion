0050f8c23e7a4d94e911c0c34f630c7ddbca359b
GGbond <z2621915516@163.com>
2026-04-13 19:15:09 +0800

    Update ring_target.c

--- test/battle/hold_effect/ring_target.c
+++ test/battle/hold_effect/ring_target.c
@@ -1,4 +1,39 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("TODO: Write Ring Target (Hold Effect) test titles")
+ASSUMPTIONS
+{
+    ASSUME(gItemsInfo[ITEM_RING_TARGET].holdEffect == HOLD_EFFECT_RING_TARGET);
+}
+
+SINGLE_BATTLE_TEST("Ring Target lets Ground-type moves hit ungrounded Flying-type holders")
+{
+    GIVEN {
+        ASSUME(GetMoveType(MOVE_EARTHQUAKE) == TYPE_GROUND);
+        ASSUME(GetSpeciesType(SPECIES_PIDGEY, 0) == TYPE_FLYING || GetSpeciesType(SPECIES_PIDGEY, 1) == TYPE_FLYING);
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_PIDGEY) { Item(ITEM_RING_TARGET); }
+    } WHEN {
+        TURN { MOVE(player, MOVE_EARTHQUAKE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_EARTHQUAKE, player);
+        HP_BAR(opponent);
+        NOT MESSAGE("It doesn't affect the opposing Pidgey…");
+    }
+}
+
+SINGLE_BATTLE_TEST("Ring Target lets Normal-type moves hit Ghost-type holders")
+{
+    GIVEN {
+        ASSUME(GetMoveType(MOVE_TACKLE) == TYPE_NORMAL);
+        ASSUME(GetSpeciesType(SPECIES_GASTLY, 0) == TYPE_GHOST || GetSpeciesType(SPECIES_GASTLY, 1) == TYPE_GHOST);
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_GASTLY) { Item(ITEM_RING_TARGET); }
+    } WHEN {
+        TURN { MOVE(player, MOVE_TACKLE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_TACKLE, player);
+        HP_BAR(opponent);
+        NOT MESSAGE("It doesn't affect the opposing Misdreavus…");
+    }
+}
