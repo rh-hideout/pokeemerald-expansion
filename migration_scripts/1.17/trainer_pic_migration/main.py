2f0d8128cd7daae51b2e87c4652deb9b030da368
AlexOn1ine <alexthenotes@gmail.com>
2026-04-15 10:07:45 +0200

    Adds migration script for trainer pic refactor

---
+++ migration_scripts/1.17/trainer_pic_migration/main.py
@@ -0,0 +1,87 @@
+import os
+
+
+def check_root_folder():
+    if not os.path.exists("Makefile"):
+        print("Please run this script from your root folder.")
+        quit()
+
+
+def read_source_file():
+    lines = []
+    with open("src/data/graphics/trainers.h", "r") as source:
+        parse = False
+        for line in source:
+            if "const struct TrainerSprite gTrainerSprites[]" in line:
+                parse = True
+                continue
+            if parse:
+                if line.strip() == "};":
+                    break
+                lines.append(line)
+    return lines
+
+
+def parse_trainer_entry(line):
+    if "TRAINER_SPRITE" not in line:
+        return None, None
+
+    line = line.replace("TRAINER_SPRITE(", "").rstrip("), \n")
+    lst = line.split(",")
+
+    enum = lst[0].strip().replace("FRONT_", "")
+    lst.pop(0)
+
+    entries = ", ".join(entry.strip() for entry in lst)
+    pic_content = f'{entries}'
+
+    return enum, pic_content
+
+
+def generate_enums(lines):
+    with open("migration_scripts/1.17/trainer_pic_migration/enums.h", "w") as output:
+        output.write(f"enum __attribute__((packed)) TrainerPicID\n")
+        output.write(f"{{\n")
+
+        for line in lines:
+            enum, pic_content = parse_trainer_entry(line)
+            if enum is None:
+                output.write(f"    TRAINER_PIC_NONE,\n")
+                continue
+            output.write(f"    {enum},\n")
+
+        output.write(f"}};")
+
+
+def generate_table(lines):
+    with open("migration_scripts/1.17/trainer_pic_migration/table.h", "w") as output:
+        output.write(f'const struct TrainerPicInfo gTrainerPicInfo[TRAINER_PIC_COUNT] =\n')
+        output.write(f'{{\n')
+
+        for line in lines:
+            enum, pic_content = parse_trainer_entry(line)
+
+            if enum is None:
+                output.write(f"    [TRAINER_PIC_NONE] =\n")
+                output.write("    {\n")
+                output.write(f"        .frontPic = TRAINER_FRONT_PIC(gTrainerFrontPic_None, gTrainerPalette_None),\n")
+                output.write("    },\n")
+                continue
+
+            output.write(f"    [{enum}] =\n")
+            output.write("    {\n")
+            output.write(f"        .frontPic = TRAINER_FRONT_PIC({pic_content}),\n")
+            output.write("    },\n")
+
+        output.write(f'}};')
+
+
+def main():
+    check_root_folder()
+    lines = read_source_file()
+    generate_enums(lines)
+    generate_table(lines)
+
+
+if __name__ == "__main__":
+    main()
