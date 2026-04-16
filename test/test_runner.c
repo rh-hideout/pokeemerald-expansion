9180ee0113376c88fb3c5d27336827136cfb5fab
Martin Griffin <martinrgriffin@gmail.com>
2026-04-16 11:02:43 +0100

    Cleanup whitespace

--- test/test_runner.c
+++ test/test_runner.c
@@ -161,7 +161,7 @@ void TestRunner_CheckMemory(void)
                         {
                             Test_MgbaPrintf("%s: %d bytes not freed", location, block->size);
                             gTestRunnerState.result = TEST_RESULT_FAIL;
-       
+
                             if (gTestRunnerState.expectedFailState == EXPECT_FAIL_OPEN)
                                 gTestRunnerState.expectedFailState = EXPECT_FAIL_SUCCESS;
                             break;
@@ -257,7 +257,7 @@ top:
 
             if (gPersistentTestRunnerState.expectCrash)
                 gTestRunnerState.expectedResult = TEST_RESULT_CRASH;
-            
+
             gTestRunnerState.expectedFailLine = 0;
             gTestRunnerState.expectedFailState = NO_EXPECT_FAIL;
         }
@@ -763,7 +763,7 @@ void Test_ExitWithResult_(enum TestResult result, u32 stopLine, const void *retu
          gTestRunnerState.test->filename, stopLine,
          gTestRunnerState.expectedFailLine, stopLine);
     }
-    
+
     ReinitCallbacks();
     if (gTestRunnerState.state == STATE_REPORT_RESULT
      && gTestRunnerState.result != gTestRunnerState.expectedResult)
