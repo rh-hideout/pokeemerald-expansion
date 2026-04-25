2ef255c2da8769b06f6b78f781d1cee5a34a8f25
Jamie Foster <jamiefosterandco@gmail.com>
2026-04-25 16:06:42 +0200

    Add some tests

--- src/ow_abilities.c
+++ src/ow_abilities.c
@@ -81,7 +81,7 @@ static UNUSED bool32 IsTrueIfUndiscoveredEggGroup(enum Species species)
     return (gSpeciesInfo[species].eggGroups[0] == EGG_GROUP_NO_EGGS_DISCOVERED);
 }
 
-static bool32 DoesLeadingMonHaveAbilityEffect(const enum Ability *abilityArray)
+bool32 DoesLeadingMonHaveAbilityEffect(const enum Ability *abilityArray)
 {
     if (GetMonData(&gParties[B_TRAINER_0][0], MON_DATA_SANITY_IS_EGG))
         return FALSE;
@@ -94,7 +94,7 @@ static bool32 DoesLeadingMonHaveAbilityEffect(const enum Ability *abilityArray)
     return FALSE;
 }
 
-static bool32 DoesPartyMemberMonHaveAbilityEffect(const enum Ability *abilityArray)
+UNUSED bool32 DoesPartyMemberHaveAbilityEffect(const enum Ability *abilityArray)
 {
     for (u32 j = 0; j < gPartiesCount[B_TRAINER_0]; j++)
     {
