a17ddf16e9c31f459b93086b3d7f220f8e742315
dylanfalzone1 <dylanfalzone31@gmail.com>
2026-05-02 06:58:49 -0400

    Added HnS vars
    Frlg actually doesn't gate their vars, just places ones with identical names at the same addresses to prevent issues. We did the same here.

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -2,6 +2,7 @@
 #define GUARD_CONSTANTS_VARS_H
 
 #include "constants/vars_frlg.h"
+#include "constants/vars_hns.h"
 
 #define VARS_START 0x4000
 
---
+++ include/constants/vars_hns.h
@@ -0,0 +1,243 @@
+#ifndef GUARD_CONSTANTS_VARS_HNS_H
+#define GUARD_CONSTANTS_VARS_HNS_H
+
+// Vars shared with Emerald (same name, same value)
+#define VAR_ICE_STEP_COUNT                               0x4022
+#define VAR_STARTER_MON                                  0x4023
+#define VAR_FRONTIER_MANIAC_FACILITY                     0x402F
+#define VAR_FRONTIER_GAMBLER_AMOUNT_BET                  0x4032
+#define VAR_FRONTIER_GAMBLER_STATE                       0x4033
+#define VAR_POKELOT_PRIZE_ITEM                           0x4045
+#define VAR_POKELOT_PRIZE_PLACE                          0x404D
+#define VAR_CONTEST_HALL_STATE                           0x4086
+#define VAR_SAFARI_ZONE_STATE                            0x40A4
+#define VAR_BRAVO_TRAINER_BATTLE_TOWER_ON                0x40BC
+#define VAR_FOSSIL_RESURRECTION_STATE                    0x40C4
+#define VAR_WHICH_FOSSIL_REVIVED                         0x40C5
+#define VAR_FRONTIER_BATTLE_MODE                         0x40CE
+#define VAR_FRONTIER_FACILITY                            0x40CF
+#define VAR_HAS_ENTERED_BATTLE_FRONTIER                  0x40D0
+#define VAR_SCOTT_STATE                                  0x40D1
+#define VAR_TRAINER_HILL_IS_ACTIVE                       0x40D6
+
+// HnS-unique vars
+#define VAR_1                                            0x4050
+#define VAR_2                                            0x4051
+#define VAR_3                                            0x4052
+#define VAR_AZALEA_TOWN_STATE                            0x4053
+#define VAR_BAOBA_QUEST_STATE                            0x4054
+#define VAR_BILLS_GRANDPA                                0x4055
+#define VAR_BLACKTHORN_CITY_STATE                        0x4056
+#define VAR_BLACKTHORN_GYM_STATE                         0x4057
+#define VAR_BUG_CONTEST_STATE                            0x4058
+#define VAR_CERULEAN_CITY_STATE                          0x4059
+#define VAR_CHERRYGROVE_CITY_STATE                       0x405A
+#define VAR_CIANWOOD_CITY_STATE                          0x405B
+#define VAR_CINNABAR_ISLAND_STATE                        0x405C
+#define VAR_CLEFAIRY_DOLL_STATE                          0x405D
+#define VAR_COMPLETED_HO_OH                              0x405E
+#define VAR_COMPLETED_LUGIA                              0x405F
+#define VAR_DRAGONS_DEN_QUIZ                             0x4060
+#define VAR_ECRUTEAK_CITY_STATE                          0x4061
+#define VAR_ECRUTEAK_CITY_THEATER                        0x4062
+#define VAR_ELECTRODES_FAINTED                           0x4063
+#define VAR_ELEVATOR                                     0x4064
+#define VAR_FAN_CLUB_CLEFAIRY                            0x4065
+#define VAR_FAN_CLUB_GUY                                 0x4066
+#define VAR_FUCHSIA_CITY_STATE                           0x4067
+#define VAR_GETFLY                                       0x4068
+#define VAR_GOLDENROD_CITY_STATE                         0x4069
+#define VAR_ICE_PATH_STATE                               0x406A
+#define VAR_ILEX_FOREST_FARFETCHD                        0x406B
+#define VAR_KANTO_ROCKET_STORY_STATE                     0x406C
+#define VAR_KANTO_SAFARI_ZONE_PROGRESS                   0x406D
+#define VAR_KENYA                                        0x406E
+#define VAR_LAKE_OF_RAGE_FISHERMAN                       0x406F
+#define VAR_LEAGUE_STATE                                 0x4070
+#define VAR_LUGIA_OR_HOOH                                0x4071
+#define VAR_MAHOGANY_TOWN_STATE                          0x4072
+#define VAR_MT_SILVER_STATE                              0x4073
+#define VAR_NEWBARKTOWN_LABSTATE                         0x4074
+#define VAR_NEWBARK_TOWN_STATE                           0x4075
+#define VAR_NUM_BADGES                                   0x4076
+#define VAR_OLIVINE_CITY_STATE                           0x4077
+#define VAR_PALLETTOWN_LABSTATE                          0x4078
+#define VAR_PEWTER_CITY_STATE                            0x4079
+#define VAR_ROCKET_HIDEOUT_STATUE_1                      0x407A
+#define VAR_ROCKET_HIDEOUT_STATUE_2                      0x407B
+#define VAR_ROCKET_HIDEOUT_STATUE_3                      0x407C
+#define VAR_ROCKET_HIDEOUT_STATUE_4                      0x407D
+#define VAR_ROCKET_HIDEOUT_STATUE_5                      0x407E
+#define VAR_ROCKET_PASSWORD                              0x407F
+#define VAR_ROUTE22_GIOVANNI_STATE                       0x4080
+#define VAR_ROUTE27_BAOBA_CALL                           0x4081
+#define VAR_ROUTE27_STATE                                0x4082
+#define VAR_ROUTE39_BARN                                 0x4083
+#define VAR_RUINSOFALPH_STATE                            0x4084
+#define VAR_SAFARI_ZONE_GATE_STATE                       0x4085
+#define VAR_SAFFRON_CITY_STATE                           0x4087
+#define VAR_SHUCKIE                                      0x4088
+#define VAR_SLOWPOKE_TAIL                                0x4089
+#define VAR_SPROUT_TOWER                                 0x408A
+#define VAR_SSAQUA_STATE                                 0x408B
+#define VAR_SUICUNE_ENCOUNTERS                           0x408C
+#define VAR_TOHJO_FALLS_GIOVANNI_STATE                   0x408D
+#define VAR_TRAIN                                        0x408E
+#define VAR_VERMILION_CITY_STATE                         0x408F
+#define VAR_VIOLET_CITY_KIMONO_GIRL                      0x4090
+#define VAR_VIOLET_CITY_STATE                            0x4091
+
+// Unused HnS vars
+#define VAR_UNUSED_HNS_0x4092                            0x4092
+#define VAR_UNUSED_HNS_0x4093                            0x4093
+#define VAR_UNUSED_HNS_0x4094                            0x4094
+#define VAR_UNUSED_HNS_0x4095                            0x4095
+#define VAR_UNUSED_HNS_0x4096                            0x4096
+#define VAR_UNUSED_HNS_0x4097                            0x4097
+#define VAR_UNUSED_HNS_0x4098                            0x4098
+#define VAR_UNUSED_HNS_0x4099                            0x4099
+#define VAR_UNUSED_HNS_0x409A                            0x409A
+#define VAR_UNUSED_HNS_0x409B                            0x409B
+#define VAR_UNUSED_HNS_0x409C                            0x409C
+#define VAR_UNUSED_HNS_0x409D                            0x409D
+#define VAR_UNUSED_HNS_0x409E                            0x409E
+#define VAR_UNUSED_HNS_0x409F                            0x409F
+#define VAR_UNUSED_HNS_0x40A0                            0x40A0
+#define VAR_UNUSED_HNS_0x40A1                            0x40A1
+#define VAR_UNUSED_HNS_0x40A2                            0x40A2
+#define VAR_UNUSED_HNS_0x40A3                            0x40A3
+#define VAR_UNUSED_HNS_0x40A5                            0x40A5
+#define VAR_UNUSED_HNS_0x40A6                            0x40A6
+#define VAR_UNUSED_HNS_0x40A7                            0x40A7
+#define VAR_UNUSED_HNS_0x40A8                            0x40A8
+#define VAR_UNUSED_HNS_0x40A9                            0x40A9
+#define VAR_UNUSED_HNS_0x40AA                            0x40AA
+#define VAR_UNUSED_HNS_0x40AB                            0x40AB
+#define VAR_UNUSED_HNS_0x40AC                            0x40AC
+#define VAR_UNUSED_HNS_0x40AD                            0x40AD
+#define VAR_UNUSED_HNS_0x40AE                            0x40AE
+#define VAR_UNUSED_HNS_0x40AF                            0x40AF
+#define VAR_UNUSED_HNS_0x40B0                            0x40B0
+#define VAR_UNUSED_HNS_0x40B1                            0x40B1
+#define VAR_UNUSED_HNS_0x40B2                            0x40B2
+#define VAR_UNUSED_HNS_0x40B3                            0x40B3
+#define VAR_UNUSED_HNS_0x40B4                            0x40B4
+#define VAR_UNUSED_HNS_0x40B5                            0x40B5
+#define VAR_UNUSED_HNS_0x40B6                            0x40B6
+#define VAR_UNUSED_HNS_0x40B7                            0x40B7
+#define VAR_UNUSED_HNS_0x40B8                            0x40B8
+#define VAR_UNUSED_HNS_0x40B9                            0x40B9
+#define VAR_UNUSED_HNS_0x40BA                            0x40BA
+#define VAR_UNUSED_HNS_0x40BB                            0x40BB
+#define VAR_UNUSED_HNS_0x40BD                            0x40BD
+#define VAR_UNUSED_HNS_0x40BE                            0x40BE
+#define VAR_UNUSED_HNS_0x40BF                            0x40BF
+#define VAR_UNUSED_HNS_0x40C0                            0x40C0
+#define VAR_UNUSED_HNS_0x40C1                            0x40C1
+#define VAR_UNUSED_HNS_0x40C2                            0x40C2
+#define VAR_UNUSED_HNS_0x40C3                            0x40C3
+#define VAR_UNUSED_HNS_0x40C6                            0x40C6
+#define VAR_UNUSED_HNS_0x40C7                            0x40C7
+#define VAR_UNUSED_HNS_0x40C8                            0x40C8
+#define VAR_UNUSED_HNS_0x40C9                            0x40C9
+#define VAR_UNUSED_HNS_0x40CA                            0x40CA
+#define VAR_UNUSED_HNS_0x40CB                            0x40CB
+#define VAR_UNUSED_HNS_0x40CC                            0x40CC
+#define VAR_UNUSED_HNS_0x40CD                            0x40CD
+#define VAR_UNUSED_HNS_0x40D2                            0x40D2
+#define VAR_UNUSED_HNS_0x40D3                            0x40D3
+#define VAR_UNUSED_HNS_0x40D4                            0x40D4
+#define VAR_UNUSED_HNS_0x40D5                            0x40D5
+#define VAR_UNUSED_HNS_0x40D7                            0x40D7
+#define VAR_UNUSED_HNS_0x40D8                            0x40D8
+#define VAR_UNUSED_HNS_0x40D9                            0x40D9
+#define VAR_UNUSED_HNS_0x40DA                            0x40DA
+#define VAR_UNUSED_HNS_0x40DB                            0x40DB
+#define VAR_UNUSED_HNS_0x40DC                            0x40DC
+#define VAR_UNUSED_HNS_0x40DD                            0x40DD
+#define VAR_UNUSED_HNS_0x40DE                            0x40DE
+#define VAR_UNUSED_HNS_0x40DF                            0x40DF
+#define VAR_UNUSED_HNS_0x40E0                            0x40E0
+#define VAR_UNUSED_HNS_0x40E1                            0x40E1
+#define VAR_UNUSED_HNS_0x40E2                            0x40E2
+#define VAR_UNUSED_HNS_0x40E3                            0x40E3
+#define VAR_UNUSED_HNS_0x40E4                            0x40E4
+#define VAR_UNUSED_HNS_0x40E5                            0x40E5
+#define VAR_UNUSED_HNS_0x40E6                            0x40E6
+#define VAR_UNUSED_HNS_0x40E7                            0x40E7
+#define VAR_UNUSED_HNS_0x40E8                            0x40E8
+#define VAR_UNUSED_HNS_0x40E9                            0x40E9
+#define VAR_UNUSED_HNS_0x40EA                            0x40EA
+#define VAR_UNUSED_HNS_0x40EB                            0x40EB
+#define VAR_UNUSED_HNS_0x40EC                            0x40EC
+#define VAR_UNUSED_HNS_0x40ED                            0x40ED
+#define VAR_UNUSED_HNS_0x40EE                            0x40EE
+#define VAR_UNUSED_HNS_0x40EF                            0x40EF
+#define VAR_UNUSED_HNS_0x40F0                            0x40F0
+#define VAR_UNUSED_HNS_0x40F1                            0x40F1
+#define VAR_UNUSED_HNS_0x40F2                            0x40F2
+#define VAR_UNUSED_HNS_0x40F3                            0x40F3
+#define VAR_UNUSED_HNS_0x40F4                            0x40F4
+#define VAR_UNUSED_HNS_0x40F5                            0x40F5
+#define VAR_UNUSED_HNS_0x40F6                            0x40F6
+#define VAR_UNUSED_HNS_0x40F7                            0x40F7
+#define VAR_UNUSED_HNS_0x40F8                            0x40F8
+#define VAR_UNUSED_HNS_0x40F9                            0x40F9
+#define VAR_UNUSED_HNS_0x40FA                            0x40FA
+#define VAR_UNUSED_HNS_0x40FB                            0x40FB
+#define VAR_UNUSED_HNS_0x40FC                            0x40FC
+#define VAR_UNUSED_HNS_0x40FD                            0x40FD
+#define VAR_UNUSED_HNS_0x40FE                            0x40FE
+#define VAR_UNUSED_HNS_0x40FF                            0x40FF
+
+#define VARS_END                                         0x40FF
+#define VARS_COUNT                                       (VARS_END - VARS_START + 1)
+
+#define SPECIAL_VARS_START            0x8000
+// special vars
+// They are commonly used as parameters to commands, or return values from commands.
+#define VAR_0x8000                    0x8000
+#define VAR_0x8001                    0x8001
+#define VAR_0x8002                    0x8002
+#define VAR_0x8003                    0x8003
+#define VAR_0x8004                    0x8004
+#define VAR_0x8005                    0x8005
+#define VAR_0x8006                    0x8006
+#define VAR_0x8007                    0x8007
+#define VAR_0x8008                    0x8008
+#define VAR_0x8009                    0x8009
+#define VAR_0x800A                    0x800A
+#define VAR_0x800B                    0x800B
+#define VAR_FACING                    0x800C
+#define VAR_RESULT                    0x800D
+#define VAR_ITEM_ID                   0x800E
+#define VAR_LAST_TALKED               0x800F
+#define VAR_CONTEST_RANK              0x8010
+#define VAR_CONTEST_CATEGORY          0x8011
+#define VAR_MON_BOX_ID                0x8012
+#define VAR_MON_BOX_POS               0x8013
+#define VAR_UNUSED_0x8014             0x8014
+#define VAR_TRAINER_BATTLE_OPPONENT_A 0x8015 // Alias of TRAINER_BATTLE_PARAM.opponentA
+
+#define SPECIAL_VARS_END              0x8015
+
+// If an overworld trigger uses this pseudo-variable as the trigger check,
+// then the script will be run using RunScriptImmediately instead of in the
+// global script context. This means it will run faster, but cannot do any
+// cutscenes nor call a wait command. Used for weather effects in vanilla.
+#define TRIGGER_RUN_IMMEDIATELY   0
+
+// Temp var aliases
+#define VAR_TEMP_CHALLENGE_STATUS  VAR_TEMP_0
+
+#define VAR_TEMP_MIXED_RECORDS         VAR_TEMP_0
+#define VAR_TEMP_RECORD_MIX_GIFT_ITEM  VAR_TEMP_1
+
+#define VAR_TEMP_PLAYING_PYRAMID_MUSIC  VAR_TEMP_E
+
+#define VAR_TEMP_FRONTIER_TUTOR_SELECTION  VAR_TEMP_D
+#define VAR_TEMP_FRONTIER_TUTOR_ID         VAR_TEMP_E
+
+#define VAR_TEMP_TRANSFERRED_SPECIES  VAR_TEMP_1
+
+#endif // GUARD_CONSTANTS_VARS_HNS_H
