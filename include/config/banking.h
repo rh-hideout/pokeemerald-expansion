973ed0b5d5293b1cf363276f5b47b65b74b99d5c
Kildemal <kildemal@protonmail.com>
2026-05-13 18:05:29 +0530

    chore(banking): disable banking features by default

--- include/config/banking.h
+++ include/config/banking.h
@@ -1,7 +1,7 @@
 #ifndef GUARD_CONFIG_BANKING_H
 #define GUARD_CONFIG_BANKING_H
 
-#define SAVINGS_ENABLED          TRUE // Enables the GSC/HGSS savings feature
+#define SAVINGS_ENABLED          FALSE // Enables the GSC/HGSS savings feature
 #define SAVINGS_FLAG             0     // Flag used to toggle savings on/off
 #define SAVINGS_PERCENT          25    // Percentage of money saved after battle (Rounded Down)
 
