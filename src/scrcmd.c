85f098b3dfb3fbe6ccfc73fc7afa81ded3747391
pkmnsnfrn <pkmnsnfrn@gmail.com>
2026-05-31 11:35:16 -0700

    cleanup (bxpy): removed whitespace [ci skip]

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -2771,7 +2771,7 @@ void NativeFunc_SetMetatileInRange(struct ScriptContext *ctx)
     bool8 hasCollision = ScriptReadByte(ctx);
     u8 elevation = ScriptReadByte(ctx);
     u32 temp;
-    
+
     if (xmin > xmax)
         SWAP(xmin, xmax, temp);
 
