11a69f82968f733286b843de613c8517fcde129f
Matteo-Buonastella <matteobuonastella@gmail.com>
2026-06-17 08:20:48 -0400

    June 17/26

--- src/new_game.c
+++ src/new_game.c
@@ -61,6 +61,7 @@ static void WarpToTruck(void);
 static void ResetMiniGamesRecords(void);
 static void ResetItemFlags(void);
 static void ResetDexNav(void);
+static void SetNewGameFlags(void);
 
 EWRAM_DATA bool8 gDifferentSaveFile = FALSE;
 EWRAM_DATA bool8 gEnableContestDebugging = FALSE;
@@ -234,6 +235,7 @@ void NewGameInitData(void)
     ResetItemFlags();
     ResetDexNav();
     ClearFollowerNPCData();
+    SetNewGameFlags();
 }
 
 static void ResetMiniGamesRecords(void)
@@ -257,4 +259,9 @@ static void ResetDexNav(void)
     memset(gSaveBlock3Ptr->dexNavSearchLevels, 0, sizeof(gSaveBlock3Ptr->dexNavSearchLevels));
 #endif
     gSaveBlock3Ptr->dexNavChain = 0;
+}
+
+static void SetNewGameFlags(void)
+{
+    FlagSet(FLAG_SYS_B_DASH);
 }
