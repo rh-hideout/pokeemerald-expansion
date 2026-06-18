e1da013afb1c9c1c63ec79fc0fed7323d652a17e
Frank <fdeblasio1004@gmail.com>
2026-06-18 08:29:04 -0400

    Renamed EFFECT_FLAIL

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -4792,7 +4792,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
         .description = COMPOUND_STRING(
             "Inflicts more damage when\n"
             "the user's HP is down."),
-        .effect = EFFECT_FLAIL,
+        .effect = EFFECT_MORE_POWER_WITH_LESS_HP,
         .power = 1,
         .type = TYPE_NORMAL,
         .accuracy = 100,
@@ -4898,7 +4898,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
         .description = COMPOUND_STRING(
             "Inflicts more damage when\n"
             "the user's HP is down."),
-        .effect = EFFECT_FLAIL,
+        .effect = EFFECT_MORE_POWER_WITH_LESS_HP,
         .power = 1,
         .type = TYPE_FIGHTING,
         .accuracy = 100,
