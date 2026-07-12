7a7591abd4d82aa01cc8328492f8110a0fe805ac
Jamie Foster <jamiefosterandco@gmail.com>
2026-07-13 00:25:19 +0200

    Fix send mon text in frlg first battle

--- src/battle_message.c
+++ src/battle_message.c
@@ -2492,8 +2492,7 @@ void BufferStringBattle(enum StringID stringID, enum BattlerId battler)
         }
         break;
     case STRINGID_INTROSENDOUT: // poke first send-out
-        if (BattlerIsPlayer(battler) || BattlerIsPlayer(BATTLE_PARTNER(battler))
-         || BattlerIsWally(battler) || BattlerIsWally(BATTLE_PARTNER(battler)))
+        if (GetBattlerSide(battler) == B_SIDE_PLAYER)
         {
             if (IsDoubleBattle() && IsValidForBattle(GetBattlerMon(BATTLE_PARTNER(battler))))
             {
