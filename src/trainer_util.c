e4960c4330db4c8d522bed7ee9f98636610ae408
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2026-07-26 21:10:34 +0200

    Apply suggestions from code review
    Co-authored-by: Alex <93446519+AlexOn1ine@users.noreply.github.com>

--- src/trainer_util.c
+++ src/trainer_util.c
@@ -110,7 +110,7 @@ void MakePartnerGenerator(struct TrainerGenerator *trainerGen, const struct Trai
 {
     u32 otID;
     trainerGen->gender = partner->gender;
-    if (trainer->aiFlags & AI_FLAG_SMART_TERA)
+    if (partner->aiFlags & AI_FLAG_SMART_TERA)
         trainerGen->smartTera = TRUE;
     trainerGen->isFrontier = FALSE;
     StringCopyN(trainerGen->name, partner->trainerName, TRAINER_NAME_LENGTH + 1);
