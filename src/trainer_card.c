37268da670aaf05501c0ea1186768223047c83ae
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2026-07-28 14:35:54 -0400

    Remove a now unused function

--- src/trainer_card.c
+++ src/trainer_card.c
@@ -115,7 +115,6 @@ static bool8 LoadCardGfx(void);
 static void CB2_InitTrainerCard(void);
 static u32 GetCappedGameStat(u8 statId, u32 maxValue);
 static bool8 HasAllFrontierSymbols(void);
-static u8 GetRubyTrainerStars(struct TrainerCard *);
 static u16 GetCaughtMonsCount(void);
 static void SetPlayerCardData(struct TrainerCard *, u8);
 static void TrainerCard_GenerateCardForPlayer(struct TrainerCard *);
@@ -686,22 +685,6 @@ u32 CountPlayerTrainerStars(void)
         if (HasAllFrontierSymbols())
             stars++;
     }
-
-    return stars;
-}
-
-static u8 GetRubyTrainerStars(struct TrainerCard *trainerCard)
-{
-    u8 stars = 0;
-
-    if (trainerCard->hofDebutHours || trainerCard->hofDebutMinutes || trainerCard->hofDebutSeconds)
-        stars++;
-    if (trainerCard->caughtAllHoenn)
-        stars++;
-    if (trainerCard->battleTowerStraightWins > 49)
-        stars++;
-    if (trainerCard->hasAllPaintings)
-        stars++;
 
     return stars;
 }
