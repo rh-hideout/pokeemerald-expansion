8a1d086a5fcd8cefff0845c8de54f1971e8a6758
grintoul1 <liam.rintoul@outlook.com>
2026-07-30 23:35:36 +0200

    Fixed spread tests

--- test/test_runner_battle.c
+++ test/test_runner_battle.c
@@ -2875,7 +2875,7 @@ s32 MoveGetTarget(enum BattlerId battlerId, enum Move moveId, struct MoveContext
          || moveTarget == TARGET_FOES_AND_ALLY
          || moveTarget == TARGET_OPPONENTS_FIELD)
         {
-            target = ((battlerId ^ BIT_SIDE) & BIT_SIDE);
+            target = ((battlerId ^ BIT_SIDE)); // Note: this could be bugged under Ally Switch, but Getters do not work here
         }
         else if (moveTarget == TARGET_SELECTED || moveTarget == TARGET_SMART || moveTarget == TARGET_OPPONENT)
         {
@@ -2885,7 +2885,7 @@ s32 MoveGetTarget(enum BattlerId battlerId, enum Move moveId, struct MoveContext
                 INVALID_IF(STATE->battlersCount > 2, "%S requires explicit target", GetMoveName(moveId));
             }
 
-            target = ((battlerId ^ BIT_SIDE) & BIT_SIDE);
+            target = ((battlerId ^ BIT_SIDE)); // Note: this could be bugged under Ally Switch, but Getters do not work here
         }
         else if (moveTarget == TARGET_USER
               || moveTarget == TARGET_ALL_BATTLERS
@@ -3163,7 +3163,7 @@ s32 GetAiMoveTargetForScoreCompare(enum BattlerId battlerId, enum Move moveId, s
     // In Single Battles ai always targets the opposing mon.
     if (GetBattleTest()->type == BATTLE_TEST_AI_SINGLES)
     {
-            target = ((battlerId ^ BIT_SIDE) & BIT_SIDE);
+            target = (battlerId ^ BIT_SIDE);
     }
     else
     {
