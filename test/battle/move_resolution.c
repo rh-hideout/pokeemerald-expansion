3201923b7ad1fd5c9aee564bf051af2c98ed5f7f
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2026-08-08 16:18:51 +0100

    Fix type

--- test/battle/move_resolution.c
+++ test/battle/move_resolution.c
@@ -4,10 +4,10 @@
 DOUBLE_BATTLE_TEST("Moves hitting into Substitute and non-Substitute trigger effects on Substitute battler first")
 {
     struct BattlePokemon *substituteUser = NULL;
-    enum BattlerId substituteUserBattlerId;
+    enum BattlerPosition substituteUserBattlerPosition;
 
-    PARAMETRIZE { substituteUser = opponentLeft; substituteUserBattlerId = B_POSITION_OPPONENT_LEFT; }
-    PARAMETRIZE { substituteUser = opponentRight; substituteUserBattlerId = B_POSITION_OPPONENT_RIGHT; }
+    PARAMETRIZE { substituteUser = opponentLeft; substituteUserBattlerPosition = B_POSITION_OPPONENT_LEFT; }
+    PARAMETRIZE { substituteUser = opponentRight; substituteUserBattlerPosition = B_POSITION_OPPONENT_RIGHT; }
 
     GIVEN {
         ASSUME(GetMoveTarget(MOVE_SURF) == TARGET_FOES_AND_ALLY);
@@ -22,7 +22,7 @@ DOUBLE_BATTLE_TEST("Moves hitting into Substitute and non-Substitute trigger eff
         ANIMATION(ANIM_TYPE_MOVE, MOVE_SUBSTITUTE, opponentLeft);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_SURF, playerLeft);
         SUB_HIT(substituteUser);
-        if (substituteUserBattlerId == B_POSITION_OPPONENT_LEFT) {
+        if (substituteUserBattlerPosition == B_POSITION_OPPONENT_LEFT) {
             MESSAGE("The substitute took damage for the opposing Wobbuffet!");
             MESSAGE("A critical hit on the opposing Wobbuffet!");
             MESSAGE("The opposing Wobbuffet's Air Balloon popped!");
