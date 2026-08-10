281db1529de7dfca9624109cc7f312a5d7f4c3fb
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2026-08-10 18:47:48 +0100

    More tests

--- test/battle/move_resolution.c
+++ test/battle/move_resolution.c
@@ -1,6 +1,70 @@
 #include "global.h"
 #include "test/battle.h"
 
+DOUBLE_BATTLE_TEST("Moves hitting all other battlers activate all effects on allies, then effects on opponents")
+{
+    GIVEN {
+        ASSUME(GetMoveTarget(MOVE_SURF) == TARGET_FOES_AND_ALLY);
+        ASSUME(GetItemHoldEffect(ITEM_AIR_BALLOON) == HOLD_EFFECT_AIR_BALLOON);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_AIR_BALLOON); }
+        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_AIR_BALLOON); }
+        OPPONENT(SPECIES_WYNAUT) { Item(ITEM_AIR_BALLOON); }
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_SURF, criticalHit: TRUE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SURF, playerLeft);
+        MESSAGE("A critical hit on Wobbuffet!");
+        MESSAGE("Wobbuffet's Air Balloon popped!");
+        MESSAGE("A critical hit on the opposing Wobbuffet!");
+        MESSAGE("A critical hit on the opposing Wynaut!");
+        MESSAGE("The opposing Wobbuffet's Air Balloon popped!");
+        MESSAGE("The opposing Wynaut's Air Balloon popped!");
+    }
+}
+
+DOUBLE_BATTLE_TEST("Moves hitting all other battlers print effectiveness messages for allies, then opponents, from left to right")
+{
+    GIVEN {
+        ASSUME(GetMoveTarget(MOVE_SURF) == TARGET_FOES_AND_ALLY);
+        ASSUME(GetMoveType(MOVE_SURF) == TYPE_WATER);
+        ASSUME(GetSpeciesType(SPECIES_CHARMANDER, 0) == TYPE_FIRE || GetSpeciesType(SPECIES_CHARMANDER, 1) == TYPE_FIRE);
+        ASSUME(GetSpeciesType(SPECIES_SQUIRTLE, 0) == TYPE_WATER || GetSpeciesType(SPECIES_SQUIRTLE, 1) == TYPE_WATER);
+        ASSUME(GetSpeciesType(SPECIES_LUDICOLO, 0) == TYPE_WATER && GetSpeciesType(SPECIES_CHARMANDER, 1) == TYPE_GRASS);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_CHARMANDER);
+        OPPONENT(SPECIES_LUDICOLO);
+        OPPONENT(SPECIES_SQUIRTLE);
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_SURF); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SURF, playerLeft);
+        MESSAGE("It's super effective on Charmander!");
+        MESSAGE("It's mostly ineffective on Ludicolo.");
+        MESSAGE("It's not very effective on Squirtle.");
+    }
+}
+
+DOUBLE_BATTLE_TEST("Moves hitting both opponents trigger effects on Left opponent then Right, then the next effect with same order")
+{
+    GIVEN {
+        ASSUME(GetMoveTarget(MOVE_SURF) == TARGET_FOES_AND_ALLY);
+        ASSUME(GetItemHoldEffect(ITEM_AIR_BALLOON) == HOLD_EFFECT_AIR_BALLOON);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET) { Item(ITEM_AIR_BALLOON); }
+        OPPONENT(SPECIES_WYNAUT) { Item(ITEM_AIR_BALLOON); }
+    } WHEN {
+        TURN { MOVE(playerLeft, MOVE_SURF, criticalHit: TRUE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SURF, playerLeft);
+        MESSAGE("A critical hit on the opposing Wobbuffet!");
+        MESSAGE("A critical hit on the opposing Wynaut!");
+        MESSAGE("The opposing Wobbuffet's Air Balloon popped!");
+        MESSAGE("The opposing Wynaut's Air Balloon popped!");
+    }
+}
+
 DOUBLE_BATTLE_TEST("Moves hitting into Substitute and non-Substitute trigger effects on Substitute battler first")
 {
     struct BattlePokemon *substituteUser = NULL;
