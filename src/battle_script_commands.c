1eea88a1a2f8f43b71a882a9a4676c28289936fd
Syreldar <42327659+Syreldar@users.noreply.github.com>
2026-09-04 20:52:24 +0200

    (!) Preserve the first failed Ball for Ball Fetch

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -8211,7 +8211,7 @@ static void SetBallThrowShakes(void)
         return;
     }
 
-    if (!gHasFetchedBall)
+    if (gLastUsedBall == ITEM_NONE)
         gLastUsedBall = gLastUsedItem;
 
     if (IsCriticalCapture())
