0c81735697626de185a69bad5bcd59092e3b0491
Syreldar <42327659+Syreldar@users.noreply.github.com>
2026-09-08 13:55:36 +0200

    Water Veil

--- test/battle/ability/water_veil.c
+++ test/battle/ability/water_veil.c
@@ -1,7 +1,92 @@
 #include "global.h"
 #include "test/battle.h"
 
-TO_DO_BATTLE_TEST("TODO: Write Water Veil (Ability) test titles")
+
+SINGLE_BATTLE_TEST("Water Veil prevents Will-O-Wisp burns")
+{
+    enum Ability ability;
+    PARAMETRIZE { ability = ABILITY_SWIFT_SWIM; }
+    PARAMETRIZE { ability = ABILITY_WATER_VEIL; }
+    GIVEN {
+        PLAYER(SPECIES_BUIZEL) { Ability(ability); MaxHP(1000); HP(1000); }
+        OPPONENT(SPECIES_VULPIX) { Ability(ABILITY_FLASH_FIRE); }
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_WILL_O_WISP); }
+    } THEN {
+        EXPECT_EQ(player->status1, ability == ABILITY_WATER_VEIL ? STATUS1_NONE : STATUS1_BURN);
+    }
+}
+
+SINGLE_BATTLE_TEST("Water Veil prevents burns from physical and special attacks")
+{
+    enum Ability ability;
+    enum Move move;
+    PARAMETRIZE { ability = ABILITY_SWIFT_SWIM; move = MOVE_FIRE_PUNCH; }
+    PARAMETRIZE { ability = ABILITY_WATER_VEIL; move = MOVE_FIRE_PUNCH; }
+    PARAMETRIZE { ability = ABILITY_SWIFT_SWIM; move = MOVE_FLAMETHROWER; }
+    PARAMETRIZE { ability = ABILITY_WATER_VEIL; move = MOVE_FLAMETHROWER; }
+    GIVEN {
+        PLAYER(SPECIES_BUIZEL) { Ability(ability); MaxHP(1000); HP(1000); Speed(100); }
+        OPPONENT(SPECIES_MEW) { Ability(ABILITY_SYNCHRONIZE); Speed(50); }
+    } WHEN {
+        TURN { MOVE(opponent, move); }
+    } SCENE {
+        HP_BAR(player);
+    } THEN {
+        EXPECT_EQ(player->status1, ability == ABILITY_WATER_VEIL ? STATUS1_NONE : STATUS1_BURN);
+    }
+}
+
+SINGLE_BATTLE_TEST("Water Veil cures an existing burn when gained through Skill Swap")
+{
+    GIVEN {
+        PLAYER(SPECIES_BUIZEL) { Ability(ABILITY_WATER_VEIL); Speed(100); }
+        OPPONENT(SPECIES_MEW) { Ability(ABILITY_SYNCHRONIZE); Status1(STATUS1_BURN); MaxHP(1000); HP(1000); Speed(50); }
+    } WHEN {
+        TURN { MOVE(player, MOVE_SKILL_SWAP); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SKILL_SWAP, player);
+        STATUS_ICON(opponent, burn: FALSE);
+    } THEN {
+        EXPECT_EQ(opponent->ability, ABILITY_WATER_VEIL);
+        EXPECT_EQ(opponent->status1, STATUS1_NONE);
+        EXPECT_EQ(opponent->hp, 1000);
+    }
+}
+
+SINGLE_BATTLE_TEST("Water Veil no longer prevents burns while suppressed")
+{
+    bool32 suppress;
+    PARAMETRIZE { suppress = FALSE; }
+    PARAMETRIZE { suppress = TRUE; }
+    GIVEN {
+        PLAYER(SPECIES_BUIZEL) { Ability(ABILITY_WATER_VEIL); MaxHP(1000); HP(1000); Speed(100); }
+        OPPONENT(SPECIES_MEW) { Ability(ABILITY_SYNCHRONIZE); Speed(50); }
+    } WHEN {
+        if (suppress)
+            TURN { MOVE(opponent, MOVE_GASTRO_ACID); }
+        TURN { MOVE(opponent, MOVE_WILL_O_WISP); }
+    } THEN {
+        EXPECT_EQ(player->status1, suppress ? STATUS1_BURN : STATUS1_NONE);
+    }
+}
+
+SINGLE_BATTLE_TEST("Water Veil prevents Flame Orb burns without consuming the item")
+{
+    enum Ability ability;
+    PARAMETRIZE { ability = ABILITY_SWIFT_SWIM; }
+    PARAMETRIZE { ability = ABILITY_WATER_VEIL; }
+    GIVEN {
+        PLAYER(SPECIES_BUIZEL) { Ability(ability); MaxHP(1000); HP(1000); Item(ITEM_FLAME_ORB); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN {}
+    } THEN {
+        EXPECT_EQ(player->status1, ability == ABILITY_WATER_VEIL ? STATUS1_NONE : STATUS1_BURN);
+        EXPECT_EQ(player->item, ITEM_FLAME_ORB);
+    }
+}
+
 
 SINGLE_BATTLE_TEST("Water Veil lets Rawst and Lum Berries cure Mold Breaker burns first")
 {
