05f3f7bbe9b4b1378b3f30694a2d23a47451a8f9
Syreldar <42327659+Syreldar@users.noreply.github.com>
2026-09-13 02:03:33 +0200

    Audit Mold Breaker tests

--- test/battle/ability/mold_breaker.c
+++ test/battle/ability/mold_breaker.c
@@ -41,11 +41,11 @@ SINGLE_BATTLE_TEST("Mold Breaker bypasses Grass Pelt", s16 damage)
 SINGLE_BATTLE_TEST("Mold Breaker cancels damage reduction from Ice Scales", s16 damage)
 {
     enum Ability ability;
-    PARAMETRIZE { ability = ABILITY_SHADOW_TAG; }
+    PARAMETRIZE { ability = ABILITY_HYPER_CUTTER; }
     PARAMETRIZE { ability = ABILITY_MOLD_BREAKER; }
     GIVEN {
         ASSUME(GetMoveCategory(MOVE_PSYCHIC) == DAMAGE_CATEGORY_SPECIAL);
-        PLAYER(SPECIES_WOBBUFFET) { Ability(ability); }
+        PLAYER(SPECIES_PINSIR) { Ability(ability); }
         OPPONENT(SPECIES_FROSMOTH) { Ability(ABILITY_ICE_SCALES); }
     } WHEN {
         TURN { MOVE(player, MOVE_PSYCHIC); }
@@ -66,7 +66,7 @@ DOUBLE_BATTLE_TEST("Mold Breaker will deactivate if user faints")
     GIVEN {
         ASSUME(GetMoveEffect(MOVE_STEEL_BEAM) == EFFECT_MAX_HP_50_RECOIL);
         PLAYER(SPECIES_PINSIR) { HP(1); Ability(ABILITY_MOLD_BREAKER); }
-        PLAYER(SPECIES_ALTARIA) { Ability(ABILITY_COTTON_DOWN); }
+        PLAYER(SPECIES_ELDEGOSS) { Ability(ABILITY_COTTON_DOWN); }
         OPPONENT(SPECIES_BELDUM) { Ability(ABILITY_CLEAR_BODY); }
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
