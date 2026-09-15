ceefe8a2ee937b97f5b794230774015b9b65c532
Jamie Foster <jamiefosterandco@gmail.com>
2026-09-15 11:06:33 +0200

    add Debug menu action to give all ribbons to a mon

--- src/debug.c
+++ src/debug.c
@@ -309,6 +309,8 @@ static void DebugAction_PCBag_Fill_PocketKeyItems(u8 taskId);
 static void DebugAction_PCBag_ClearBag(u8 taskId);
 static void DebugAction_PCBag_ClearBoxes(u8 taskId);
 
+static void DebugAction_Pokemon_SetRibbons(u8 taskId);
+
 static void DebugAction_Party_HealParty(u8 taskId);
 static void DebugAction_Party_ClearPokerus(u8 taskId);
 static void DebugAction_Party_ClearParty(u8 taskId);
@@ -413,6 +415,7 @@ extern const u8 Debug_BerryWeedsDisabled[];
 
 extern const u8 Common_EventScript_MoveRelearner[];
 
+static const struct DebugSelection sGiveAllRibbonsSelection;
 static const struct DebugSelection sWarpSelection;
 static const struct DebugSelection sSetWeatherSelection;
 static const struct DebugSelection sSpeciesGeneratorSelection;
@@ -633,6 +636,7 @@ static const struct DebugMenuOption sDebugMenu_Actions_EditPokemon[] =
     { COMPOUND_STRING("Set Hidden Nature"),  DebugAction_ExecuteScript, Debug_EventScript_SetHiddenNature },
     { COMPOUND_STRING("Set Friendship"),     DebugAction_ExecuteScript, Debug_EventScript_SetFriendship },
     { COMPOUND_STRING("Set Ability"),        DebugAction_ExecuteScript, Debug_EventScript_SetAbility },
+    { COMPOUND_STRING("Set Ribbons"),        DebugAction_Selection_Init, &sGiveAllRibbonsSelection },
     { NULL }
 };
 
@@ -1336,6 +1340,12 @@ static void DebugSelectionStep_ReturnToGiveMenu(u8 taskId)
 {
     Debug_RemoveCallbackMenu();
     DebugAction_OpenSubMenu(taskId, sDebugMenu_Actions_Give);
+}
+
+static void DebugSelectionStep_ReturnToEditPokemonMenu(u8 taskId)
+{
+    Debug_RemoveCallbackMenu();
+    DebugAction_OpenSubMenu(taskId, sDebugMenu_Actions_EditPokemon);
 }
 
 static void DebugSelectionStep_ReturnToTrainersMenu(u8 taskId)
@@ -2188,7 +2198,7 @@ static const struct DebugSelectionStep sItemGeneratorSelectionStep = {
 static const struct DebugSelection sSpeciesGeneratorSelection = {
     .onInit = Debug_CreateInputDisplayWindow,
     .onCancel = DebugSelectionStep_ReturnToUtilMenu,
-    .onComplete = DebugSelection_SpeciesGenerator_OnComplete,//DebugSelection_SetWarp_OnComplete,
+    .onComplete = DebugSelection_SpeciesGenerator_OnComplete,
     .steps = {&sSpeciesGeneratorSelectionStep, &sRandomGeneratorArgsSelectionStep},
     .maxSteps = 2,
 };
@@ -2196,7 +2206,7 @@ static const struct DebugSelection sSpeciesGeneratorSelection = {
 static const struct DebugSelection sItemGeneratorSelection = {
     .onInit = Debug_CreateInputDisplayWindow,
     .onCancel = DebugSelectionStep_ReturnToUtilMenu,
-    .onComplete = DebugSelection_ItemGenerator_OnComplete,//DebugSelection_SetWarp_OnComplete,
+    .onComplete = DebugSelection_ItemGenerator_OnComplete,
     .steps = {&sItemGeneratorSelectionStep, &sRandomGeneratorArgsSelectionStep},
     .maxSteps = 2,
 };
@@ -3771,6 +3781,63 @@ static void DebugAction_PCBag_ClearBoxes(u8 taskId)
     Debug_DestroyMenu_Full(taskId);
     ScriptContext_Enable();
 }
+
+// *******************************
+// Actions Pokemon Edit
+
+static u32 DebugSelectionStep_GetPartyCount(u8 taskId)
+{
+    return (CalculatePlayerPartyCount() - 1);
+}
+
+static void DebugSelectionStep_UpdatePartyMember(u8 taskId, u8 digits, u32 min, u32 max)
+{
+    u32 partyIndex = gTasks[taskId].tInput;
+    ConvertIntToDecimalStringN(gStringVar3, partyIndex, STR_CONV_MODE_LEADING_ZEROS, digits);
+    StringExpandPlaceholders(gStringVar1, COMPOUND_STRING("Party Id: {STR_VAR_3}"));
+    GetMonData(&gParties[B_TRAINER_PLAYER][partyIndex], MON_DATA_NICKNAME, gStringVar2);
+
+    StringCopy(gStringVar3, COMPOUND_STRING(""));
+    Debug_ResetInputDisplayMonIcon(taskId, GetMonData(&gParties[B_TRAINER_PLAYER][partyIndex], MON_DATA_SPECIES));
+    DebugNativeStep_PrintWindowSelection(taskId);
+}
+
+static const struct DebugSelectionStep sPartyMemberSelectionStep = {
+    .stepUpdate = DebugSelectionStep_UpdatePartyMember,
+    .stepConfirm = DebugSelectionStep_GenericInputConfirmAndDestroyIcon,
+    .minValue = 0,
+    .maxFunc = DebugSelectionStep_GetPartyCount,
+    .useMaxFunc = TRUE,
+    .digits = 1
+};
+
+static bool32 DebugSelection_Pokemon_GiveAllRibbons(u8 taskId)
+{
+    u32 partyIndex = DebugSelection_GetData(taskId, 0);
+    struct Pokemon *mon = &gParties[B_TRAINER_PLAYER][partyIndex];
+    u32 maxContextRibbon = 4;
+    for (u32 i = MON_DATA_COOL_RIBBON; i < MON_DATA_COOL_RIBBON + CONTEST_CATEGORIES_COUNT; i++)
+    {
+        SetMonData(mon, i, &maxContextRibbon);
+    }
+    bool32 hasRibbon = TRUE;
+    for (u32 i = MON_DATA_CHAMPION_RIBBON; i <= MON_DATA_WORLD_RIBBON; i++)
+    {
+        SetMonData(mon, i, &hasRibbon);
+    }
+    FlagSet(FLAG_SYS_RIBBON_GET);
+    DebugNativeStep_CloseDebugWindow(taskId);
+    return TRUE;
+}
+
+static const struct DebugSelection sGiveAllRibbonsSelection = {
+    .onInit = Debug_CreateInputDisplayWindow,
+    .onCancel = DebugSelectionStep_ReturnToEditPokemonMenu,
+    .onComplete = DebugSelection_Pokemon_GiveAllRibbons,
+    .steps = {&sPartyMemberSelectionStep},
+    .maxSteps = 1,
+};
+
 
 // *******************************
 // Actions Sound
