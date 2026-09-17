238f50c9975693cd7288bc16fa394d14d753fecb
kubes <59513201+Kubes-Content@users.noreply.github.com>
2026-09-16 20:04:03 -0400

    fix(global.h): support CLion IDE preprocessing
    fix(global.h): support CLion IDE preprocessing

--- include/global.h
+++ include/global.h
@@ -39,7 +39,7 @@
 #endif
 
 /// IDE support
-#if defined(__APPLE__) || defined(__CYGWIN__) || defined(__INTELLISENSE__)
+#if defined(__APPLE__) || defined(__CLION_IDE__) || defined(__CYGWIN__) || defined(__INTELLISENSE__)
 // We define these when using certain IDEs to fool preproc
 #define _(x)        {x}
 #define __(x)       {x}
