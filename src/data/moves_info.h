bdff963241ff5fccc9d1e149bead0ce80f413473
Syreldar <42327659+Syreldar@users.noreply.github.com>
2026-09-21 22:01:12 +0200

    Fix move description compilation with more generation settings

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -3527,8 +3527,8 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_SKULL_BASH] =
     {
         .name = COMPOUND_STRING("Skull Bash"),
-        .description = COMPOUND_STRING(
     #if B_UPDATED_MOVE_DATA >= GEN_2
+        .description = COMPOUND_STRING(
             "Tucks in to raise Defense,\n"
             "then attacks the next turn."),
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -3538,6 +3538,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .onChargeTurnOnly = TRUE,
         }),
     #else
+        .description = COMPOUND_STRING(
             "Tucks in the head, then\n"
             "attacks on the next turn."),
     #endif
@@ -3896,8 +3897,8 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_SKY_ATTACK] =
     {
         .name = COMPOUND_STRING("Sky Attack"),
-        .description = COMPOUND_STRING(
     #if B_UPDATED_MOVE_DATA >= GEN_3
+        .description = COMPOUND_STRING(
             "2-turn move. May flinch and\n"
             "has a high critical-hit rate."),
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -3905,6 +3906,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .chance = 30,
         }),
     #else
+        .description = COMPOUND_STRING(
             "Searches out weak spots,\n"
             "then strikes the next turn."),
     #endif
@@ -3989,8 +3991,8 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_DIZZY_PUNCH] =
     {
         .name = COMPOUND_STRING("Dizzy Punch"),
-        .description = COMPOUND_STRING(
     #if B_UPDATED_MOVE_DATA >= GEN_2
+        .description = COMPOUND_STRING(
             "A rhythmic punch that may\n"
             "confuse the foe."),
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -3998,6 +4000,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .chance = 20,
         }),
     #else
+        .description = COMPOUND_STRING(
             "The foe is hit with a\n"
             "rhythmic punch."),
     #endif
@@ -4281,8 +4284,8 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_ROCK_SLIDE] =
     {
         .name = COMPOUND_STRING("Rock Slide"),
-        .description = COMPOUND_STRING(
     #if B_UPDATED_MOVE_DATA >= GEN_2
+        .description = COMPOUND_STRING(
             "Large boulders are hurled.\n"
             "May cause flinching."),
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -4290,6 +4293,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .chance = 30,
         }),
     #else
+        .description = COMPOUND_STRING(
             "Hits the foes with an\n"
             "avalanche of boulders."),
     #endif
@@ -4400,12 +4404,14 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_TRI_ATTACK] =
     {
         .name = COMPOUND_STRING("Tri Attack"),
-        .description = COMPOUND_STRING(
     #if B_UPDATED_MOVE_DATA >= GEN_2
-            "Fires three types of beams.\n"
         #if B_USE_FROSTBITE
+        .description = COMPOUND_STRING(
+            "Fires three types of beams.\n"
             "May burn/parlyz/frostbite."),
         #else
+        .description = COMPOUND_STRING(
+            "Fires three types of beams.\n"
             "May burn/paralyze/freeze."),
         #endif
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -4414,6 +4420,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .argument.randomMoveEffects = { MOVE_EFFECT_BURN, MOVE_EFFECT_PARALYSIS, MOVE_EFFECT_FREEZE_OR_FROSTBITE },
         }),
     #else
+        .description = COMPOUND_STRING(
             "A triangular field of energy\n"
             "is created and launched."),
     #endif
@@ -6255,8 +6262,8 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_RAPID_SPIN] =
     {
         .name = COMPOUND_STRING("Rapid Spin"),
-        .description = COMPOUND_STRING(
     #if B_SPEED_BUFFING_RAPID_SPIN >= GEN_8
+        .description = COMPOUND_STRING(
             "Spins to remove traps\n"
             "and raise Speed."),
         .additionalEffects = ADDITIONAL_EFFECTS({
@@ -6266,6 +6273,7 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             .chance = 100,
         }),
     #else
+        .description = COMPOUND_STRING(
             "Spins the body at high\n"
             "speed to remove traps."),
     #endif
@@ -7351,20 +7359,23 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
     [MOVE_CHARGE] =
     {
         .name = COMPOUND_STRING("Charge"),
-        .description = COMPOUND_STRING(
     #if B_CHARGE >= GEN_9
         #if B_CHARGE_SPDEF_RAISE >= GEN_5
+        .description = COMPOUND_STRING(
             "Ups the user's next Electric\n"
             "move. It also raises Sp. Def."),
         #else
+        .description = COMPOUND_STRING(
             "Charges power to boost the\n"
             "Electric move used next."),
         #endif
     #else
         #if B_CHARGE_SPDEF_RAISE >= GEN_5
+        .description = COMPOUND_STRING(
             "Ups the user's next move if\n"
             "Electric. Also raises Sp. Def."),
         #else
+        .description = COMPOUND_STRING(
             "Charges power to boost the\n"
             "Electric move used next turn."),
         #endif
