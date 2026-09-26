f9234815ce5a9be59d09767bf33272d64e10127f
grintoul1 <liam.rintoul@outlook.com>
2026-09-26 09:36:29 +0200

    Updated comments

--- include/constants/trainer_slide.h
+++ include/constants/trainer_slide.h
@@ -6,33 +6,34 @@ enum TrainerSlideType
     TRAINER_SLIDE_NONE,
     TRAINER_SLIDE_BEFORE_FIRST_TURN,
 
-    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_CRITICAL_HIT, // Slide activates for the attacking battler when it lands its first crit
-    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_SUPER_EFFECTIVE_HIT, // Slide activates for the attacking battler when it lands its first supereffective hit
-    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_STAB_MOVE, // Slide activates for the attacking battler when it lands its first STAB hit
-    TRAINER_SLIDE_ATTACKER_FAINTS_FIRST_MON, // Slide activates for the attacking battler when it faints its first opponent
-    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_INEFFECTIVE, // Slide activates for the attacking battler when it lands its first ineffective move
+    // The following comments can be read as "This Trainer's messsage will be displayed when...":
+    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_CRITICAL_HIT, // their Pokemon lands a critical hit for the first time
+    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_SUPER_EFFECTIVE_HIT, // their Pokemon lands a super effective hit for the first time
+    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_STAB_MOVE, // their Pokemon lands a STAB move for the first time
+    TRAINER_SLIDE_ATTACKER_FAINTS_FIRST_MON, // their Pokemon faints its first opponent
+    TRAINER_SLIDE_ATTACKER_LANDS_FIRST_INEFFECTIVE, // their Pokemon lands an ineffective move for the first time
 
-    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_CRITICAL_HIT, // Slide activates for the defending battler when it takes its first crit
-    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_SUPER_EFFECTIVE_HIT, // Slide activates for the defending battler when it takes its first supereffective hit
-    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_STAB_MOVE, // Slide activates for the defending battler when it takes its first STAB hit
-    TRAINER_SLIDE_DEFENDER_LOSES_FIRST_MON, // Slide activates for the defending battler when its first mon is lost
-    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_INEFFECTIVE, // Slide activates for the defending battler when it takes its first ineffective move
+    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_CRITICAL_HIT, // their Pokemon receives a critical hit for the first time
+    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_SUPER_EFFECTIVE_HIT, // their Pokemon receives a super effective hit for the first time
+    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_STAB_MOVE, // their Pokemon receives a STAB move hit for the first time
+    TRAINER_SLIDE_DEFENDER_LOSES_FIRST_MON, // their first Pokemon faints
+    TRAINER_SLIDE_DEFENDER_TAKES_FIRST_INEFFECTIVE, // their Pokemon receives an ineffective hit for the first time
 
-    TRAINER_SLIDE_SELF_LAST_SWITCHIN, // Slide activates for the battler when it sends out its last mon
-    TRAINER_SLIDE_SELF_LAST_HALF_HP, // Slide activates for the battler when its last mon is between 25-50% HP
-    TRAINER_SLIDE_SELF_LAST_LOW_HP, // Slide activates for the battler when its last mon is under 25% HP
-    TRAINER_SLIDE_SELF_MEGA_EVOLUTION, // Slide activates for the battler when it is about to Mega
-    TRAINER_SLIDE_SELF_Z_MOVE, // Slide activates for the battler when it is about to use a Z move
-    TRAINER_SLIDE_SELF_DYNAMAX, // Slide activates for the battler when it is about to Dynamax
-    TRAINER_SLIDE_SELF_TERA, // Slide activates for the battler when it is about to Tera
+    TRAINER_SLIDE_SELF_LAST_SWITCHIN, // they send out their last Pokemon
+    TRAINER_SLIDE_SELF_LAST_HALF_HP, // their last Pokemon is between 25% and 50% HP
+    TRAINER_SLIDE_SELF_LAST_LOW_HP, // their last Pokemon is under 25% HP
+    TRAINER_SLIDE_SELF_MEGA_EVOLUTION, // their Pokemon is going to Mega Evolve
+    TRAINER_SLIDE_SELF_Z_MOVE, // their Pokemon is going to use a Z-Move
+    TRAINER_SLIDE_SELF_DYNAMAX, // their Pokemon is going to Dynamax
+    TRAINER_SLIDE_SELF_TERA, // their Pokemon is going to Terastallize
 
-    TRAINER_SLIDE_OPPONENT_LAST_SWITCHIN, // Slide activates for the battler when its opponent(s) sends out their last mon
-    TRAINER_SLIDE_OPPONENT_LAST_HALF_HP, // Slide activates for the battler when its opponent(s) last mon is between 25-50% HP
-    TRAINER_SLIDE_OPPONENT_LAST_LOW_HP, // Slide activates for the battler when its opponent(s) last mon is under 25% HP
-    TRAINER_SLIDE_OPPONENT_MEGA_EVOLUTION, // Slide activates for the battler when its opponent is about to Mega
-    TRAINER_SLIDE_OPPONENT_Z_MOVE, // Slide activates for the battler when its opponent is about to use a Z move
-    TRAINER_SLIDE_OPPONENT_DYNAMAX, // Slide activates for the battler when its opponent is about to Dynamax
-    TRAINER_SLIDE_OPPONENT_TERA, // Slide activates for the battler when its opponent is about to Tera
+    TRAINER_SLIDE_OPPONENT_LAST_SWITCHIN, // an opposing Trainer sends out their last Pokemon
+    TRAINER_SLIDE_OPPONENT_LAST_HALF_HP, // their opponent's last Pokemon is between 25% and 50% HP
+    TRAINER_SLIDE_OPPONENT_LAST_LOW_HP, // their opponent's last Pokemon is under 25% HP
+    TRAINER_SLIDE_OPPONENT_MEGA_EVOLUTION, // an opposing Pokemon is going to Mega Evolve
+    TRAINER_SLIDE_OPPONENT_Z_MOVE, // an opposing Pokemon is going to use a Z-Move
+    TRAINER_SLIDE_OPPONENT_DYNAMAX, // an opposing Pokemon is going to Dynamax
+    TRAINER_SLIDE_OPPONENT_TERA, // an opposing Pokemon is going to Terastallize
     TRAINER_SLIDE_COUNT,
 };
 
